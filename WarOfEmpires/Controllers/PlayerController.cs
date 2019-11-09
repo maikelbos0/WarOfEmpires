b1fcc6b8a219e65c9db2893d52763712b2b0dce0
Maikel Bos <maikel.bos0@gmail.com>
2019-11-09 13:00:51 +0100

    Use new players query

---
+++ WarOfEmpires/Controllers/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Web.Mvc;
+using WarOfEmpires.Models;
+using WarOfEmpires.Models.Players;
+using WarOfEmpires.Queries.Players;
+using WarOfEmpires.Services;
+
+namespace WarOfEmpires.Controllers {
+    [Authorize]
+    [RoutePrefix("Player")]
+    public sealed class PlayerController : BaseController {
+        private readonly IDataGridViewService _dataGridViewService;
+
+        public PlayerController(IAuthenticationService authenticationService, IMessageService messageService, IDataGridViewService dataGridViewService) : base(messageService, authenticationService) {
+            _dataGridViewService = dataGridViewService;
+        }
+
+        [Route]
+        [Route("Index")]
+        [HttpGet]
+        public ActionResult Index() {
+            return View();
+        }
+        
+        [Route("GetPlayers")]
+        [HttpPost]
+        public ActionResult GetPlayers(DataGridViewMetaData metaData) {
+            IEnumerable<PlayerViewModel> data = _messageService.Dispatch(new GetPlayersQuery());
+
+            data = _dataGridViewService.ApplyMetaData(data, ref metaData);
+
+            return Json(new {
+                metaData,
+                data
+            });
+        }
+    }
+}
