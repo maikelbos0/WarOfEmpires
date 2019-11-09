a2909eb2b9c63e61700fef23ca720bceb8f580fc
Maikel Bos <maikel.bos0@gmail.com>
2019-11-09 16:57:31 +0100

    Recruiting domain

---
+++ WarOfEmpires.Domain.Tests/Players/PlayerTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarOfEmpires.Domain.Players;
+
+namespace WarOfEmpires.Domain.Tests.Players {
+    [TestClass]
+    public sealed class PlayerTests {
+        [TestMethod]
+        public void Player_Recruit_Adds_To_CurrentRecruitingEffort() {
+            var player = new Player(0, "Test");
+            player.RecruitsPerDay = 3;
+
+            player.Recruit();
+
+            player.CurrentRecruitingEffort.Should().Be(3);
+        }
+
+        [TestMethod]
+        public void Player_Recruit_Adds_Peasants_When_Possible() {
+            var player = new Player(0, "Test");
+            player.Peasants = 0;
+            player.CurrentRecruitingEffort = 23;
+            player.RecruitsPerDay = 3;
+
+            player.Recruit();
+
+            player.Peasants.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void Player_Recruit_Adds_No_Peasants_When_Not_Possible() {
+            var player = new Player(0, "Test");
+            player.Peasants = 0;
+            player.CurrentRecruitingEffort = 3;
+            player.RecruitsPerDay = 3;
+
+            player.Recruit();
+
+            player.Peasants.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void Player_Recruit_Gives_Correct_Effort_Remainder_When_Adding_Peasants() {
+            var player = new Player(0, "Test");
+            player.Peasants = 0;
+            player.CurrentRecruitingEffort = 23;
+            player.RecruitsPerDay = 3;
+
+            player.Recruit();
+
+            player.CurrentRecruitingEffort.Should().Be(2);
+        }
+    }
+}
--- WarOfEmpires.Domain/Players/Player.cs
+++ WarOfEmpires.Domain/Players/Player.cs
@@ -1,7 +1,15 @@
 ﻿namespace WarOfEmpires.Domain.Players {
     public class Player : AggregateRoot {
+        public const int RecruitingEffortStep = 24;
+
         public virtual string DisplayName { get; set; }
         public virtual Security.User User { get; protected set; }
+        public virtual int Peasants { get; set; } = 10;
+        public virtual int RecruitsPerDay { get; set; } = 1;
+        /// <summary>
+        /// Current number of expected recruits / 24
+        /// </summary>
+        public virtual int CurrentRecruitingEffort { get; set; } = 0;
 
         protected Player() {
         }
@@ -9,6 +17,20 @@
         public Player(int id, string displayName) {
             Id = id;
             DisplayName = displayName;
+        }
+
+        /// <summary>
+        /// Hourly function to work out the new recruiting efford and possible new peasants
+        /// </summary>
+        public void Recruit() {
+            CurrentRecruitingEffort += RecruitsPerDay;
+
+            if (CurrentRecruitingEffort > RecruitingEffortStep) {
+                var newRecruits = CurrentRecruitingEffort / RecruitingEffortStep;
+
+                CurrentRecruitingEffort -= newRecruits * RecruitingEffortStep;
+                Peasants += newRecruits;
+            }
         }
     }
 }
