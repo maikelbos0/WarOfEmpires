359cdc251f43b4e5fff4930238c9d0fcfa7895ee
Maikel Bos <maikel.bos0@gmail.com>
2019-11-13 16:23:04 +0100

    Start of admin page

---
+++ WarOfEmpires/Controllers/AdministrationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using WarOfEmpires.Services;
+
+namespace WarOfEmpires.Controllers {
+    [Authorize]
+    [RoutePrefix("Administration")]
+    public class AdministrationController : BaseController {
+        public AdministrationController(IAuthenticationService authenticationService, IMessageService messageService) : base(messageService, authenticationService) {
+        }
+
+        [Route]
+        [Route("Index")]
+        [HttpGet]
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
