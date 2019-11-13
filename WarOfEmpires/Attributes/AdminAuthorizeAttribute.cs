ced05f7ec5050983f39a98b88b72d6e982f0997a
Maikel Bos <maikel.bos0@gmail.com>
2019-11-13 17:01:07 +0100

    Admin authorization

---
+++ WarOfEmpires/Attributes/AdminAuthorizeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using WarOfEmpires.Services;
+using Unity;
+using System.Net;
+
+namespace WarOfEmpires.Attributes {
+    sealed public class AdminAuthorizeAttribute : ActionFilterAttribute {
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            if (!UnityConfig.Container.Resolve<IAuthenticationService>().IsAdmin()) {
+                filterContext.Result = new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+            }
+        }
+    }
+}
--- WarOfEmpires/Controllers/AdministrationController.cs
+++ WarOfEmpires/Controllers/AdministrationController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Web.Mvc;
+using WarOfEmpires.Attributes;
 using WarOfEmpires.Services;
 
 namespace WarOfEmpires.Controllers {
     [Authorize]
+    [AdminAuthorize]
     [RoutePrefix("Administration")]
     public class AdministrationController : BaseController {
         public AdministrationController(IAuthenticationService authenticationService, IMessageService messageService) : base(messageService, authenticationService) {
