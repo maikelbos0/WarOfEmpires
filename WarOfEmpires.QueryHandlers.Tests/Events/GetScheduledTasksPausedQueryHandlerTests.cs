0552340584d4cadbfde11b2fb58fc58d2c30d686
Maikel Bos <maikel.bos0@gmail.com>
2019-11-13 14:25:13 +0100

    Paused query implementation

--- WarOfEmpires.QueryHandlers.Tests/Events/GetScheduledTasksPausedQueryHandlerTests.cs
+++ WarOfEmpires.QueryHandlers.Tests/Events/GetScheduledTasksPausedQueryHandlerTests.cs
@@ -1,21 +1,61 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using WarOfEmpires.Domain.Events;
+using WarOfEmpires.Queries.Events;
+using WarOfEmpires.QueryHandlers.Events;
+using WarOfEmpires.Test.Utilities;
 
 namespace WarOfEmpires.QueryHandlers.Tests.Events {
     [TestClass]
     public sealed class GetScheduledTasksPausedQueryHandlerTests {
+        private readonly FakeWarContext _context = new FakeWarContext();
+
+        public void AddTasks(params bool[] arePaused) {
+            foreach (var isPaused in arePaused) {
+                var task = Substitute.For<ScheduledTask>();
+
+                task.IsPaused.Returns(isPaused);
+                _context.ScheduledTasks.Add(task);
+            }
+        }
+
         [TestMethod]
         public void GetScheduledTasksPausedQueryHandler_Returns_True_When_All_Paused() {
-            throw new System.NotImplementedException();
+            var query = new GetScheduledTasksPausedQuery();
+            var handler = new GetScheduledTasksPausedQueryHandler(_context);
+
+            AddTasks(true, true);
+
+            handler.Execute(query).Should().BeTrue();
         }
 
         [TestMethod]
         public void GetScheduledTasksPausedQueryHandler_Returns_False_When_None_Paused() {
-            throw new System.NotImplementedException();
+            var query = new GetScheduledTasksPausedQuery();
+            var handler = new GetScheduledTasksPausedQueryHandler(_context);
+
+            AddTasks(false, false);
+
+            handler.Execute(query).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void GetScheduledTasksPausedQueryHandler_Returns_Null_When_No_Tasks_Exist() {
+            var query = new GetScheduledTasksPausedQuery();
+            var handler = new GetScheduledTasksPausedQueryHandler(_context);
+
+            handler.Execute(query).Should().BeNull();
         }
 
         [TestMethod]
         public void GetScheduledTasksPausedQueryHandler_Returns_Null_When_Some_Paused() {
-            throw new System.NotImplementedException();
+            var query = new GetScheduledTasksPausedQuery();
+            var handler = new GetScheduledTasksPausedQueryHandler(_context);
+
+            AddTasks(true, false);
+
+            handler.Execute(query).Should().BeNull();
         }
     }
 }
--- WarOfEmpires.QueryHandlers/Events/GetScheduledTasksPausedQueryHandler.cs
+++ WarOfEmpires.QueryHandlers/Events/GetScheduledTasksPausedQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using WarOfEmpires.Queries.Events;
+﻿using System.Linq;
+using WarOfEmpires.Database;
+using WarOfEmpires.Queries.Events;
 using WarOfEmpires.QueryHandlers.Decorators;
 using WarOfEmpires.Utilities.Container;
 
@@ -6,8 +8,21 @@ namespace WarOfEmpires.QueryHandlers.Events {
     [InterfaceInjectable]
     [Audit]
     public sealed class GetScheduledTasksPausedQueryHandler : IQueryHandler<GetScheduledTasksPausedQuery, bool?> {
+        private readonly IWarContext _context;
+
+        public GetScheduledTasksPausedQueryHandler(IWarContext context) {
+            _context = context;
+        }
+
         public bool? Execute(GetScheduledTasksPausedQuery query) {
-            throw new System.NotImplementedException();
+            var taskPausedStatus = _context.ScheduledTasks.Select(t => t.IsPaused).Distinct().ToList();
+
+            if (taskPausedStatus.Count == 1) {
+                return taskPausedStatus.Single();
+            }
+            else {
+                return null;
+            }
         }
     }
 }
