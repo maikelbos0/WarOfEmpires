87d25ad9814a3e9d481d3b256b62313fe2ea529a
Maikel Bos <maikel.bos0@gmail.com>
2019-11-26 17:34:58 +0100

    Building tests

--- WarOfEmpires.Domain.Tests/Empires/BuildingTests.cs
+++ WarOfEmpires.Domain.Tests/Empires/BuildingTests.cs
@@ -1,16 +1,29 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarOfEmpires.Domain.Common;
+using WarOfEmpires.Domain.Empires;
 
 namespace WarOfEmpires.Domain.Tests.Empires {
     [TestClass]
     public sealed class BuildingTests {
         [TestMethod]
         public void Building_Generates_Name_Correctly() {
-            throw new System.NotImplementedException();
+            var building = new Building(
+                new ExpressionGenerator<string>((int level, int levelOffset) => $"Level {level}"),
+                new ExpressionGenerator<Resources>((int level, int levelOffset) => new Resources(gold: level * 20000))
+            );
+
+            building.GetName(5).Should().Be("Level 5");
         }
 
         [TestMethod]
         public void Building_Generates_Costs_Correctly() {
-            throw new System.NotImplementedException();
+            var building = new Building(
+                new ExpressionGenerator<string>((int level, int levelOffset) => $"Level {level}"),
+                new ExpressionGenerator<Resources>((int level, int levelOffset) => new Resources(gold: level * 20000))
+            );
+
+            building.GetNextLevelCost(5).Should().Be(new Resources(gold: 100000));
         }
     }
 }
--- WarOfEmpires.Domain.Tests/Empires/ExpressionGeneratorTests.cs
+++ WarOfEmpires.Domain.Tests/Empires/ExpressionGeneratorTests.cs
@@ -1,31 +1,61 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WarOfEmpires.Domain.Empires;
 
 namespace WarOfEmpires.Domain.Tests.Empires {
     [TestClass]
     public sealed class ExpressionGeneratorTests {
         [TestMethod]
         public void ExpressionGenerator_Generates_Value_From_Default_Constant() {
-            throw new System.NotImplementedException();
+            var generator = new ExpressionGenerator<string>("Default");
+
+            generator.Get(5).Should().Be("Default");
         }
 
         [TestMethod]
         public void ExpressionGenerator_Generates_Value_From_Constant() {
-            throw new System.NotImplementedException();
+            var generator = new ExpressionGenerator<string>("Default");
+
+            generator.Add(4, "Medium");
+            generator.Add(8, "Large");
+
+            generator.Get(4).Should().Be("Medium");
         }
 
         [TestMethod]
         public void ExpressionGenerator_Generates_Value_From_Default_GeneratorFunction() {
-            throw new System.NotImplementedException();
+            var generator = new ExpressionGenerator<string>((int level, int levelOffset) => $"Level {level}, offset {levelOffset}");
+
+            generator.Get(5).Should().Be("Level 5, offset 5");
         }
 
         [TestMethod]
         public void ExpressionGenerator_Generates_Value_From_GeneratorFunction() {
-            throw new System.NotImplementedException();
+            var generator = new ExpressionGenerator<string>("Default");
+
+            generator.Add(4, (int level, int levelOffset) => $"Level {level}, offset {levelOffset}");
+            generator.Add(8, "New");
+
+            generator.Get(5).Should().Be("Level 5, offset 1");
         }
 
         [TestMethod]
-        public void ExpressionGenerator_Throws_Exception_For_Negative_Level() {
-            throw new System.NotImplementedException();
+        public void ExpressionGenerator_Add_Throws_Exception_For_Negative_Level() {
+            var generator = new ExpressionGenerator<string>("Default");
+
+            Action action = () => generator.Add(-1, "Error");
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [TestMethod]
+        public void ExpressionGenerator_Get_Throws_Exception_For_Negative_Level() {
+            var generator = new ExpressionGenerator<string>("Default");
+
+            Action action = () => generator.Get(-1);
+
+            action.Should().Throw<ArgumentOutOfRangeException>();
         }
     }
 }
