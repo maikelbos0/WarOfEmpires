dd1d7d8b66438b62c1cdc05c9f05ef6a2c3b65fb
Maikel Bos <maikel.bos0@gmail.com>
2019-12-23 21:17:36 +0100

    Sequence generator for building costs

---
+++ WarOfEmpires.Domain.Tests/Empires/SequenceGeneratorFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarOfEmpires.Domain.Empires;
+
+namespace WarOfEmpires.Domain.Tests.Empires {
+    [TestClass]
+    public sealed class SequenceGeneratorFactoryTests {
+        [DataTestMethod]
+        [DataRow(0, 2, DisplayName = "First of first round")]
+        [DataRow(1, 3, DisplayName = "Second of first round")]
+        [DataRow(4, 13, DisplayName = "Last of first round")]
+        [DataRow(5, 20, DisplayName = "First of second round")]
+        [DataRow(9, 130, DisplayName = "Last of second round")]
+        [DataRow(12, 500, DisplayName = "Third of third round")]
+        public void SequenceGeneratorFactory_GetValue_Works(int level, int expectedResult) {
+            SequenceGeneratorFactory.GetValue(level).Should().Be(expectedResult);
+        }
+
+        [TestMethod]
+        public void SequenceGeneratorFactory_GetGeneratorFunction_Works() {
+            var generator = SequenceGeneratorFactory.GetGeneratorFunction(1000);
+
+            generator(0, 0).Should().Be(2000);
+            generator(9, 0).Should().Be(130000);
+        }
+    }
+}
---
+++ WarOfEmpires.Domain/Empires/SequenceGeneratorFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace WarOfEmpires.Domain.Empires {
+    public static class SequenceGeneratorFactory {
+        private static readonly int[] _seed = { 2, 3, 5, 8, 13 };
+
+        public static ExpressionGenerator<int>.GeneratorFunction GetGeneratorFunction(int multiplier) {
+            return (int level, int levelOffset) => GetValue(level) * multiplier;
+        }
+
+        public static int GetValue(int level) {
+            var exponent = (int)System.Math.Pow(10, level / _seed.Length);
+
+            return exponent * _seed[level % _seed.Length];
+        }
+    }
+}
