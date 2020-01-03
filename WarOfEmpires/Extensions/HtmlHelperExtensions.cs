e162d8758b16e8376ccd570b2068e0fa77f8447c
Maikel Bos <maikel.bos0@gmail.com>
2020-01-03 19:02:37 +0100

    Easier display of resources

---
+++ WarOfEmpires/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using WarOfEmpires.Models.Empires;
+
+namespace WarOfEmpires.Extensions {
+    public static class HtmlHelperExtensions {
+        public static MvcHtmlString DisplayFor<TModel>(this HtmlHelper<TModel> html, Expression<Func<TModel, ResourcesViewModel>> expression) {
+            var model = expression.Compile().Invoke(html.ViewData.Model);
+
+            return html.Partial("_DisplayResources", model);
+        }
+    }
+}
