2bb54d565435562a0b856ec69f676caabba20369
Maikel Bos <maikel.bos0@gmail.com>
2020-01-25 14:40:24 +0100

    Test setup for siege commands

--- WarOfEmpires.CommandHandlers.Tests/Empires/BuildSiegeCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Empires/BuildSiegeCommandHandlerTests.cs
@@ -4,7 +4,58 @@ namespace WarOfEmpires.CommandHandlers.Tests.Empires {
     [TestClass]
     public sealed class BuildSiegeCommandHandlerTests {
         [TestMethod]
-        public void Implement() {
+        public void BuildSiegeCommandHandler_Succceeds() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void BuildSiegeCommandHandler_Allows_Empty_Values() {
+            throw new System.NotImplementedException();
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 0, 0, DisplayName = "FireArrows")]
+        [DataRow(0, 1, 0, DisplayName = "BatteringRams")]
+        [DataRow(0, 0, 1, DisplayName = "ScalingLadders")]
+        public void BuildSiegeCommandHandler_Fails_For_Too_Little_Maintenance() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void BuildSiegeCommandHandler_Fails_For_Alphanumeric_FireArrows() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void BuildSiegeCommandHandler_Fails_For_Alphanumeric_BatteringRams() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void BuildSiegeCommandHandler_Fails_For_Alphanumeric_ScalingLadders() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void BuildSiegeCommandHandler_Fails_For_Negative_FireArrows() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void BuildSiegeCommandHandler_Fails_For_Negative_BatteringRams() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void BuildSiegeCommandHandler_Fails_For_Negative_ScalingLadders() {
+            throw new System.NotImplementedException();
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 0, 0, DisplayName = "FireArrows")]
+        [DataRow(0, 1, 0, DisplayName = "BatteringRams")]
+        [DataRow(0, 0, 1, DisplayName = "ScalingLadders")]
+        public void BuildSiegeCommandHandler_Fails_For_Too_Little_Resources() {
             throw new System.NotImplementedException();
         }
     }
--- WarOfEmpires.CommandHandlers.Tests/Empires/DiscardSiegeCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Empires/DiscardSiegeCommandHandlerTests.cs
@@ -4,7 +4,57 @@ namespace WarOfEmpires.CommandHandlers.Tests.Empires {
     [TestClass]
     public sealed class DiscardSiegeCommandHandlerTests {
         [TestMethod]
-        public void Implement() {
+        public void DiscardSiegeCommandHandler_Succeeds() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Allows_Empty_Values() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Fails_For_Alphanumeric_FireArrows() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Fails_For_Alphanumeric_BatteringRams() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Fails_For_Alphanumeric_ScalingLadders() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Fails_For_Too_High_FireArrows() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Fails_For_Too_High_BatteringRams() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Fails_For_Too_High_ScalingLadders() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Fails_For_Negative_FireArrows() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Fails_For_Negative_BatteringRams() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void DiscardSiegeCommandHandler_Fails_For_Negative_ScalingLadders() {
             throw new System.NotImplementedException();
         }
     }
