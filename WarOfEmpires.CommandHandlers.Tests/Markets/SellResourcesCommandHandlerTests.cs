d587d402d29aa22fa61d051e9cb51804e4b9b4e9
Maikel Bos <maikel.bos0@gmail.com>
2020-03-15 14:24:13 +0100

    All tests for selling resources

--- WarOfEmpires.CommandHandlers.Tests/Markets/SellResourcesCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Markets/SellResourcesCommandHandlerTests.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 using System.Collections.Generic;
+using System.Linq;
+using WarOfEmpires.CommandHandlers.Markets;
+using WarOfEmpires.Commands.Markets;
 using WarOfEmpires.Domain.Common;
 using WarOfEmpires.Domain.Empires;
+using WarOfEmpires.Domain.Markets;
 using WarOfEmpires.Domain.Players;
 using WarOfEmpires.Domain.Security;
 using WarOfEmpires.Repositories.Players;
@@ -25,7 +30,7 @@ namespace WarOfEmpires.CommandHandlers.Tests.Markets {
             var player = Substitute.For<Player>();
             player.User.Returns(user);
             player.Workers.Returns(new List<Workers>() { new Workers(WorkerType.Merchants, 10) });
-            player.GetBuildingBonus(BuildingType.Market).Returns(6);
+            player.GetBuildingBonus(BuildingType.Market).Returns(3);
             player.CanAfford(Arg.Any<Resources>()).Returns(true);
 
             _context.Users.Add(user);
@@ -35,46 +40,363 @@ namespace WarOfEmpires.CommandHandlers.Tests.Markets {
 
         [TestMethod]
         public void SellResourcesCommandHandler_Succeeds() {
-            throw new System.NotImplementedException();
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "1000", "10", "2000", "9", "3000", "8", "4000", "7");
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+            _player.Received().SellResources(Arg.Is<IEnumerable<MerchandiseTotals>>(m => m.Count() == 4));
+            _context.CallsToSaveChanges.Should().Be(1);
         }
 
         [TestMethod]
         public void SellResourcesCommandHandler_Allows_Empty_Values() {
-            throw new System.NotImplementedException();
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "", "", "", "", "", "", "", "");
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+            _player.DidNotReceiveWithAnyArgs().SellResources(default);
         }
 
         [TestMethod]
         public void SellResourcesCommandHandler_Fails_For_Too_Few_Available_Merchants() {
-            // Test with caravans on the market already to make sure those are taken into account
-            throw new System.NotImplementedException();
+            _player.Caravans.Returns(new List<Caravan>() {
+                new Caravan(_player),
+                new Caravan(_player)
+            });
+
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "50000", "10", "50000", "9", "50000", "8", "51000", "7");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.Should().BeNull();
+            result.Errors[0].Message.Should().Be("You don't have enough merchants available to send to the market");
+            _player.DidNotReceiveWithAnyArgs().SellResources(default);
         }
 
-        // TODO duplicate these tests for wood, stone, ore
         [TestMethod]
         public void SellResourcesCommandHandler_Fails_For_Alphanumeric_Food() {
-            throw new System.NotImplementedException();
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "A", "5", "5", "5", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Food");
+            result.Errors[0].Message.Should().Be("Food must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
-        public void SellResourcesCommandHandler_Fails_For_Negative_Food() {
-            throw new System.NotImplementedException();
+        public void SellResourcesCommandHandler_Fails_For_Zero_Food() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "0", "5", "5", "5", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Food");
+            result.Errors[0].Message.Should().Be("Food must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void SellResourcesCommandHandler_Fails_For_Too_Little_Available_Food() {
-            throw new System.NotImplementedException();
-        }
+            _player.CanAfford(Arg.Any<Resources>()).Returns(false);
 
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "", "", "", "", "", "");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Food");
+            result.Errors[0].Message.Should().Be("You don't have enough food available to sell that much");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
 
         [TestMethod]
         public void SellResourcesCommandHandler_Fails_For_Alphanumeric_FoodPrice() {
-            throw new System.NotImplementedException();
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "A", "5", "5", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.FoodPrice");
+            result.Errors[0].Message.Should().Be("Food price must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
-        public void SellResourcesCommandHandler_Fails_For_Negative_FoodPrice() {
-            throw new System.NotImplementedException();
+        public void SellResourcesCommandHandler_Fails_For_Zero_FoodPrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "0", "5", "5", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.FoodPrice");
+            result.Errors[0].Message.Should().Be("Food price must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
         }
 
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Food_Without_FoodPrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "", "5", "5", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.FoodPrice");
+            result.Errors[0].Message.Should().Be("Food price is required when selling food");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Alphanumeric_Wood() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "A", "5", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Wood");
+            result.Errors[0].Message.Should().Be("Wood must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Zero_Wood() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "0", "5", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Wood");
+            result.Errors[0].Message.Should().Be("Wood must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Too_Little_Available_Wood() {
+            _player.CanAfford(Arg.Any<Resources>()).Returns(false);
+
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "", "", "5", "5", "", "", "", "");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Wood");
+            result.Errors[0].Message.Should().Be("You don't have enough wood available to sell that much");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Alphanumeric_WoodPrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "A", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.WoodPrice");
+            result.Errors[0].Message.Should().Be("Wood price must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Zero_WoodPrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "0", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.WoodPrice");
+            result.Errors[0].Message.Should().Be("Wood price must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Wood_Without_WoodPrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "", "5", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.WoodPrice");
+            result.Errors[0].Message.Should().Be("Wood price is required when selling wood");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Alphanumeric_Stone() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "5", "A", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Stone");
+            result.Errors[0].Message.Should().Be("Stone must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Zero_Stone() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "5", "0", "5", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Stone");
+            result.Errors[0].Message.Should().Be("Stone must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Too_Little_Available_Stone() {
+            _player.CanAfford(Arg.Any<Resources>()).Returns(false);
+
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "", "", "", "", "5", "5", "", "");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Stone");
+            result.Errors[0].Message.Should().Be("You don't have enough stone available to sell that much");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Alphanumeric_StonePrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "5", "5", "A", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.StonePrice");
+            result.Errors[0].Message.Should().Be("Stone price must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Zero_StonePrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "0", "5", "5", "5", "0", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.StonePrice");
+            result.Errors[0].Message.Should().Be("Stone price must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Stone_Without_StonePrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "5", "5", "", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.StonePrice");
+            result.Errors[0].Message.Should().Be("Stone price is required when selling stone");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Alphanumeric_Ore() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "5", "5", "5", "A", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Ore");
+            result.Errors[0].Message.Should().Be("Ore must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Zero_Ore() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "5", "5", "5", "0", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Ore");
+            result.Errors[0].Message.Should().Be("Ore must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Too_Little_Available_Ore() {
+            _player.CanAfford(Arg.Any<Resources>()).Returns(false);
+
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "", "", "", "", "", "", "5", "5");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.Ore");
+            result.Errors[0].Message.Should().Be("You don't have enough ore available to sell that much");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Alphanumeric_OrePrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "5", "5", "5", "5", "5", "5", "A");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.OrePrice");
+            result.Errors[0].Message.Should().Be("Ore price must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Zero_OrePrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "0", "5", "5", "5", "5", "5", "0");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.OrePrice");
+            result.Errors[0].Message.Should().Be("Ore price must be a valid number");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellResourcesCommandHandler_Fails_For_Ore_Without_OrePrice() {
+            var handler = new SellResourcesCommandHandler(_repository);
+            var command = new SellResourcesCommand("test@test.com", "5", "", "5", "5", "5", "5", "5", "");
+
+            var result = handler.Execute(command);
+
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.ToString().Should().Be("c => c.OrePrice");
+            result.Errors[0].Message.Should().Be("Ore price is required when selling ore");
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
     }
 }
