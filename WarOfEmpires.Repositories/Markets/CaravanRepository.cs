cbab3d12e1b66e9eb751fd4d6ea4ea805939f444
Maikel Bos <maikel.bos0@gmail.com>
2020-03-19 13:25:46 +0100

    Caravan repository tests and fixes

--- WarOfEmpires.Repositories/Markets/CaravanRepository.cs
+++ WarOfEmpires.Repositories/Markets/CaravanRepository.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Linq;
 using WarOfEmpires.Database;
 using WarOfEmpires.Domain.Markets;
+using WarOfEmpires.Domain.Security;
 
 namespace WarOfEmpires.Repositories.Markets {
     public class CaravanRepository : ICaravanRepository {
@@ -14,6 +15,7 @@ namespace WarOfEmpires.Repositories.Markets {
 
         public IEnumerable<Caravan> GetForMerchandiseType(MerchandiseType type) {
             return _context.Players
+                .Where(p => p.User.Status == UserStatus.Active)
                 .SelectMany(p => p.Caravans)
                 .SelectMany(c => c.Merchandise, (c, m) => new { Caravan = c, Merchandise = m })
                 .Where(c => c.Merchandise.Type == type)
@@ -21,6 +23,7 @@ namespace WarOfEmpires.Repositories.Markets {
                 .ThenBy(c => c.Caravan.Id)
                 .Select(c => c.Caravan)
                 .Include(c => c.Merchandise)
+                .Include(c => c.Player)
                 .ToList();
         }
 
