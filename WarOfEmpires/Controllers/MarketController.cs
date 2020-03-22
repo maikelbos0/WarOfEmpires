0f8cb2243206ac5827e4cbd3b7c69c22799cda2c
Maikel Bos <maikel.bos0@gmail.com>
2020-03-22 12:22:51 +0100

    Market menu

---
+++ WarOfEmpires/Controllers/MarketController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using WarOfEmpires.Services;
+
+namespace WarOfEmpires.Controllers {
+    [Authorize]
+    [RoutePrefix("Empire")]
+    public class MarketController : BaseController {
+        public MarketController(IAuthenticationService authenticationService, IMessageService messageService) : base(messageService, authenticationService) {
+        }
+
+        [Route("Index")]
+        [HttpGet]
+        public ActionResult Index() {
+            return new EmptyResult();
+        }
+
+        [Route("BuyIndex")]
+        [HttpGet]
+        public ActionResult BuyIndex() {
+            return new EmptyResult();
+        }
+    }
+}
