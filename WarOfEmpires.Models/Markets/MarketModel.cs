50d46271c02331048b0d0e7c7eb17a2db7903204
Maikel Bos <maikel.bos0@gmail.com>
2020-03-28 14:13:35 +0100

    Allow bigger amounts to be sold in one go

--- WarOfEmpires.Models/Markets/MarketModel.cs
+++ WarOfEmpires.Models/Markets/MarketModel.cs
@@ -4,22 +4,22 @@ using System.ComponentModel.DataAnnotations;
 namespace WarOfEmpires.Models.Markets {
     public sealed class MarketModel {
         [DisplayName("Food")]
-        [RegularExpression("^\\d{0,6}$", ErrorMessage = "Food must be a valid number")]
+        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Food must be a valid number")]
         public string Food { get; set; }
         [RegularExpression("^\\d{0,6}$", ErrorMessage = "Food price must be a valid number")]
         public string FoodPrice { get; set; }
         [DisplayName("Wood")]
-        [RegularExpression("^\\d{0,6}$", ErrorMessage = "Wood must be a valid number")]
+        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Wood must be a valid number")]
         public string Wood { get; set; }
         [RegularExpression("^\\d{0,6}$", ErrorMessage = "Wood price must be a valid number")]
         public string WoodPrice { get; set; }
         [DisplayName("Stone")]
-        [RegularExpression("^\\d{0,6}$", ErrorMessage = "Stone must be a valid number")]
+        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Stone must be a valid number")]
         public string Stone { get; set; }
         [RegularExpression("^\\d{0,6}$", ErrorMessage = "Stone price must be a valid number")]
         public string StonePrice { get; set; }
         [DisplayName("Ore")]
-        [RegularExpression("^\\d{0,6}$", ErrorMessage = "Ore must be a valid number")]
+        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Ore must be a valid number")]
         public string Ore { get; set; }
         [RegularExpression("^\\d{0,6}$", ErrorMessage = "Ore price must be a valid number")]
         public string OrePrice { get; set; }
