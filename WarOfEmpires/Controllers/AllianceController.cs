c1e13e26e508bf00b29cbda93c8337a27232feb3
Maikel Bos <maikel.bos0@gmail.com>
2020-04-27 15:31:01 +0200

    Start on pages

---
+++ WarOfEmpires/Controllers/AllianceController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using WarOfEmpires.Models.Alliances;
+using WarOfEmpires.Services;
+
+namespace WarOfEmpires.Controllers {
+    public class AllianceController : BaseController {
+        public AllianceController(IAuthenticationService authenticationService, IMessageService messageService, IDataGridViewService dataGridViewService)
+            : base(messageService, authenticationService, dataGridViewService) {
+        }
+
+        [HttpGet]
+        public ActionResult Create() {
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateAllianceModel model) {
+            return null;
+        }
+    }
+}
