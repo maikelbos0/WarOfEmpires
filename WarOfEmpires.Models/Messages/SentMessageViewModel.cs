191c7505fe5c96b569dda4b03f88f6d63dbb060f
Maikel Bos <maikel.bos0@gmail.com>
2020-05-04 19:08:34 +0200

    Grid extension: messages

--- WarOfEmpires.Models/Messages/SentMessageViewModel.cs
+++ WarOfEmpires.Models/Messages/SentMessageViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using WarOfEmpires.Models.Grids;
 using WarOfEmpires.Utilities.Formatting;
 
 namespace WarOfEmpires.Models.Messages {
-    public sealed class SentMessageViewModel : EntityViewModel {
-        public string Recipient { get; set; }
+    [GridSorting(nameof(Date), true)]
+    public sealed class SentMessageViewModel : EntityViewModel {        
         public DateTime Date { get; set; }
+        [GridColumn(0, 15, "Sent", SortData = nameof(Date))]
         public string DateString { get { return Date.ToString(StringFormat.Date); } }
+        [GridColumn(1, 20, "Recipient")]
+        public string Recipient { get; set; }
+        [GridColumn(2, 65, "Subject")]
         public string Subject { get; set; }
         public bool IsRead { get; set; }
     }
