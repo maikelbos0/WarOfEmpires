c0f9d1f35273ed76dea668473ab581fed25052e6
Maikel Bos <maikel.bos0@gmail.com>
2020-05-27 18:20:17 +0200

    Player page for received invites

--- WarOfEmpires.QueryHandlers.Tests/Alliances/GetReceivedInvitesQueryHandlerTest.cs
+++ WarOfEmpires.QueryHandlers.Tests/Alliances/GetReceivedInvitesQueryHandlerTest.cs
@@ -1,11 +1,77 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WarOfEmpires.Domain.Alliances;
+using WarOfEmpires.Domain.Players;
+using WarOfEmpires.Domain.Security;
+using WarOfEmpires.Queries.Alliances;
+using WarOfEmpires.QueryHandlers.Alliances;
+using WarOfEmpires.Test.Utilities;
 
 namespace WarOfEmpires.QueryHandlers.Tests.Alliances {
     [TestClass]
     public sealed class GetReceivedInvitesQueryHandlerTest {
+        private readonly FakeWarContext _context = new FakeWarContext();
+
+
+        public GetReceivedInvitesQueryHandlerTest() {
+            var user = Substitute.For<User>();
+            var player = Substitute.For<Player>();
+            var invites = new List<Invite>();
+
+            user.Id.Returns(1);
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Active);
+            player.Id.Returns(1);
+            player.User.Returns(user);
+            player.DisplayName.Returns("Test");
+            player.Invites.Returns(invites);
+
+            _context.Users.Add(user);
+            _context.Players.Add(player);
+
+            foreach (var allianceName in new[] { "Allies", "Another" }) {
+                var alliance = Substitute.For<Alliance>();
+                var invite = Substitute.For<Invite>();
+
+                alliance.Id.Returns(invites.Count + 3);
+                alliance.Code.Returns(allianceName.ToUpper().Substring(0, 4));
+                alliance.Name.Returns(allianceName);
+
+                invite.Id.Returns(invites.Count + 1);
+                invite.Date.Returns(new DateTime(2020, 1, 30 - invites.Count));
+                invite.Alliance.Returns(alliance);
+                invite.Player.Returns(player);
+                invite.Message.Returns($"Invite from {allianceName}");
+                invites.Add(invite);
+
+                _context.Alliances.Add(alliance);
+            }
+        }
+
         [TestMethod]
-        public void MyTestMethod() {
-            throw new System.NotImplementedException();
+        public void GetReceivedInvitesQueryHandler_Returns_Correct_Information() {
+            var handler = new GetReceivedInvitesQueryHandler(_context);
+            var query = new GetReceivedInvitesQuery("test@test.com");
+
+            var result = handler.Execute(query).ToList();
+
+            result.Should().HaveCount(2);
+            result[0].Id.Should().Be(2);
+            result[0].AllianceId.Should().Be(4);
+            result[0].AllianceCode.Should().Be("ANOT");
+            result[0].AllianceName.Should().Be("Another");
+            result[0].Date.Should().Be(new DateTime(2020, 1, 29));
+            result[0].Message.Should().Be("Invite from Another");
+            result[1].Id.Should().Be(1);
+            result[1].AllianceId.Should().Be(3);
+            result[1].AllianceCode.Should().Be("ALLI");
+            result[1].AllianceName.Should().Be("Allies");
+            result[1].Date.Should().Be(new DateTime(2020, 1, 30));
+            result[1].Message.Should().Be("Invite from Allies");
         }
     }
 }
--- WarOfEmpires.QueryHandlers/Alliances/GetReceivedInvitesQueryHandler.cs
+++ WarOfEmpires.QueryHandlers/Alliances/GetReceivedInvitesQueryHandler.cs
@@ -1,15 +1,35 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using WarOfEmpires.Database;
 using WarOfEmpires.Models.Alliances;
 using WarOfEmpires.Queries.Alliances;
 using WarOfEmpires.QueryHandlers.Decorators;
 using WarOfEmpires.Utilities.Container;
+using WarOfEmpires.Utilities.Services;
 
 namespace WarOfEmpires.QueryHandlers.Alliances {
     [InterfaceInjectable]
     [Audit]
     public sealed class GetReceivedInvitesQueryHandler : IQueryHandler<GetReceivedInvitesQuery, IEnumerable<ReceivedInviteViewModel>> {
+        private readonly IWarContext _context;
+        public GetReceivedInvitesQueryHandler(IWarContext context) {
+            _context = context;
+        }
+
         public IEnumerable<ReceivedInviteViewModel> Execute(GetReceivedInvitesQuery query) {
-            throw new System.NotImplementedException();
+            return _context.Players
+                .Single(p => EmailComparisonService.Equals(p.User.Email, query.Email))
+                .Invites
+                .OrderBy(i => i.Date)
+                .Select(i => new ReceivedInviteViewModel() {
+                    Id = i.Id,
+                    Date = i.Date,
+                    AllianceId = i.Alliance.Id,
+                    AllianceCode = i.Alliance.Code,
+                    AllianceName = i.Alliance.Name,
+                    Message = i.Message
+                })
+                .ToList();
         }
     }
 }
