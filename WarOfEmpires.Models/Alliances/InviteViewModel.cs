b9dce626bf20f7ec363813c1cffd3b13a1ad5db2
Maikel Bos <maikel.bos0@gmail.com>
2020-07-11 16:47:26 +0200

    Responsive grids for invites

--- WarOfEmpires.Models/Alliances/InviteViewModel.cs
+++ WarOfEmpires.Models/Alliances/InviteViewModel.cs
@@ -6,13 +6,13 @@ namespace WarOfEmpires.Models.Alliances {
     [GridSorting(nameof(Date), true)]
     public sealed class InviteViewModel : EntityViewModel {
         public DateTime Date { get; set; }
-        [GridColumn(0, 15, "Date", SortData = nameof(Date))]
+        [GridColumn(0, 25, "Date", SortData = nameof(Date), ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromMedium)]
         public string DateString { get { return Date.ToString(StringFormat.Date); } }
         public int PlayerId { get; set; }
-        [GridColumn(1, 30, "Recipient")]
+        [GridColumn(1, 25, "Recipient")]
         public string PlayerName { get; set; }
         public bool IsRead { get; set; }
-        [GridColumn(3, 55, "Subject")]
+        [GridColumn(3, 50, "Subject")]
         public string Subject { get; set; }
     }
 }
