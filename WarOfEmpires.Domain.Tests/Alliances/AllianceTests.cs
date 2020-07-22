ccf150976fb6ff8d8724d6125f375711ef83122f
Maikel Bos <maikel.bos0@gmail.com>
2020-07-22 11:01:40 +0200

    Alliance role domain setup

--- WarOfEmpires.Domain.Tests/Alliances/AllianceTests.cs
+++ WarOfEmpires.Domain.Tests/Alliances/AllianceTests.cs
@@ -32,5 +32,60 @@ namespace WarOfEmpires.Domain.Tests.Alliances {
             alliance.ChatMessages.First().Date.Should().BeCloseTo(DateTime.UtcNow, 1000);
             alliance.ChatMessages.First().Message.Should().Be("Test message");
         }
+
+        [TestMethod]
+        public void Alliance_CreateRole_Succeeds() {
+            var member = new Player(1, "Member");
+            var alliance = new Alliance(member, "TEST", "The Test");
+
+            alliance.CreateRole("Testrole");
+
+            alliance.Roles.Should().HaveCount(1);
+            alliance.Roles.First().Alliance.Should().Be(alliance);
+            alliance.Roles.First().Name.Should().Be("Testrole");
+        }
+
+        [TestMethod]
+        public void Alliance_DeleteRole_Succeeds() {
+            var member = new Player(1, "Member");
+            var alliance = new Alliance(member, "TEST", "The Test");
+            var role = new Role(alliance, "Testrole");
+
+            role.Players.Add(member);
+            alliance.Roles.Add(role);
+
+            alliance.DeleteRole(role);
+
+            alliance.Roles.Should().HaveCount(0);
+            role.Players.Should().HaveCount(0);
+        }
+
+        [TestMethod]
+        public void Alliance_SetRole_Succeeds() {
+            var member = new Player(1, "Member 1");
+            var alliance = new Alliance(member, "TEST", "The Test");
+            var role = new Role(alliance, "Testrole");
+
+            alliance.Roles.Add(role);
+
+            alliance.SetRole(member, role);
+
+            role.Players.Should().BeEquivalentTo(member);
+        }
+
+        [TestMethod]
+        public void Alliance_ClearRole_Succeeds() {
+            var member1 = new Player(1, "Member 1");
+            var member2 = new Player(2, "Member 2");
+            var alliance = new Alliance(member1, "TEST", "The Test");
+            var role = new Role(alliance, "Testrole");
+
+            role.Players.Add(member1);
+            role.Players.Add(member2);
+            alliance.Roles.Add(role);
+
+            alliance.ClearRole(member1);
+            role.Players.Should().BeEquivalentTo(member2);
+        }
     }
 }
