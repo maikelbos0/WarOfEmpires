ce45481ae4fee6f871a742312d3050a15d267679
Maikel Bos <maikel.bos0@gmail.com>
2020-07-27 15:44:54 +0200

    Set and clear role command tests

--- WarOfEmpires.CommandHandlers.Tests/Alliances/ClearRoleCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/ClearRoleCommandHandlerTests.cs
@@ -1,21 +1,84 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using WarOfEmpires.CommandHandlers.Alliances;
+using WarOfEmpires.Commands.Alliances;
+using WarOfEmpires.Domain.Alliances;
+using WarOfEmpires.Domain.Players;
+using WarOfEmpires.Domain.Security;
+using WarOfEmpires.Repositories.Players;
+using WarOfEmpires.Test.Utilities;
 
 namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
     [TestClass]
     public sealed class ClearRoleCommandHandlerTests {
+        private readonly FakeWarContext _context = new FakeWarContext();
+        private readonly PlayerRepository _repository;
+        private readonly Player _player;
+        private readonly Alliance _alliance;
+        private readonly Role _role;
+
+        public ClearRoleCommandHandlerTests() {
+            _repository = new PlayerRepository(_context);
+
+            _alliance = Substitute.For<Alliance>();
+
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Active);
+            user.Id.Returns(2);
+
+            _player = Substitute.For<Player>();
+            _player.Alliance.Returns(_alliance);
+            _player.User.Returns(user);
+            _player.Id.Returns(2);
+
+            _role = Substitute.For<Role>();
+            _role.Alliance.Returns(_alliance);
+            _role.Id.Returns(3);
+            _role.Players.Returns(new List<Player>() { _player });
+            _alliance.Roles.Returns(new List<Role>() { _role });
+
+            _context.Alliances.Add(_alliance);
+            _context.Users.Add(user);
+            _context.Players.Add(_player);
+        }
+
         [TestMethod]
         public void ClearRoleCommandHandler_Succeeds() {
-            throw new System.NotImplementedException();
+            var handler = new ClearRoleCommandHandler(_repository);
+            var command = new ClearRoleCommand("test@test.com", "2");
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+            _role.Players.Should().BeEmpty();
         }
 
         [TestMethod]
         public void ClearRoleCommandHandler_Throws_Exception_For_Player_Not_In_Alliance() {
-            throw new System.NotImplementedException();
+            _player.Alliance.Returns((Alliance)null);
+
+            var handler = new ClearRoleCommandHandler(_repository);
+            var command = new ClearRoleCommand("test@test.com", "2");
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<InvalidOperationException>();
         }
 
         [TestMethod]
         public void ClearRoleCommandHandler_Throws_Exception_For_Member_Of_Different_Alliance() {
-            throw new System.NotImplementedException();
+            _alliance.Members.Remove(_player);
+
+            var handler = new ClearRoleCommandHandler(_repository);
+            var command = new ClearRoleCommand("test@test.com", "2");
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<InvalidOperationException>();
         }
     }
 }
--- WarOfEmpires.CommandHandlers.Tests/Alliances/SetRoleCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/SetRoleCommandHandlerTests.cs
@@ -1,26 +1,96 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using WarOfEmpires.CommandHandlers.Alliances;
+using WarOfEmpires.Commands.Alliances;
+using WarOfEmpires.Domain.Alliances;
+using WarOfEmpires.Domain.Players;
+using WarOfEmpires.Domain.Security;
+using WarOfEmpires.Repositories.Players;
+using WarOfEmpires.Test.Utilities;
 
 namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
     [TestClass]
-    public sealed class SetRoleCommandHandlerTestsHandlerTests {
+    public sealed class SetRoleCommandHandlerTests {
+        private readonly FakeWarContext _context = new FakeWarContext();
+        private readonly PlayerRepository _repository;
+        private readonly Player _player;
+        private readonly Alliance _alliance;
+        private readonly Role _role;
+
+        public SetRoleCommandHandlerTests() {
+            _repository = new PlayerRepository(_context);
+
+            _alliance = Substitute.For<Alliance>();
+
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Active);
+            user.Id.Returns(2);
+
+            _player = Substitute.For<Player>();
+            _player.Alliance.Returns(_alliance);
+            _player.User.Returns(user);
+            _player.Id.Returns(2);
+
+            _role = Substitute.For<Role>();
+            _role.Alliance.Returns(_alliance);
+            _role.Id.Returns(3);
+            _role.Players.Returns(new List<Player>() { });
+
+            _context.Alliances.Add(_alliance);
+            _context.Users.Add(user);
+            _context.Players.Add(_player);
+        }
+
         [TestMethod]
         public void SetRoleCommandHandler_Succeeds() {
-            throw new System.NotImplementedException();
+            var handler = new SetRoleCommandHandler(_repository);
+            var command = new SetRoleCommand("test@test.com", "2", "3");
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+            _role.Players.Should().HaveCount(1);
+            _role.Players.Should().Contain(_player);
         }
 
         [TestMethod]
         public void SetRoleCommandHandler_Throws_Exception_For_Player_Not_In_Alliance() {
-            throw new System.NotImplementedException();
+            _player.Alliance.Returns((Alliance)null);
+
+            var handler = new SetRoleCommandHandler(_repository);
+            var command = new SetRoleCommand("test@test.com", "2", "3");
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<InvalidOperationException>();
         }
 
         [TestMethod]
         public void SetRoleCommandHandler_Throws_Exception_For_Role_Of_Different_Alliance() {
-            throw new System.NotImplementedException();
+            _alliance.Roles.Remove(_role);
+
+            var handler = new SetRoleCommandHandler(_repository);
+            var command = new SetRoleCommand("test@test.com", "2", "3");
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<InvalidOperationException>();
         }
 
         [TestMethod]
         public void SetRoleCommandHandler_Throws_Exception_For_Member_Of_Different_Alliance() {
-            throw new System.NotImplementedException();
+            _alliance.Members.Remove(_player);
+
+            var handler = new SetRoleCommandHandler(_repository);
+            var command = new SetRoleCommand("test@test.com", "2", "3");
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<InvalidOperationException>();
         }
     }
 }
