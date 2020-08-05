308c2e85ee3082d93822b822b9dbedef06350b3a
Maikel Bos <maikel.bos0@gmail.com>
2020-08-05 19:17:33 +0200

    Fluent second half of alliances

--- WarOfEmpires.QueryHandlers.Tests/Alliances/GetInvitesQueryHandlerTests.cs
+++ WarOfEmpires.QueryHandlers.Tests/Alliances/GetInvitesQueryHandlerTests.cs
@@ -12,12 +12,12 @@ namespace WarOfEmpires.QueryHandlers.Tests.Alliances {
     public sealed class GetInvitesQueryHandlerTests {
         [TestMethod]
         public void GetInvitesQueryHandler_Returns_Correct_Information() {
-            var builder = new FakeBuilder();
-
-            builder.BuildAlliance(1)
-                .WithInvite(1, builder.BuildPlayer(4).Player, subject: "Message", isRead: false, date: new DateTime(2020, 2, 15))
-                .WithInvite(2, builder.BuildPlayer(5, status: UserStatus.Inactive).Player, subject: "Message", isRead: false, date: new DateTime(2020, 1, 2))
-                .WithInvite(3, builder.BuildPlayer(6).Player, subject: "Another message", isRead: true, date: new DateTime(2020, 1, 10))
+            var builder = new FakeBuilder()
+                .WithPlayer(4, out var activePlayer)
+                .WithPlayer(5, out var inactivePlayer, status: UserStatus.Inactive)
+                .BuildAlliance(1)
+                .WithInvite(1, activePlayer, subject: "Message", isRead: false, date: new DateTime(2020, 2, 15))
+                .WithInvite(2, inactivePlayer, subject: "Message", isRead: false, date: new DateTime(2020, 1, 2))
                 .BuildLeader(1);
 
             var handler = new GetInvitesQueryHandler(builder.Context);
@@ -25,19 +25,13 @@ namespace WarOfEmpires.QueryHandlers.Tests.Alliances {
 
             var result = handler.Execute(query).ToList();
 
-            result.Should().HaveCount(2);
+            result.Should().HaveCount(1);
             result[0].Id.Should().Be(1);
             result[0].PlayerId.Should().Be(4);
             result[0].PlayerName.Should().Be("Test display name 4");
             result[0].IsRead.Should().BeFalse();
             result[0].Date.Should().Be(new DateTime(2020, 2, 15));
             result[0].Subject.Should().Be("Message");
-            result[1].Id.Should().Be(3);
-            result[1].PlayerId.Should().Be(6);
-            result[1].PlayerName.Should().Be("Test display name 6");
-            result[1].IsRead.Should().BeTrue();
-            result[1].Date.Should().Be(new DateTime(2020, 1, 10));
-            result[1].Subject.Should().Be("Another message");
         }
     }
 }
