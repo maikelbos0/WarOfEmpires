41903e6d8b6c001ae032b6c67426fa6f38ddf242
Maikel Bos <maikel.bos0@gmail.com>
2020-08-09 09:49:36 +0200

    More empire tests with fake builder

--- WarOfEmpires.CommandHandlers.Tests/Empires/SetTaxCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Empires/SetTaxCommandHandlerTests.cs
@@ -3,46 +3,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 using WarOfEmpires.CommandHandlers.Empires;
 using WarOfEmpires.Commands.Empires;
-using WarOfEmpires.Domain.Players;
-using WarOfEmpires.Domain.Security;
 using WarOfEmpires.Repositories.Players;
 using WarOfEmpires.Test.Utilities;
 
 namespace WarOfEmpires.CommandHandlers.Tests.Empires {
     [TestClass]
     public sealed class SetTaxCommandHandlerTests {
-        private readonly FakeWarContext _context = new FakeWarContext();
-        private readonly PlayerRepository _repository;
-        private readonly Player _player;
-
-        public SetTaxCommandHandlerTests() {
-            _repository = new PlayerRepository(_context);
-
-            var user = Substitute.For<User>();
-            user.Email.Returns("test@test.com");
-            user.Status.Returns(UserStatus.Active);
-
-            var player = Substitute.For<Player>();
-            player.User.Returns(user);
-
-            _context.Users.Add(user);
-            _context.Players.Add(player);
-            _player = player;
-        }
-
         [DataTestMethod]
         [DataRow("0", DisplayName = "Minimum")]
         [DataRow("50", DisplayName = "Normal")]
         [DataRow("100", DisplayName = "Maximum")]
         public void SetTaxCommandHandler_Succeeds(string tax) {
-            var command = new SetTaxCommand("test@test.com", tax);
-            var handler = new SetTaxCommandHandler(_repository);
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new SetTaxCommandHandler(new PlayerRepository(builder.Context));
+            var command = new SetTaxCommand("test1@test.com", tax);
 
             var result = handler.Execute(command);
 
             result.Success.Should().BeTrue();
-            _player.Received().Tax = int.Parse(tax);
-            _context.CallsToSaveChanges.Should().Be(1);
+            builder.Player.Received().Tax = int.Parse(tax);
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
 
         [DataTestMethod]
@@ -50,13 +32,17 @@ namespace WarOfEmpires.CommandHandlers.Tests.Empires {
         [DataRow("-1", DisplayName = "Negative")]
         [DataRow("101", DisplayName = "Too High")]
         public void SetTaxCommandHandler_Fails_For_Invalid_Tax(string tax) {
-            var command = new SetTaxCommand("test@test.com", tax);
-            var handler = new SetTaxCommandHandler(_repository);
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new SetTaxCommandHandler(new PlayerRepository(builder.Context));
+            var command = new SetTaxCommand("test1@test.com", tax);
 
             var result = handler.Execute(command);
             
             result.Should().HaveError("Tax", "Tax must be a valid number");
-            _player.DidNotReceive().Tax = Arg.Any<int>();
+            builder.Player.DidNotReceive().Tax = Arg.Any<int>();
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
     }
 }
--- WarOfEmpires.CommandHandlers.Tests/Empires/TrainTroopsCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Empires/TrainTroopsCommandHandlerTests.cs
@@ -5,45 +5,24 @@ using System.Collections.Generic;
 using WarOfEmpires.CommandHandlers.Empires;
 using WarOfEmpires.Commands.Empires;
 using WarOfEmpires.Domain.Attacks;
-using WarOfEmpires.Domain.Common;
-using DomainPlayers = WarOfEmpires.Domain.Players;
-using WarOfEmpires.Domain.Security;
 using WarOfEmpires.Repositories.Players;
 using WarOfEmpires.Test.Utilities;
-using WarOfEmpires.Utilities.Formatting;
 using System;
 
 namespace WarOfEmpires.CommandHandlers.Tests.Empires {
     [TestClass]
     public sealed class TrainTroopsCommandHandlerTests {
-        private readonly FakeWarContext _context = new FakeWarContext();
-        private readonly PlayerRepository _repository;
-        private readonly DomainPlayers.Player _player;
-        private readonly EnumFormatter _formatter = new EnumFormatter();
-
-        public TrainTroopsCommandHandlerTests() {
-            _repository = new PlayerRepository(_context);
-
-            var user = Substitute.For<User>();
-            user.Email.Returns("test@test.com");
-            user.Status.Returns(UserStatus.Active);
-
-            var player = Substitute.For<DomainPlayers.Player>();
-            player.GetAvailableBarracksCapacity().Returns(20);
-            player.User.Returns(user);
-            player.Peasants.Returns(30);
-            player.CanAfford(Arg.Any<Resources>()).Returns(true);
-
-            _context.Users.Add(user);
-            _context.Players.Add(player);
-            _player = player;
-        }
-        
         [TestMethod]
         public void TrainTroopsCommandHandler_Succeeds() {
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>(){
-                new TroopInfo("Archers", "5", "3"),
+            var builder = new FakeBuilder()
+                .BuildPlayer(1)
+                .WithPeasants(20);
+
+            builder.Player.GetAvailableBarracksCapacity().Returns(20);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>(){
+                new TroopInfo("Archers", "6", "3"),
                 new TroopInfo("Cavalry", "4", "2"),
                 new TroopInfo("Footmen", "4", "1")
             });
@@ -51,16 +30,20 @@ namespace WarOfEmpires.CommandHandlers.Tests.Empires {
             var result = handler.Execute(command);
 
             result.Success.Should().BeTrue();
-            _player.Received().TrainTroops(TroopType.Archers, 5, 3);
-            _player.Received().TrainTroops(TroopType.Cavalry, 4, 2);
-            _player.Received().TrainTroops(TroopType.Footmen, 4, 1);
-            _context.CallsToSaveChanges.Should().Be(1);
+            builder.Player.Received().TrainTroops(TroopType.Archers, 6, 3);
+            builder.Player.Received().TrainTroops(TroopType.Cavalry, 4, 2);
+            builder.Player.Received().TrainTroops(TroopType.Footmen, 4, 1);
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
         
         [TestMethod]
         public void TrainTroopsCommandHandler_Allows_Empty_Troops() {
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>(){
+            var builder = new FakeBuilder()
+                .BuildPlayer(1)
+                .WithPeasants(30);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>(){
                 new TroopInfo("Archers", "", ""),
                 new TroopInfo("Cavalry", "", ""),
                 new TroopInfo("Footmen", "", "")
@@ -69,129 +52,157 @@ namespace WarOfEmpires.CommandHandlers.Tests.Empires {
             var result = handler.Execute(command);
 
             result.Success.Should().BeTrue();
-            _player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
 
         [TestMethod]
         public void TrainTroopsCommandHandler_Throws_Exception_For_Invalid_Type() {
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>() {
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>() {
                 new TroopInfo("Test", "1", "1")
             });
 
             Action action = () => handler.Execute(command);
 
             action.Should().Throw<ArgumentException>();
-            _player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
-            _context.CallsToSaveChanges.Should().Be(0);
+            builder.Player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void TrainTroopsCommandHandler_Fails_For_Too_Little_Barracks_Room() {
-            _player.Peasants.Returns(30);
+            var builder = new FakeBuilder()
+                .BuildPlayer(1)
+                .WithPeasants(30);
 
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>(){
-                new TroopInfo("Archers", "8", "2"),
-                new TroopInfo("Cavalry", "8", "2"),
-                new TroopInfo("Footmen", "8", "2")
+            builder.Player.GetAvailableBarracksCapacity().Returns(20);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>(){
+                new TroopInfo("Archers", "5", "2"),
+                new TroopInfo("Cavalry", "5", "2"),
+                new TroopInfo("Footmen", "5", "2")
             });
 
             var result = handler.Execute(command);
 
             result.Should().HaveError("You don't have enough barracks available to train that many troops");
-            _player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
-            _context.CallsToSaveChanges.Should().Be(0);
+            builder.Player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void TrainTroopsCommandHandler_Fails_For_Alphanumeric_Soldiers() {
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>(){
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>(){
                 new TroopInfo("Archers", "A", "2")
             });
 
             var result = handler.Execute(command);
 
             result.Should().HaveError("Troops[0].Soldiers", "Invalid number");
-            _player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
-            _context.CallsToSaveChanges.Should().Be(0);
+            builder.Player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void TrainTroopsCommandHandler_Fails_For_Alphanumeric_Mercenaries() {
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>(){
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>(){
                 new TroopInfo("Archers", "2", "A")
             });
 
             var result = handler.Execute(command);
 
             result.Should().HaveError("Troops[0].Mercenaries", "Invalid number");
-            _player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
-            _context.CallsToSaveChanges.Should().Be(0);
+            builder.Player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void TrainTroopsCommandHandler_Fails_For_Negative_Soldiers() {
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>(){
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>(){
                 new TroopInfo("Archers", "-1", "2")
             });
 
             var result = handler.Execute(command);
 
             result.Should().HaveError("Troops[0].Soldiers", "Invalid number");
-            _player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
-            _context.CallsToSaveChanges.Should().Be(0);
+            builder.Player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void TrainTroopsCommandHandler_Fails_For_Negative_Mercenaries() {
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>(){
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>(){
                 new TroopInfo("Archers", "2", "-1")
             });
 
             var result = handler.Execute(command);
 
             result.Should().HaveError("Troops[0].Mercenaries", "Invalid number");
-            _player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
-            _context.CallsToSaveChanges.Should().Be(0);
+            builder.Player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
         
         [TestMethod]
         public void TrainTroopsCommandHandler_Fails_For_Too_High_TroopCounts() {
-            _player.GetAvailableBarracksCapacity().Returns(40);
+            var builder = new FakeBuilder()
+                .BuildPlayer(1)
+                .WithPeasants(30);
 
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>(){
+            builder.Player.GetAvailableBarracksCapacity().Returns(40);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>(){
                 new TroopInfo("Archers", "11", "0"),
-                new TroopInfo("Cavalry", "11", "0"),
-                new TroopInfo("Footmen", "11", "0")
+                new TroopInfo("Cavalry", "10", "0"),
+                new TroopInfo("Footmen", "10", "0")
             });
 
             var result = handler.Execute(command);
 
             result.Should().HaveError("You don't have that many peasants available to train");
-            _player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
-            _context.CallsToSaveChanges.Should().Be(0);
+            builder.Player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
         
         [TestMethod]
         public void TrainTroopsCommandHandler_Fails_For_Too_Little_Resources() {
-            _player.CanAfford(Arg.Any<Resources>()).Returns(false);
+            var builder = new FakeBuilder()
+                .BuildPlayer(1, canAffordAnything: false)
+                .WithPeasants(30);
 
-            var handler = new TrainTroopsCommandHandler(_repository);
-            var command = new TrainTroopsCommand("test@test.com", new List<TroopInfo>(){
+            builder.Player.GetAvailableBarracksCapacity().Returns(20);
+
+            var handler = new TrainTroopsCommandHandler(new PlayerRepository(builder.Context));
+            var command = new TrainTroopsCommand("test1@test.com", new List<TroopInfo>(){
                 new TroopInfo("Archers", "1", "1")
             });
 
             var result = handler.Execute(command);
 
             result.Should().HaveError("You don't have enough resources to train these troops");
-            _player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
-            _context.CallsToSaveChanges.Should().Be(0);
+            builder.Player.DidNotReceiveWithAnyArgs().TrainTroops(default, default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
     }
 }
