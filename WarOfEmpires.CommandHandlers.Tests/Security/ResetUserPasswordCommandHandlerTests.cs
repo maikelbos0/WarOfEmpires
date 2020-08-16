2aa1eba80a0b797d58857a014471cb028ffbeeac
Maikel Bos <maikel.bos0@gmail.com>
2020-08-16 16:09:59 +0200

    Final user command tests with fake builder

--- WarOfEmpires.CommandHandlers.Tests/Security/ResetUserPasswordCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Security/ResetUserPasswordCommandHandlerTests.cs
@@ -11,111 +11,93 @@ using System;
 namespace WarOfEmpires.CommandHandlers.Tests.Security {
     [TestClass]
     public sealed class ResetUserPasswordCommandHandlerTests {
-        private readonly FakeWarContext _context = new FakeWarContext();
-        private readonly UserRepository _repository;
-
-        public ResetUserPasswordCommandHandlerTests() {
-            _repository = new UserRepository(_context);
-        }
-
         [TestMethod]
         public void ResetUserPasswordCommandHandler_Succeeds() {
-            var handler = new ResetUserPasswordCommandHandler(_repository);
-            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
-            var user = Substitute.For<User>();
-            user.Email.Returns("test@test.com");
-            user.Status.Returns(UserStatus.Active);
-            user.Password.Returns(new Password("test"));
-            user.PasswordResetToken.Returns(new TemporaryPassword("test"));
+            var builder = new FakeBuilder()
+                .BuildUser(1);
 
-            _context.Users.Add(user);
+            builder.User.PasswordResetToken.Returns(new TemporaryPassword("reset-token"));
+
+            var handler = new ResetUserPasswordCommandHandler(new UserRepository(builder.Context));
+            var command = new ResetUserPasswordCommand("test1@test.com", "reset-token", "test2");
 
             var result = handler.Execute(command);
 
             result.Success.Should().BeTrue();
-            user.Received().ResetPassword("test2");
-            user.DidNotReceive().ResetPasswordFailed();
-            _context.CallsToSaveChanges.Should().Be(1);
+            builder.User.Received().ResetPassword("test2");
+            builder.User.DidNotReceiveWithAnyArgs().ResetPasswordFailed();
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
 
         [TestMethod]
         public void ResetUserPasswordCommandHandler_Fails_For_Expired_Token() {
-            var handler = new ResetUserPasswordCommandHandler(_repository);
-            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
-            var user = Substitute.For<User>();
-            user.Email.Returns("test@test.com");
-            user.Status.Returns(UserStatus.Active);
-            user.Password.Returns(new Password("test"));
-            var token = new TemporaryPassword("test");
-            typeof(TemporaryPassword).GetProperty(nameof(TemporaryPassword.ExpiryDate)).SetValue(token, DateTime.UtcNow.AddSeconds(-60));
-            user.PasswordResetToken.Returns(token);
+            var builder = new FakeBuilder()
+                .BuildUser(1);
 
-            _context.Users.Add(user);
+            builder.User.PasswordResetToken.Returns(new TemporaryPassword("reset-token"));
+            typeof(TemporaryPassword).GetProperty(nameof(TemporaryPassword.ExpiryDate)).SetValue(builder.User.PasswordResetToken, DateTime.UtcNow.AddSeconds(-60));
 
+            var handler = new ResetUserPasswordCommandHandler(new UserRepository(builder.Context));
+            var command = new ResetUserPasswordCommand("test1@test.com", "reset-token", "test2");
             var result = handler.Execute(command);
 
             result.Should().HaveError("The password reset link has expired; please request a new one");
-            user.DidNotReceive().ResetPassword(Arg.Any<string>());
-            user.Received().ResetPasswordFailed();
+            builder.User.DidNotReceiveWithAnyArgs().ResetPassword(default);
+            builder.User.Received().ResetPasswordFailed();
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
 
         [TestMethod]
         public void ResetUserPasswordCommandHandler_Fails_When_Already_Reset() {
-            var handler = new ResetUserPasswordCommandHandler(_repository);
-            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
-            var user = Substitute.For<User>();
-            user.Email.Returns("test@test.com");
-            user.Status.Returns(UserStatus.Active);
-            user.Password.Returns(new Password("test"));
-            user.PasswordResetToken.Returns(TemporaryPassword.None);
+            var builder = new FakeBuilder()
+                .BuildUser(1);
 
-            _context.Users.Add(user);
+            builder.User.PasswordResetToken.Returns(TemporaryPassword.None);
+
+            var handler = new ResetUserPasswordCommandHandler(new UserRepository(builder.Context));
+            var command = new ResetUserPasswordCommand("test1@test.com", "test", "test2");
 
             var result = handler.Execute(command);
 
             result.Should().HaveError("The password reset link has expired; please request a new one");
-            user.DidNotReceive().ResetPassword(Arg.Any<string>());
-            user.Received().ResetPasswordFailed();
+            builder.User.DidNotReceiveWithAnyArgs().ResetPassword(default);
+            builder.User.Received().ResetPasswordFailed();
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
 
         [TestMethod]
         public void ResetUserPasswordCommandHandler_Fails_For_Incorrect_Token() {
-            var handler = new ResetUserPasswordCommandHandler(_repository);
-            var command = new ResetUserPasswordCommand("test@test.com", "wrong", "test2");
-            var user = Substitute.For<User>();
-            user.Email.Returns("test@test.com");
-            user.Status.Returns(UserStatus.Active);
-            user.Password.Returns(new Password("test"));
-            user.PasswordResetToken.Returns(new TemporaryPassword("test"));
+            var builder = new FakeBuilder()
+                .BuildUser(1);
 
-            _context.Users.Add(user);
+            builder.User.PasswordResetToken.Returns(new TemporaryPassword("reset-token"));
+
+            var handler = new ResetUserPasswordCommandHandler(new UserRepository(builder.Context));
+            var command = new ResetUserPasswordCommand("test1@test.com", "wrong", "test2");
 
             var result = handler.Execute(command);
 
             result.Should().HaveError("The password reset link has expired; please request a new one");
-            user.DidNotReceive().ResetPassword(Arg.Any<string>());
-            user.Received().ResetPasswordFailed();
+            builder.User.DidNotReceiveWithAnyArgs().ResetPassword(default);
+            builder.User.Received().ResetPasswordFailed();
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
 
         [TestMethod]
         public void ResetUserPasswordCommandHandler_Throws_Exception_For_Invalid_User() {
-            var handler = new ResetUserPasswordCommandHandler(_repository);
-            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
-            var user = Substitute.For<User>();
-            user.Email.Returns("test@test.com");
-            user.Status.Returns(UserStatus.Inactive);
-            user.Password.Returns(new Password("test"));
-            user.PasswordResetToken.Returns(new TemporaryPassword("test"));
+            var builder = new FakeBuilder()
+                .BuildUser(1, status: UserStatus.Inactive);
 
-            _context.Users.Add(user);
+            builder.User.PasswordResetToken.Returns(new TemporaryPassword("reset-token"));
 
-            Action commandAction = () => {
-                var result = handler.Execute(command);
-            };
+            var handler = new ResetUserPasswordCommandHandler(new UserRepository(builder.Context));
+            var command = new ResetUserPasswordCommand("test1@test.com", "test", "test2");
+
+            Action commandAction = () => handler.Execute(command);
 
             commandAction.Should().Throw<InvalidOperationException>();
-            user.DidNotReceive().ResetPassword(Arg.Any<string>());
-            user.DidNotReceive().ResetPasswordFailed();
+            builder.User.DidNotReceiveWithAnyArgs().ResetPassword(default);
+            builder.User.DidNotReceiveWithAnyArgs().ResetPasswordFailed();
         }
     }
 }
