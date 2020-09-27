ae8438a73fca7786ffe1ea6630586e7d55318ed4
Maikel Bos <maikel.bos0@gmail.com>
2020-09-27 18:37:59 +0200

    TODOs

--- WarOfEmpires.Domain/Alliances/Role.cs
+++ WarOfEmpires.Domain/Alliances/Role.cs
@@ -6,6 +6,7 @@ namespace WarOfEmpires.Domain.Alliances {
         public virtual Alliance Alliance { get; protected set; }
         public virtual ICollection<Player> Players { get; protected set; } = new List<Player>();
         public virtual string Name { get; set; }
+        // TODO move alliance rights to (flags) enum
         public virtual bool CanInvite { get; set; }
 
         protected Role() {
--- WarOfEmpires/Views/WebViewPage.cs
+++ WarOfEmpires/Views/WebViewPage.cs
@@ -3,6 +3,7 @@ using WarOfEmpires.Services;
 
 namespace WarOfEmpires.Views {
     public abstract class WebViewPage<TModel> : System.Web.Mvc.WebViewPage<TModel> {
+        // TODO add as extension method instead of overriding page        
         public bool IsAuthorized(IAllianceAuthorizationRequest request) {
             return UnityConfig.Container.Resolve<IAuthorizationService>().IsAuthorized(request);
         }
