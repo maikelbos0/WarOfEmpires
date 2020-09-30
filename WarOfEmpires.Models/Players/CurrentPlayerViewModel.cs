c5bb31dbe88fdc58b1bda03c6ce9c1cb9aa0b74b
Maikel Bos <maikel.bos0@gmail.com>
2020-09-30 10:03:25 +0200

    Right in menu

--- WarOfEmpires.Models/Players/CurrentPlayerViewModel.cs
+++ WarOfEmpires.Models/Players/CurrentPlayerViewModel.cs
@@ -4,6 +4,7 @@
         public bool IsAdmin { get; set; }
         public bool IsInAlliance { get; set; }
         public bool CanInvite { get; set; }
+        public bool CanManageRoles { get; set; }
         public string DisplayName { get; set; }
     }
 }
