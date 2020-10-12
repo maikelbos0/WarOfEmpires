2b9564c1fee8ab5364b4dcc414c39788e6b63123
Maikel Bos <maikel.bos0@gmail.com>
2020-10-12 18:15:02 +0200

    TODO list for validation summary

--- WarOfEmpires/Global.asax.cs
+++ WarOfEmpires/Global.asax.cs
@@ -23,5 +23,41 @@ namespace WarOfEmpires {
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        /*
+         * TODO @using (Html.BeginForm("PauseScheduledTasks", "Administration")) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Administration\ScheduledTasks.cshtml	10	26
+         * TODO @using (Html.BeginForm("UnpauseScheduledTasks", "Administration")) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Administration\ScheduledTasks.cshtml	18	26
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new { @class = "html-only" })) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\Create.cshtml	13	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new { @class = "html-only" })) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\CreateRole.cshtml	13	14
+         * TODO @using (Html.BeginForm("PostChatMessage", null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\Home.cshtml	66	22
+         * TODO using (Html.BeginForm("DeleteChatMessage", null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\Home.cshtml	96	33
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\Invite.cshtml	13	14
+         * TODO @using (Html.BeginForm("WithdrawInvite", null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\InviteDetails.cshtml	35	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\ReceivedInviteDetails.cshtml	9	14
+         * TODO @using (Html.BeginForm("DeleteRole", null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\RoleDetails.cshtml	14	18
+         * TODO @using (Html.BeginForm("ClearRole", null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\RoleDetails.cshtml	49	46
+         * TODO @using (Html.BeginForm()) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Alliance\SetRole.cshtml	13	14
+         * TODO @using (Html.BeginForm()) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Attack\Execute.cshtml	9	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new { data_success_message = "Your resources were stored in their banks." })) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Empire\Banking.cshtml	13	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Empire\Siege.cshtml	20	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new { data_success_message = "Your tax rate was changed." })) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Empire\Tax.cshtml	13	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Empire\Troops.cshtml	32	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Empire\Workers.cshtml	31	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new { data_success_message = $"Your {Model.Name} was upgraded." })) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Empire\_Building.cshtml	12	14
+         * TODO @using (Html.BeginForm()) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Home\ChangeEmail.cshtml	9	14
+         * TODO @using (Html.BeginForm()) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Home\ChangePassword.cshtml	9	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new { @class = "html-only" })) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Home\Deactivate.cshtml	9	14
+         * TODO @using (Html.BeginForm()) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Home\ForgotPassword.cshtml	9	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new { @class = "html-only" })) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Home\LogIn.cshtml	9	14
+         * TODO @using (Html.BeginForm()) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Home\Register.cshtml	9	14
+         * TODO @using (Html.BeginForm()) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Home\ResetPassword.cshtml	9	14
+         * TODO @using (Html.BeginForm()) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Home\SendActivation.cshtml	9	14
+         * TODO @using (Html.BeginForm("LogOut", "Home", FormMethod.Post, new { @class = "html-only d-inline" })) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Layout\_Menu.cshtml	113	42
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Market\Buy.cshtml	13	14
+         * TODO @using (Html.BeginForm(null, null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Market\Sell.cshtml	21	14
+         * TODO @using (Html.BeginForm("WithdrawCaravan", null, FormMethod.Post, new {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Market\_Caravans.cshtml	32	38
+         * TODO @using (Html.BeginForm()) {	C:\Users\Gebruiker\source\repos\WarOfEmpires\WarOfEmpires\Views\Message\Send.cshtml	13	14
+        */
     }
 }
+ 
