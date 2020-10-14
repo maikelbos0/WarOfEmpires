147426d6e4b0a75f5119575ed5d1880143792d4e
Maikel Bos <maikel.bos0@gmail.com>
2020-10-14 09:54:24 +0200

    Command result builder test definition

--- WarOfEmpires.Tests/ActionResults/CommandResultBuilderTests.cs
+++ WarOfEmpires.Tests/ActionResults/CommandResultBuilderTests.cs
@@ -4,7 +4,57 @@ namespace WarOfEmpires.Tests.ActionResults {
     [TestClass]
     public sealed class CommandResultBuilderTests {
         [TestMethod]
-        public void CommandResultBuilder_Tests() {
+        public void CommandResultBuilder_Requires_OnFailure_OnSuccess() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_Returns_OnFailure_For_ModelState_Error() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_Returns_OnFailure_For_Command_Error() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_Returns_OnSuccess_For_Succesful_Execution() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_Returns_OnSuccess_For_Warnings() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_Clears_ModelState_For_Success() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_OnSuccess_View_Works() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_OnFailure_View_Works() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_OnFailure_View_Model_Works() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_OnFailure_Model_Works() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CommandResultBuilder_ThrowOnFailure_Works() {
             throw new System.NotImplementedException();
         }
     }
