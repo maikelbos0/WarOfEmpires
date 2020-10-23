95aeec212d61b2c06ead7255b78f36f0ec6677f9
Maikel Bos <maikel.bos0@gmail.com>
2020-10-23 17:46:31 +0200

    Kick and leave command test definitions

--- WarOfEmpires.CommandHandlers.Tests/Alliances/KickFromAllianceCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/KickFromAllianceCommandHandlerTests.cs
@@ -4,7 +4,22 @@ namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
     [TestClass]
     public sealed class KickFromAllianceCommandHandlerTests {
         [TestMethod]
-        public void Todo() {
+        public void KickFromAllianceCommandHandler_Succeeds() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void KickFromAllianceCommandHandler_Fails_For_Member_Alliance_Leader() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void KickFromAllianceCommandHandler_Throws_Exception_For_Player_Not_In_Alliance() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void KickFromAllianceCommandHandler_Throws_Exception_For_Member_Not_In_Alliance() {
             throw new System.NotImplementedException();
         }
     }
--- WarOfEmpires.CommandHandlers.Tests/Alliances/LeaveAllianceCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/LeaveAllianceCommandHandlerTests.cs
@@ -4,7 +4,17 @@ namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
     [TestClass]
     public sealed class LeaveAllianceCommandHandlerTests {
         [TestMethod]
-        public void Todo() {
+        public void LeaveAllianceCommandHandler_Succeeds() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void LeaveAllianceCommandHandler_Fails_For_Alliance_Leader() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void LeaveAllianceCommandHandler_Throws_Exception_For_Player_Not_In_Alliance() {
             throw new System.NotImplementedException();
         }
     }
