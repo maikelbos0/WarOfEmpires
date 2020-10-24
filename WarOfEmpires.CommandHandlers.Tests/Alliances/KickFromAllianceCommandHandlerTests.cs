ca1aea5b50984d2015c962d468653b9fec696821
Maikel Bos <maikel.bos0@gmail.com>
2020-10-24 20:37:37 +0200

    Kick and leave test implementations

--- WarOfEmpires.CommandHandlers.Tests/Alliances/KickFromAllianceCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/KickFromAllianceCommandHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using System;
 using WarOfEmpires.CommandHandlers.Alliances;
 using WarOfEmpires.Commands.Alliances;
 using WarOfEmpires.Repositories.Alliances;
@@ -27,18 +28,53 @@ namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
         }
 
         [TestMethod]
-        public void KickFromAllianceCommandHandler_Fails_For_Member_Alliance_Leader() {
-            throw new System.NotImplementedException();
+        public void KickFromAllianceCommandHandler_Throws_Exception_For_Member_Alliance_Leader() {
+            var builder = new FakeBuilder()
+                .BuildAlliance(1)
+                .WithLeader(1);
+
+            var handler = new KickFromAllianceCommandHandler(new AllianceRepository(builder.Context));
+            var command = new KickFromAllianceCommand("test1@test.com", "1");
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<InvalidOperationException>();
+            builder.Alliance.DidNotReceiveWithAnyArgs().RemoveMember(default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void KickFromAllianceCommandHandler_Throws_Exception_For_Player_Not_In_Alliance() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .WithPlayer(1)
+                .BuildAlliance(1)
+                .WithMember(2);
+
+            var handler = new KickFromAllianceCommandHandler(new AllianceRepository(builder.Context));
+            var command = new KickFromAllianceCommand("test1@test.com", "2");
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<InvalidOperationException>();
+            builder.Alliance.DidNotReceiveWithAnyArgs().RemoveMember(default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void KickFromAllianceCommandHandler_Throws_Exception_For_Member_Not_In_Alliance() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .WithPlayer(1)
+                .BuildAlliance(1)
+                .WithMember(2);
+
+            var handler = new KickFromAllianceCommandHandler(new AllianceRepository(builder.Context));
+            var command = new KickFromAllianceCommand("test2@test.com", "1");
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<InvalidOperationException>();
+            builder.Alliance.DidNotReceiveWithAnyArgs().RemoveMember(default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
     }
 }
