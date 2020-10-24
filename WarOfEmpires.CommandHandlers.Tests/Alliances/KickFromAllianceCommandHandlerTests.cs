7ba314eb03beb63f7d381ed430e375523a000772
Maikel Bos <maikel.bos0@gmail.com>
2020-10-24 10:28:56 +0200

    First test implementation for kick

--- WarOfEmpires.CommandHandlers.Tests/Alliances/KickFromAllianceCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/KickFromAllianceCommandHandlerTests.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using WarOfEmpires.CommandHandlers.Alliances;
+using WarOfEmpires.Commands.Alliances;
+using WarOfEmpires.Repositories.Alliances;
+using WarOfEmpires.Test.Utilities;
 
 namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
     [TestClass]
     public sealed class KickFromAllianceCommandHandlerTests {
         [TestMethod]
         public void KickFromAllianceCommandHandler_Succeeds() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .BuildAlliance(1)
+                .WithLeader(1)
+                .WithMember(2, out var member);
+
+            var handler = new KickFromAllianceCommandHandler(new AllianceRepository(builder.Context));
+            var command = new KickFromAllianceCommand("test1@test.com", "2");
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+            builder.Alliance.Received().RemoveMember(member);
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
 
         [TestMethod]
