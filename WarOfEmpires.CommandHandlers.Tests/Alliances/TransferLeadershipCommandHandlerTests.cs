e722ed3fd9521cd0c9b7a803dbd2580b7bdff267
Maikel Bos <maikel.bos0@gmail.com>
2020-11-12 20:11:03 +0100

    Setup for new transfer leadership command

---
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/TransferLeadershipCommandHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
+    [TestClass]
+    public sealed class TransferLeadershipCommandHandlerTests {
+        [TestMethod]
+        public void TransferLeadershipCommandHandler_Succeeds() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TransferLeadershipCommandHandler_Throws_Exception_When_Not_In_Alliance() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TransferLeadershipCommandHandler_Throws_Exception_When_Not_Leader() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TransferLeadershipCommandHandler_Throws_Exception_For_Member_Current_Leader() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TransferLeadershipCommandHandler_Throws_Exception_For_Member_Not_In_Alliance () {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WarOfEmpires.CommandHandlers/Alliances/TransferLeadershipCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using WarOfEmpires.CommandHandlers.Decorators;
+using WarOfEmpires.Commands.Alliances;
+using WarOfEmpires.Repositories.Alliances;
+using WarOfEmpires.Utilities.Container;
+
+namespace WarOfEmpires.CommandHandlers.Alliances {
+    [InterfaceInjectable]
+    [Audit]
+    public sealed class TransferLeadershipCommandHandler : ICommandHandler<TransferLeadershipCommand> {
+        private readonly IAllianceRepository _repository;
+
+        public TransferLeadershipCommandHandler(IAllianceRepository repository) {
+            _repository = repository;
+        }
+
+        public CommandResult<TransferLeadershipCommand> Execute(TransferLeadershipCommand command) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
