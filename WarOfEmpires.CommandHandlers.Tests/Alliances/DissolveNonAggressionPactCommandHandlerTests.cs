5c0191d05dfe7821a8d7e4b4e8c287dc25c1e933
Maikel Bos <maikel.bos0@gmail.com>
2021-01-09 20:12:02 +0100

    Pact command handler test definitions

---
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/DissolveNonAggressionPactCommandHandlerTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
+    [TestClass]
+    public sealed class DissolveNonAggressionPactCommandHandlerTests {
+        [TestMethod]
+        public void DissolveNonAggressionPactCommandHandler_Succeeds() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/RejectNonAggressionPactRequestCommandHandlerTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
+    [TestClass]
+    public sealed class RejectNonAggressionPactRequestCommandHandlerTests {
+        [TestMethod]
+        public void RejectNonAggressionPactRequestCommandHandler_Succeeds() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
