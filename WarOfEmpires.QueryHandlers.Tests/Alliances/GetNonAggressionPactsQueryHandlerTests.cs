a1987650e9f936bc39cbb19133ad9440ce0c5879
Maikel Bos <maikel.bos0@gmail.com>
2021-01-10 12:00:52 +0100

    Pact query test definitions

---
+++ WarOfEmpires.QueryHandlers.Tests/Alliances/GetNonAggressionPactsQueryHandlerTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WarOfEmpires.Queries.Alliances {
+    [TestClass]
+    public sealed class GetNonAggressionPactsQueryHandlerTests {
+        [TestMethod]
+        public void GetNonAggressionPactsQueryHandler_Returns_Correct_Information() {
+            throw new System.NotImplementedException();
+
+        }
+    }
+}
---
+++ WarOfEmpires.QueryHandlers.Tests/Alliances/GetSentNonAggressionPactRequestsQueryHandlerTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WarOfEmpires.Queries.Alliances {
+    [TestClass]
+    public sealed class GetSentNonAggressionPactRequestsQueryHandlerTests {
+        [TestMethod]
+        public void GetSentNonAggressionPactRequestsQueryHandler_Returns_Correct_Information() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
