510332edf2e8a5d6488811c8d1412a6c9a7eb947
Maikel Bos <maikel.bos0@gmail.com>
2021-01-10 12:14:39 +0100

    Pact queries

---
+++ WarOfEmpires.Queries/Alliances/GetNonAggressionPactsQuery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WarOfEmpires.Models.Alliances;
+
+namespace WarOfEmpires.Queries.Alliances {
+    public sealed class GetNonAggressionPactsQuery : IQuery<IEnumerable<NonAggressionPactViewModel>> {
+        public string Email { get; set; }
+
+        public GetNonAggressionPactsQuery(string email) {
+            Email = email;
+        }
+    }
+}
---
+++ WarOfEmpires.Queries/Alliances/GetReceivedNonAggressionPactRequestsQuery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WarOfEmpires.Models.Alliances;
+
+namespace WarOfEmpires.Queries.Alliances {
+    public sealed class GetReceivedNonAggressionPactRequestsQuery : IQuery<IEnumerable<ReceivedNonAggressionPactRequestViewModel>> {
+        public string Email { get; set; }
+
+        public GetReceivedNonAggressionPactRequestsQuery(string email) {
+            Email = email;
+        }
+    }
+}
