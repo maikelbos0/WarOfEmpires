020ea8fc151f875091a754003660dc3c318eb741
Maikel Bos <maikel.bos0@gmail.com>
2021-01-11 19:20:44 +0100

    Pact query handler implementation

--- WarOfEmpires.QueryHandlers.Tests/Alliances/GetNonAggressionPactsQueryHandlerTests.cs
+++ WarOfEmpires.QueryHandlers.Tests/Alliances/GetNonAggressionPactsQueryHandlerTests.cs
@@ -32,6 +32,7 @@ namespace WarOfEmpires.QueryHandlers.Tests.Alliances {
                 .WithAlliance(2, out var firstAlliance, "TEST", "Pact test")
                 .WithAlliance(3, out var secondAlliance, "ASDF", "Another test")
                 .BuildAlliance(1)
+                .WithLeader(1)
                 .WithNonAggressionPact(2, secondAlliance)
                 .WithNonAggressionPact(1, firstAlliance);
 
@@ -41,7 +42,7 @@ namespace WarOfEmpires.QueryHandlers.Tests.Alliances {
             var result = handler.Execute(query);
 
             result.First().Id.Should().Be(1);
-            result.First().AllianceId.Should().Be(1);
+            result.First().AllianceId.Should().Be(2);
             result.First().Code.Should().Be("TEST");
             result.First().Name.Should().Be("Pact test");
         }
--- WarOfEmpires.QueryHandlers/Alliances/GetNonAggressionPactsQueryHandler.cs
+++ WarOfEmpires.QueryHandlers/Alliances/GetNonAggressionPactsQueryHandler.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using WarOfEmpires.Database;
 using WarOfEmpires.Models.Alliances;
 using WarOfEmpires.Queries.Alliances;
 using WarOfEmpires.QueryHandlers.Decorators;
 using WarOfEmpires.Utilities.Container;
+using WarOfEmpires.Utilities.Services;
 
 namespace WarOfEmpires.QueryHandlers.Alliances {
     [InterfaceInjectable]
@@ -16,7 +19,21 @@ namespace WarOfEmpires.QueryHandlers.Alliances {
         }
 
         public IEnumerable<NonAggressionPactViewModel> Execute(GetNonAggressionPactsQuery query) {
-            throw new System.NotImplementedException();
+            var alliance = _context.Players
+                .Include(p => p.Alliance.NonAggressionPacts.Select(n => n.Alliances))
+                .Single(p => EmailComparisonService.Equals(p.User.Email, query.Email))
+                .Alliance;
+
+            return alliance
+                .NonAggressionPacts
+                .OrderBy(p => p.Id)
+                .SelectMany(p => p.Alliances.Select(a => new NonAggressionPactViewModel() {
+                    Id = p.Id,
+                    AllianceId = a.Id,
+                    Code = a.Code,
+                    Name = a.Name
+                }))
+                .Where(p => p.AllianceId != alliance.Id);
         }
     }
 }
