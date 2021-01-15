1b908e4214f27ec8bdd376b08c56d167c1b7bed8
Maikel Bos <maikel.bos0@gmail.com>
2021-01-15 17:49:09 +0100

    Pact command implementation

--- WarOfEmpires.Commands/Alliances/DissolveNonAggressionPactCommand.cs
+++ WarOfEmpires.Commands/Alliances/DissolveNonAggressionPactCommand.cs
@@ -1,4 +1,11 @@
 ﻿namespace WarOfEmpires.Commands.Alliances {
     public sealed class DissolveNonAggressionPactCommand : ICommand {
+        public string Email { get; }
+        public int NonAggressionPactId { get; }
+
+        public DissolveNonAggressionPactCommand(string email, int nonAggressionPactId) {
+            Email = email;
+            NonAggressionPactId = nonAggressionPactId;
+        }
     }
 }
