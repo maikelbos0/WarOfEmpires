e0485436bf96252803b1463070427e3343423a31
Maikel Bos <maikel.bos0@gmail.com>
2021-01-17 17:40:51 +0100

    Send pact request test implementation

--- WarOfEmpires.CommandHandlers.Tests/Alliances/SendNonAggressionPactRequestCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/SendNonAggressionPactRequestCommandHandlerTests.cs
@@ -1,26 +1,84 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using WarOfEmpires.CommandHandlers.Alliances;
+using WarOfEmpires.Commands.Alliances;
+using WarOfEmpires.Repositories.Alliances;
+using WarOfEmpires.Test.Utilities;
 
 namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
     [TestClass]
     public sealed class SendNonAggressionPactRequestCommandHandlerTests {
         [TestMethod]
         public void SendNonAggressionPactRequestCommandHandler_Succeeds() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .WithAlliance(1, out var recipient)
+                .BuildAlliance(2)
+                .WithLeader(1);
+
+            var handler = new SendNonAggressionPactRequestCommandHandler(new AllianceRepository(builder.Context));
+            var command = new SendNonAggressionPactRequestCommand("test1@test.com", 1);
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+            builder.Alliance.Received().SendNonAggressionPactRequest(recipient);
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
 
         [TestMethod]
         public void SendNonAggressionPactRequestCommandHandler_Fails_For_Already_Sent() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .WithAlliance(1, out var recipient)
+                .BuildAlliance(2)
+                .WithLeader(1)
+                .WithNonAggressionPactRequestTo(1, recipient);
+
+            var handler = new SendNonAggressionPactRequestCommandHandler(new AllianceRepository(builder.Context));
+            var command = new SendNonAggressionPactRequestCommand("test1@test.com", 1);
+
+            var result = handler.Execute(command);
+
+            result.Should().HaveError("This alliance already has an outstanding non aggression pact request from your alliance");
+            builder.Alliance.DidNotReceiveWithAnyArgs().SendNonAggressionPactRequest(default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void SendNonAggressionPactRequestCommandHandler_Fails_For_Already_In_Pact() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .WithAlliance(1, out var recipient)
+                .BuildAlliance(2)
+                .WithLeader(1)
+                .WithNonAggressionPact(1, recipient);
+
+            var handler = new SendNonAggressionPactRequestCommandHandler(new AllianceRepository(builder.Context));
+            var command = new SendNonAggressionPactRequestCommand("test1@test.com", 1);
+
+            var result = handler.Execute(command);
+
+            result.Should().HaveError("Your alliance is already in a non aggression pact with this alliance");
+            builder.Alliance.DidNotReceiveWithAnyArgs().SendNonAggressionPactRequest(default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
 
         [TestMethod]
         public void SendNonAggressionPactRequestCommandHandler_Throws_Exception_For_Same_Alliance() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .WithAlliance(1, out var recipient)
+                .BuildAlliance(2)
+                .WithLeader(1)
+                .WithNonAggressionPact(1, recipient);
+
+            var handler = new SendNonAggressionPactRequestCommandHandler(new AllianceRepository(builder.Context));
+            var command = new SendNonAggressionPactRequestCommand("test1@test.com", 2);
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<InvalidOperationException>();
+            builder.Alliance.DidNotReceiveWithAnyArgs().SendNonAggressionPactRequest(default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
         }
     }
 }
