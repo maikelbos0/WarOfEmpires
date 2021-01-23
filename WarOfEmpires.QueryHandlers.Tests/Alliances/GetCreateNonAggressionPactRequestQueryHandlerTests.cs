e5df0da4b384a615b48164eb51b2f2fbee671a2a
Maikel Bos <maikel.bos0@gmail.com>
2021-01-23 14:43:10 +0100

    Create pact query handler implementation

--- WarOfEmpires.QueryHandlers.Tests/Alliances/GetCreateNonAggressionPactRequestQueryHandlerTests.cs
+++ WarOfEmpires.QueryHandlers.Tests/Alliances/GetCreateNonAggressionPactRequestQueryHandlerTests.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarOfEmpires.Queries.Alliances;
+using WarOfEmpires.QueryHandlers.Alliances;
+using WarOfEmpires.Test.Utilities;
 
 namespace WarOfEmpires.QueryHandlers.Tests.Alliances {
     [TestClass]
     public sealed class GetCreateNonAggressionPactRequestQueryHandlerTests {
         [TestMethod]
         public void GetCreateNonAggressionPactRequestQueryHandler_Returns_Correct_Information() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .BuildAlliance(1);
+
+            var handler = new GetCreateNonAggressionPactRequestQueryHandler(builder.Context);
+            var query = new GetCreateNonAggressionPactRequestQuery(1);
+
+            var result = handler.Execute(query);
+
+            result.AllianceId.Should().Be(1);
+            result.AllianceCode.Should().Be("FS");
+            result.AllianceName.Should().Be("Føroyskir Samgonga");
         }
     }
 }
--- WarOfEmpires.QueryHandlers/Alliances/GetCreateNonAggressionPactRequestQueryHandler.cs
+++ WarOfEmpires.QueryHandlers/Alliances/GetCreateNonAggressionPactRequestQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using WarOfEmpires.Database;
+﻿using System.Linq;
+using WarOfEmpires.Database;
 using WarOfEmpires.Models.Alliances;
 using WarOfEmpires.Queries.Alliances;
 using WarOfEmpires.QueryHandlers.Decorators;
@@ -8,14 +9,18 @@ namespace WarOfEmpires.QueryHandlers.Alliances {
     [InterfaceInjectable]
     [Audit]
     public sealed class GetCreateNonAggressionPactRequestQueryHandler : IQueryHandler<GetCreateNonAggressionPactRequestQuery, CreateNonAggressionPactRequestModel> {
-        private readonly IWarContext context;
+        private readonly IWarContext _context;
 
         public GetCreateNonAggressionPactRequestQueryHandler(IWarContext context) {
-            this.context = context;
+            _context = context;
         }
 
         public CreateNonAggressionPactRequestModel Execute(GetCreateNonAggressionPactRequestQuery query) {
-            throw new System.NotImplementedException();
+            return _context.Alliances.Select(a => new CreateNonAggressionPactRequestModel() {
+                AllianceId = a.Id,
+                AllianceCode = a.Code,
+                AllianceName = a.Name
+            }).Single(a => a.AllianceId == query.AllianceId);
         }
     }
 }
