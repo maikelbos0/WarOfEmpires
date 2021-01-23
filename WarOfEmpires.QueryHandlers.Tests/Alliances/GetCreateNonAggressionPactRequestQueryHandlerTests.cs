7e0840b235866b5424a2014f5d6a10e739378109
Maikel Bos <maikel.bos0@gmail.com>
2021-01-23 14:32:18 +0100

    Create pact request query setup

---
+++ WarOfEmpires.QueryHandlers.Tests/Alliances/GetCreateNonAggressionPactRequestQueryHandlerTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WarOfEmpires.QueryHandlers.Tests.Alliances {
+    [TestClass]
+    public sealed class GetCreateNonAggressionPactRequestQueryHandlerTests {
+        [TestMethod]
+        public void GetCreateNonAggressionPactRequestQueryHandler_Returns_Correct_Information() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WarOfEmpires.QueryHandlers/Alliances/GetCreateNonAggressionPactRequestQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using WarOfEmpires.Database;
+using WarOfEmpires.Models.Alliances;
+using WarOfEmpires.Queries.Alliances;
+using WarOfEmpires.QueryHandlers.Decorators;
+using WarOfEmpires.Utilities.Container;
+
+namespace WarOfEmpires.QueryHandlers.Alliances {
+    [InterfaceInjectable]
+    [Audit]
+    public sealed class GetCreateNonAggressionPactRequestQueryHandler : IQueryHandler<GetCreateNonAggressionPactRequestQuery, CreateNonAggressionPactRequestModel> {
+        private readonly IWarContext context;
+
+        public GetCreateNonAggressionPactRequestQueryHandler(IWarContext context) {
+            this.context = context;
+        }
+
+        public CreateNonAggressionPactRequestModel Execute(GetCreateNonAggressionPactRequestQuery query) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
