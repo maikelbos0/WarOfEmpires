46436daf31637554a6e76b5a240a3aa657a99f4a
Maikel Bos <maikel.bos0@gmail.com>
2021-02-13 15:39:44 +0100

    Database migration for new right

---
+++ WarOfEmpires.Database/Migrations/202102131437225_Roles-CanManageNonAggressionPacts.cs
@@ -0,0 +1,18 @@
+﻿namespace WarOfEmpires.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RolesCanManageNonAggressionPacts : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("Alliances.Roles", "CanManageNonAggressionPacts", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("Alliances.Roles", "CanManageNonAggressionPacts");
+        }
+    }
+}
