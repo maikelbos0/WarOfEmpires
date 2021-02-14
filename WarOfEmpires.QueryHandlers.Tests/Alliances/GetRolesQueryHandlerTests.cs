4a9ddebb8a4caf241a9a8e99b169041137297574
Maikel Bos <maikel.bos0@gmail.com>
2021-02-14 15:50:06 +0100

    Add pact right to roles grid view

--- WarOfEmpires.QueryHandlers.Tests/Alliances/GetRolesQueryHandlerTests.cs
+++ WarOfEmpires.QueryHandlers.Tests/Alliances/GetRolesQueryHandlerTests.cs
@@ -33,6 +33,7 @@ namespace WarOfEmpires.QueryHandlers.Tests.Alliances {
             result.Single(r => r.Id == 1).CanManageRoles.Should().BeTrue();
             result.Single(r => r.Id == 1).CanDeleteChatMessages.Should().BeTrue();
             result.Single(r => r.Id == 1).CanKickMembers.Should().BeTrue();
+            result.Single(r => r.Id == 1).CanManageNonAggressionPacts.Should().BeTrue();
             result.Single(r => r.Id == 1).Players.Should().Be(2);
         }
     }
