ebdc84c366bd8b480859bf28e0186dc941e78e25
Maikel Bos <maikel.bos0@gmail.com>
2021-02-15 19:59:34 +0100

    Added todo

--- WarOfEmpires.Models/Alliances/AllianceDetailsViewModel.cs
+++ WarOfEmpires.Models/Alliances/AllianceDetailsViewModel.cs
@@ -2,6 +2,8 @@
 
 namespace WarOfEmpires.Models.Alliances {
     public sealed class AllianceDetailsViewModel : EntityViewModel {
+        // TODO add can send pact request
+        // TODO decide if the current player rights are part of that
         public string Code { get; set; }
         public string Name { get; set; }
         public int LeaderId { get; set; }
