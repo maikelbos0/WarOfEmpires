6284f45bea19f656b8a1df9c9e080e5a99c78622
Maikel Bos <maikel.bos0@gmail.com>
2021-02-15 19:44:23 +0100

    Current alliance rights query pact right

--- WarOfEmpires.Models/Alliances/CurrentAllianceRightsViewModel.cs
+++ WarOfEmpires.Models/Alliances/CurrentAllianceRightsViewModel.cs
@@ -7,5 +7,6 @@
         public bool CanKickMembers { get; set; }
         public bool CanTransferLeadership { get; set; }
         public bool CanDisbandAlliance { get; set; }
+        public bool CanManageNonAggressionPacts { get; set; }
     }
 }
