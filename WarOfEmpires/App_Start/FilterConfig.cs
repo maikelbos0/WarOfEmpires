d5db96ce63c24fe6e95934f756e00f670e9bcf21
Maikel Bos <maikel.bos0@gmail.com>
2021-03-23 19:43:48 +0100

    Delete unused filter config

--- WarOfEmpires/App_Start/FilterConfig.cs
+++
@@ -1,8 +0,0 @@
-
-namespace WarOfEmpires.App_Start {
-    public static class FilterConfig {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
-
-        }
-    }
-}
