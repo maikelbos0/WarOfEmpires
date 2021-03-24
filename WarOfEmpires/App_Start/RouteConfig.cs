b846bb6c59117a5b57ed804cbfac568a9ea9e04c
Maikel Bos <maikel.bos0@gmail.com>
2021-03-24 19:59:32 +0100

    Remove nonfunctional route config

--- WarOfEmpires/App_Start/RouteConfig.cs
+++
@@ -1,9 +0,0 @@
-
-namespace WarOfEmpires.App_Start {
-    public static class RouteConfig {
-        public static void RegisterRoutes(RouteCollection routes) {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapMvcAttributeRoutes();
-        }
-    }
-}
--- WarOfEmpires/Global.asax.cs
+++ WarOfEmpires/Global.asax.cs
@@ -6,7 +6,6 @@ namespace WarOfEmpires {
     public class Global : System.Web.HttpApplication {
         protected void Application_Start(object sender, EventArgs e) {
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
