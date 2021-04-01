5413fb8195c643f4bab3c5dbb662d545eb329ad2
Maikel Bos <maikel.bos0@gmail.com>
2021-04-01 18:52:43 +0200

    Create all fake db sets

--- WarOfEmpires.Test.Utilities/FakeWarContext.cs
+++ WarOfEmpires.Test.Utilities/FakeWarContext.cs
@@ -10,11 +10,11 @@ namespace WarOfEmpires.Test.Utilities {
 
     public sealed class FakeWarContext : IWarContext {
         public DbSet<Security.User> Users { get; set; } = new FakeDbSet<Security.User>();
-        public DbSet<Auditing.CommandExecution> CommandExecutions { get; set; }
-        public DbSet<Auditing.QueryExecution> QueryExecutions { get; set; }
-        public DbSet<Alliances.Alliance> Alliances { get; set; }
-        public DbSet<Players.Player> Players { get; set; }
-        public DbSet<Events.ScheduledTask> ScheduledTasks { get; set; }
+        public DbSet<Auditing.CommandExecution> CommandExecutions { get; set; } = new FakeDbSet<Auditing.CommandExecution>();
+        public DbSet<Auditing.QueryExecution> QueryExecutions { get; set; } = new FakeDbSet<Auditing.QueryExecution>();
+        public DbSet<Alliances.Alliance> Alliances { get; set; } = new FakeDbSet<Alliances.Alliance>();
+        public DbSet<Players.Player> Players { get; set; } = new FakeDbSet<Players.Player>();
+        public DbSet<Events.ScheduledTask> ScheduledTasks { get; set; } = new FakeDbSet<Events.ScheduledTask>();
 
         public int CallsToSaveChanges { get; private set; }
 
