5d92855811523197381ecdf42e4137744b9516cd
Maikel Bos <maikel.bos0@gmail.com>
2021-04-05 19:41:58 +0200

    Clean up fluent assertions extensions

--- WarOfEmpires.CommandHandlers.Tests/FluentAssertionsExtensions.cs
+++ WarOfEmpires.CommandHandlers.Tests/FluentAssertionsExtensions.cs
@@ -20,43 +20,18 @@ namespace WarOfEmpires.CommandHandlers.Tests {
             Subject = instance;
         }
 
-        // TODO refactor to accept expression type to get rid of dependency on MVC
-        /* Ex:
-            var result = new CommandResult<Commands.Markets.SellResourcesCommand>();
-            result.AddError(c => c.Merchandise[0].Type, "test");
-
-            System.Linq.Expressions.Expression<System.Func<Commands.Markets.SellResourcesCommand, string>> expression = c => c.Merchandise[0].Type;
-
-            result.Errors[0].Expression.ToString().Should().Be(expression.ToString());
-        */
-
         public AndConstraint<CommandResultAssertions<TCommand>> HaveError(Expression<Func<TCommand, object>> expression, string message) {
             Execute.Assertion
                 .ForCondition(Subject.Errors.Count == 1)
                 .FailWith("Expected {context:CommandResult} to contain an error, but found {0} errors.", Subject.Errors.Count)
                 .Then
-                .ForCondition(Subject.Errors.Single().Expression.GetExpressionText() == expression.GetExpressionText())
+                .ForCondition(Subject.Errors.Single().Expression?.GetExpressionText() == expression?.GetExpressionText())
                 .FailWith("Expected {context:CommandResult} to contain an error with expression {0}, but found expression {1}.", expression, Subject.Errors.Single().Expression)
                 .Then
                 .ForCondition(Subject.Errors.Single().Message == message)
                 .FailWith("Expected {context:CommandResult} to contain an error with message {0}, but found message {1}.", message, Subject.Errors.Single().Message);
 
             return new AndConstraint<CommandResultAssertions<TCommand>>(this);
-        }
-
-        [Obsolete]
-        public AndConstraint<CommandResultAssertions<TCommand>> HaveError(string expression, string message) {
-            Execute.Assertion
-                .ForCondition(Subject.Errors.Count == 1)
-                .FailWith("Expected {context:CommandResult} to contain an error, but found {0} errors.", Subject.Errors.Count)
-                .Then
-                .ForCondition(GetExpressionText(Subject.Errors.Single()) == expression)
-                .FailWith("Expected {context:CommandResult} to contain an error with expression {0}, but found expression {1}.", expression, GetExpressionText(Subject.Errors.Single()))
-                .Then
-                .ForCondition(Subject.Errors.Single().Message == message)
-                .FailWith("Expected {context:CommandResult} to contain an error with message {0}, but found message {1}.", message, Subject.Errors.Single().Message);
-
-            return new AndConstraint<CommandResultAssertions<TCommand>> (this);
         }
 
         public AndConstraint<CommandResultAssertions<TCommand>> HaveWarning(string message) {
@@ -70,16 +45,8 @@ namespace WarOfEmpires.CommandHandlers.Tests {
             return new AndConstraint<CommandResultAssertions<TCommand>>(this);
         }
 
-        private string GetExpressionText(CommandError<TCommand> commandError) {
-            if (commandError.Expression == null) {
-                return null;
-            }
-
-            return null;
-        }
-
         public AndConstraint<CommandResultAssertions<TCommand>> HaveError(string message) {
-            return HaveError((string)null, message);
+            return HaveError(null, message);
         }
     }
 }
