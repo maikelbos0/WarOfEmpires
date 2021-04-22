b3682388521247f40b911938420c1c9ee6cfaf1c
Maikel Bos <maikel.bos0@gmail.com>
2021-04-22 18:11:19 +0200

    Disable authorization service resolving for now

--- WarOfEmpires/Views/WebViewPage.cs
+++ WarOfEmpires/Views/WebViewPage.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using Unity;
 using WarOfEmpires.Services;
 
 namespace WarOfEmpires.Views {
     // TODO determine if this is still the way to add custom services
     public abstract class WebViewPage<TModel> { //: System.Web.Mvc.WebViewPage<TModel> {
-        private Lazy<IAuthorizationService> _authorizationService = new Lazy<IAuthorizationService>(() => UnityConfig.Container.Resolve<IAuthorizationService>());
+        private Lazy<IAuthorizationService> _authorizationService; // = new Lazy<IAuthorizationService>(() => UnityConfig.Container.Resolve<IAuthorizationService>());
 
         public IAuthorizationService AuthorizationService => _authorizationService.Value;
     }
