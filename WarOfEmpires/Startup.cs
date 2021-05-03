ad39f2c324fc448c20e2e857d009ebc50f060d6b
Maikel Bos <maikel.bos0@gmail.com>
2021-05-03 11:47:34 +0200

    Add user secrets

--- WarOfEmpires/Startup.cs
+++ WarOfEmpires/Startup.cs
@@ -8,32 +8,31 @@ using WarOfEmpires.Utilities.Configuration;
 using WarOfEmpires.Utilities.DependencyInjection;
 
 namespace WarOfEmpires {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
+    public class Startup {
+        public Startup(IWebHostEnvironment env) {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddUserSecrets("c61bb83f-efb6-4b20-a8d9-eca92057923a");
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             // TODO should be AddMVC?
             services.AddControllersWithViews(ConfigureMvcOptions);
             services.AddHttpContextAccessor();
             services.AddServices(typeof(Startup).Assembly);
-
-            // TODO add user secret for SendGrid API key
             services.AddSingleton(Configuration.GetSection(AppSettings.Key).Get<AppSettings>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
@@ -44,16 +43,14 @@ namespace WarOfEmpires {
             app.UseAuthorization();
 
             // TODO test default controller and default action for non default controller
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
 
-        private void ConfigureMvcOptions(MvcOptions mvcOptions)
-        { 
+        private void ConfigureMvcOptions(MvcOptions mvcOptions) {
         }
     }
 }
