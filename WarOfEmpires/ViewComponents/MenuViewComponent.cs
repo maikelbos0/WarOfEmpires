7daeee052e11387b7d8b1bd8db204288db7b0874
Maikel Bos <maikel.bos0@gmail.com>
2021-05-10 19:19:29 +0200

    Move menu to viewcomponent

---
+++ WarOfEmpires/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarOfEmpires.Models.Players;
+using WarOfEmpires.Queries.Players;
+using WarOfEmpires.Services;
+
+namespace WarOfEmpires.ViewComponents {
+    public class MenuViewComponent : ViewComponent {
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IMessageService _messageService;
+
+        public MenuViewComponent(IAuthenticationService authenticationService, IMessageService messageService) {
+            _authenticationService = authenticationService;
+            _messageService = messageService;
+        }
+
+        public IViewComponentResult Invoke() {
+            if (_authenticationService.IsAuthenticated) {
+                return View(_messageService.Dispatch(new GetCurrentPlayerQuery(_authenticationService.Identity)));
+            }
+            else {
+                return View(new CurrentPlayerViewModel() {
+                    IsAuthenticated = false
+                });
+            }
+        }
+    }
+}
