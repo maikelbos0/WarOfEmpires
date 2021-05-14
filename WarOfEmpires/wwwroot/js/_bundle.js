9a6eeef7f346ee8e9ffefc4f267373ca4b74f982
Maikel Bos <maikel.bos0@gmail.com>
2021-05-14 09:31:01 +0200

    Use javascript to load partials async

--- WarOfEmpires/wwwroot/js/_bundle.js
+++ WarOfEmpires/wwwroot/js/_bundle.js
@@ -429,3 +429,26 @@ $(function () {
     ResourceManager.refresh();
     AjaxManager.onSuccess.push(ResourceManager.refresh);
 });
+$(function () {
+    $('.partial-content').each(function () {
+        var panel = $(this);
+        var url = panel.data('partial-url');
+
+        if (url) {
+            $.ajax({
+                url: url,
+                method: "GET",
+                cache: false,
+                success: function (result) {
+                    panel.html(result);
+                },
+                error: function () {
+                    toastr.error("An error occurred loading partial content; please refresh the page for accurate values.");
+                }
+            });
+        }
+        else {
+            toastr.error("An error occurred determining content url; please contact support to resolve this issue.");
+        }
+    });
+});
---
+++ WarOfEmpires/wwwroot/js/partial-manager.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    $('.partial-content').each(function () {
+        var panel = $(this);
+        var url = panel.data('partial-url');
+
+        if (url) {
+            $.ajax({
+                url: url,
+                method: "GET",
+                cache: false,
+                success: function (result) {
+                    panel.html(result);
+                },
+                error: function () {
+                    toastr.error("An error occurred loading partial content; please refresh the page for accurate values.");
+                }
+            });
+        }
+        else {
+            toastr.error("An error occurred determining content url; please contact support to resolve this issue.");
+        }
+    });
+});
