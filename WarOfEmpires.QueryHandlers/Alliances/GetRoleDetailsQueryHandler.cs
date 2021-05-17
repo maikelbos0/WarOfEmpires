069d5ebb86e0e51afe9df3b0e7f0b3f328cb1b38
Maikel Bos <maikel.bos0@gmail.com>
2021-05-17 14:18:17 +0200

    Fix includes

--- WarOfEmpires.QueryHandlers/Alliances/GetRoleDetailsQueryHandler.cs
+++ WarOfEmpires.QueryHandlers/Alliances/GetRoleDetailsQueryHandler.cs
@@ -20,7 +20,7 @@ namespace WarOfEmpires.QueryHandlers.Alliances {
         [Audit]
         public RoleDetailsViewModel Execute(GetRoleDetailsQuery query) {
             var role = _context.Players
-                .Include(p => p.Alliance.Roles.Select(r => r.Players.Select(rp => rp.User)))
+                .Include(p => p.Alliance.Roles).ThenInclude(r => r.Players).ThenInclude(rp => rp.User)
                 .Where(p => EmailComparisonService.Equals(p.User.Email, query.Email))
                 .SelectMany(p => p.Alliance.Roles)
                 .Single(r => r.Id == query.RoleId);
