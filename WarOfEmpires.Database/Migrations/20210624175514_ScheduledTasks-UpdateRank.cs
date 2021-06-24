843226b810aa8652314c5b46c216c1485cf2de5c
Maikel Bos <maikel.bos0@gmail.com>
2021-06-24 20:01:11 +0200

    Increase frequency of rank updates

---
+++ WarOfEmpires.Database/Migrations/20210624175514_ScheduledTasks-UpdateRank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfEmpires.Database.Migrations
+{
+    public partial class ScheduledTasksUpdateRank : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Events",
+                table: "ScheduledTasks",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Interval",
+                value: new TimeSpan(0, 0, 1, 0, 0));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Events",
+                table: "ScheduledTasks",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Interval",
+                value: new TimeSpan(0, 0, 2, 0, 0));
+        }
+    }
+}
--- WarOfEmpires.Database/WarContext.cs
+++ WarOfEmpires.Database/WarContext.cs
@@ -70,7 +70,7 @@ namespace WarOfEmpires.Database {
                 Events.ScheduledTask.Create<Empires.RecruitTaskTriggeredEvent>(1, new TimeSpan(1, 0, 0), Events.TaskExecutionMode.ExecuteAllIntervals),
                 Events.ScheduledTask.Create<Empires.TurnTaskTriggeredEvent>(2, new TimeSpan(0, 10, 0), Events.TaskExecutionMode.ExecuteAllIntervals),
                 Events.ScheduledTask.Create<Empires.BankTurnTaskTriggeredEvent>(3, new TimeSpan(4, 0, 0), Events.TaskExecutionMode.ExecuteAllIntervals),
-                Events.ScheduledTask.Create<Empires.UpdateRankTaskTriggeredEvent>(4, new TimeSpan(0, 2, 0), Events.TaskExecutionMode.ExecuteOnce)
+                Events.ScheduledTask.Create<Empires.UpdateRankTaskTriggeredEvent>(4, new TimeSpan(0, 1, 0), Events.TaskExecutionMode.ExecuteOnce)
             );
         }
 
