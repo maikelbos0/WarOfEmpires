0ab0bfa455e86dd6f493afc2a9e4131c6a27a5c1
Maikel Bos <maikel.bos0@gmail.com>
2021-06-26 09:54:22 +0200

    Add tests for victory status

--- WarOfEmpires.Models/Players/VictoryStatusViewModel.cs
+++ WarOfEmpires.Models/Players/VictoryStatusViewModel.cs
@@ -4,6 +4,6 @@ namespace WarOfEmpires.Models.Players {
     public sealed class VictoryStatusViewModel {
         public int? CurrentGrandOverlordId { get; set; }
         public string CurrentGrandOverlord { get; set; }
-        public TimeSpan GrandOverlordTime { get; set; }
+        public TimeSpan? GrandOverlordTime { get; set; }
     }
 }
--- WarOfEmpires.QueryHandlers.Tests/Players/GetVictoryStatusQueryHandlerTests.cs
+++ WarOfEmpires.QueryHandlers.Tests/Players/GetVictoryStatusQueryHandlerTests.cs
@@ -1,11 +1,61 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using WarOfEmpires.Domain.Players;
+using WarOfEmpires.Domain.Security;
+using WarOfEmpires.Queries.Players;
+using WarOfEmpires.QueryHandlers.Players;
+using WarOfEmpires.Test.Utilities;
 
 namespace WarOfEmpires.QueryHandlers.Tests.Players {
     [TestClass]
     public sealed class GetVictoryStatusQueryHandlerTests {
         [TestMethod]
-        public void Todo() {
-            throw new System.NotImplementedException();
+        public void GetVictoryStatusQueryHandler_Returns_No_GrandOverlord_By_Default() {
+            var builder = new FakeBuilder()
+                .WithPlayer(1);
+
+            var handler = new GetVictoryStatusQueryHandler(builder.Context);
+            var query = new GetVictoryStatusQuery();
+
+            var result = handler.Execute(query);
+
+            result.CurrentGrandOverlordId.Should().BeNull();
+            result.CurrentGrandOverlord.Should().BeNull();
+            result.GrandOverlordTime.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void GetVictoryStatusQueryHandler_Returns_GrandOverlord_If_Available() {
+            var builder = new FakeBuilder()
+                .BuildPlayer(1, displayName: "Winner", title: TitleType.GrandOverlord);
+
+            builder.Player.GrandOverlordTime.Returns(TimeSpan.FromMinutes(1234));
+
+            var handler = new GetVictoryStatusQueryHandler(builder.Context);
+            var query = new GetVictoryStatusQuery();
+
+            var result = handler.Execute(query);
+
+            result.CurrentGrandOverlordId.Should().Be(1);
+            result.CurrentGrandOverlord.Should().Be("Winner");
+            result.GrandOverlordTime.Should().Be(TimeSpan.FromMinutes(1234));
+        }
+
+        [TestMethod]
+        public void GetVictoryStatusQueryHandler_Returns_No_GrandOverlord_If_Inactive() {
+            var builder = new FakeBuilder()
+                .WithPlayer(1, title: TitleType.GrandOverlord, status: UserStatus.Inactive);
+
+            var handler = new GetVictoryStatusQueryHandler(builder.Context);
+            var query = new GetVictoryStatusQuery();
+
+            var result = handler.Execute(query);
+
+            result.CurrentGrandOverlordId.Should().BeNull();
+            result.CurrentGrandOverlord.Should().BeNull();
+            result.GrandOverlordTime.Should().BeNull();
         }
     }
 }
