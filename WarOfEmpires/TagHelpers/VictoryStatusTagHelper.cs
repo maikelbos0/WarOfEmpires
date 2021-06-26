dba52b4b94fac100dd211e15dd7f7729ff9571c3
Maikel Bos <maikel.bos0@gmail.com>
2021-06-26 13:10:05 +0200

    Add victory status to menu

---
+++ WarOfEmpires/TagHelpers/VictoryStatusTagHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace WarOfEmpires.TagHelpers {
+    public class VictoryStatusTagHelper : PartialTagHelper {
+        public VictoryStatusTagHelper(IUrlHelperFactory urlHelperFactory, IActionContextAccessor actionContextAccessor) {
+            Url = urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext).Action("_VictoryStatus", "Player");
+            AjaxRefresh = true;
+        }
+    }
+}
