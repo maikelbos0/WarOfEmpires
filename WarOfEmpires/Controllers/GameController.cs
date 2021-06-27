ba786529593c8a63736b1931838c238952fb3b7a
Maikel Bos <maikel.bos0@gmail.com>
2021-06-27 09:22:33 +0200

    Use game status on interface

---
+++ WarOfEmpires/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WarOfEmpires.Filters;
+using WarOfEmpires.Queries.Game;
+using WarOfEmpires.Services;
+
+namespace WarOfEmpires.Controllers {
+    [Authorize]
+    [UserOnline]
+    [Route("Game")]
+    public class GameController : BaseController {
+        public GameController(IAuthenticationService authenticationService, IMessageService messageService, IDataGridViewService dataGridViewService)
+            : base(messageService, authenticationService, dataGridViewService) {
+        }
+
+        [HttpGet("_GameStatus")]
+        public PartialViewResult _GameStatus() {
+            return PartialView(_messageService.Dispatch(new GetGameStatusQuery()));
+        }
+    }
+}
---
+++ WarOfEmpires/TagHelpers/GameStatusTagHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace WarOfEmpires.TagHelpers {
+    public class GameStatusTagHelper : PartialTagHelper {
+        public GameStatusTagHelper(IUrlHelperFactory urlHelperFactory, IActionContextAccessor actionContextAccessor) {
+            Url = urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext).Action("_GameStatus", "Game");
+            AjaxRefresh = true;
+        }
+    }
+}
