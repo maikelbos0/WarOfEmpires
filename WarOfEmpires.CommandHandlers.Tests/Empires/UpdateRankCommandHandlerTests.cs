a92984e41cea3ebe720ff3e44a88381f3dfbde74
Maikel Bos <maikel.bos0@gmail.com>
2021-06-27 09:38:47 +0200

    Set GO in game status

--- WarOfEmpires.CommandHandlers.Tests/Empires/UpdateRankCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Empires/UpdateRankCommandHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using System;
 using System.Collections.Generic;
 using WarOfEmpires.CommandHandlers.Empires;
 using WarOfEmpires.Commands.Empires;
@@ -8,6 +9,7 @@ using WarOfEmpires.Domain.Players;
 using WarOfEmpires.Domain.Security;
 using WarOfEmpires.Repositories.Players;
 using WarOfEmpires.Test.Utilities;
+using WarOfEmpires.Utilities.Game;
 
 namespace WarOfEmpires.CommandHandlers.Tests.Empires {
     [TestClass]
@@ -15,15 +17,16 @@ namespace WarOfEmpires.CommandHandlers.Tests.Empires {
         [TestMethod]
         public void UpdateRankCommandHandler_Calls_RankService_Update_For_Active_Players() {
             var rankService = Substitute.For<IRankService>();
+            var gameStatus = new GameStatus();
             var builder = new FakeBuilder()
-                .WithPlayer(1, out var player)
-                .WithPlayer(2, out var anotherPlayer)
-                .WithPlayer(3, out var inactivePlayer, status: UserStatus.Inactive)
-                .WithPlayer(4, out var newPlayer, status: UserStatus.New);
+                            .WithPlayer(1, out var player)
+                            .WithPlayer(2, out var anotherPlayer)
+                            .WithPlayer(3, out var inactivePlayer, status: UserStatus.Inactive)
+                            .WithPlayer(4, out var newPlayer, status: UserStatus.New);
 
-            var handler = new UpdateRankCommandHandler(new PlayerRepository(builder.Context), rankService);
+            var handler = new UpdateRankCommandHandler(new PlayerRepository(builder.Context), rankService, gameStatus);
             var command = new UpdateRankCommand();
-            
+
             handler.Execute(command);
             rankService.Received().Update(Arg.Do<IEnumerable<Player>>(players => {
                 players.Should().HaveCount(2);
@@ -31,6 +34,42 @@ namespace WarOfEmpires.CommandHandlers.Tests.Empires {
                 players.Should().Contain(anotherPlayer);
             }));
             builder.Context.CallsToSaveChanges.Should().Be(1);
+        }
+
+
+        [TestMethod]
+        public void UpdateRankCommandHandler_Sets_GrandOverlord_If_Available() {
+            var rankService = Substitute.For<IRankService>();
+            var gameStatus = new GameStatus();
+            var builder = new FakeBuilder()
+                            .WithPlayer(1, displayName: "The OG", title: TitleType.GrandOverlord, grandOverlordTime: TimeSpan.FromMinutes(1234));
+
+            var handler = new UpdateRankCommandHandler(new PlayerRepository(builder.Context), rankService, gameStatus);
+            var command = new UpdateRankCommand();
+
+            handler.Execute(command);
+
+            gameStatus.CurrentGrandOverlordId.Should().Be(1);
+            gameStatus.CurrentGrandOverlord.Should().Be("The OG");
+            gameStatus.CurrentGrandOverlordTime.Should().Be(TimeSpan.FromMinutes(1234));
+        }
+
+
+        [TestMethod]
+        public void UpdateRankCommandHandler_Clears_GrandOverlord_If_Not_Available() {
+            var rankService = Substitute.For<IRankService>();
+            var gameStatus = new GameStatus() { CurrentGrandOverlordId = 1, CurrentGrandOverlord = "The OG", CurrentGrandOverlordTime = TimeSpan.FromMinutes(1234) };
+            var builder = new FakeBuilder()
+                            .WithPlayer(1);
+
+            var handler = new UpdateRankCommandHandler(new PlayerRepository(builder.Context), rankService, gameStatus);
+            var command = new UpdateRankCommand();
+
+            handler.Execute(command);
+
+            gameStatus.CurrentGrandOverlordId.Should().BeNull();
+            gameStatus.CurrentGrandOverlord.Should().BeNull();
+            gameStatus.CurrentGrandOverlordTime.Should().BeNull();
         }
     }
 }
--- WarOfEmpires.CommandHandlers/Empires/UpdateRankCommandHandler.cs
+++ WarOfEmpires.CommandHandlers/Empires/UpdateRankCommandHandler.cs
@@ -1,26 +1,46 @@
-﻿using VDT.Core.DependencyInjection;
+﻿using System.Linq;
+using VDT.Core.DependencyInjection;
 using WarOfEmpires.CommandHandlers.Decorators;
 using WarOfEmpires.Commands.Empires;
 using WarOfEmpires.Domain.Players;
 using WarOfEmpires.Repositories.Players;
+using WarOfEmpires.Utilities.Game;
 
 namespace WarOfEmpires.CommandHandlers.Empires {
     [TransientServiceImplementation(typeof(ICommandHandler<UpdateRankCommand>))]
     public sealed class UpdateRankCommandHandler : ICommandHandler<UpdateRankCommand> {
         private readonly IPlayerRepository _repository;
         private readonly IRankService _rankService;
+        private readonly IGameStatus _gameStatus;
 
-        public UpdateRankCommandHandler(IPlayerRepository repository, IRankService rankService) {
+        public UpdateRankCommandHandler(IPlayerRepository repository, IRankService rankService, IGameStatus gameStatus) {
             _repository = repository;
             _rankService = rankService;
+            _gameStatus = gameStatus;
         }
 
         [Audit]
         public CommandResult<UpdateRankCommand> Execute(UpdateRankCommand command) {
             var result = new CommandResult<UpdateRankCommand>();
+            var players = _repository.GetAll();
 
-            _rankService.Update(_repository.GetAll());
+            _rankService.Update(players);
             _repository.SaveChanges();
+
+            var grandOverlord = players.SingleOrDefault(p => p.Title == TitleType.GrandOverlord);
+
+            if (grandOverlord == null) {
+                _gameStatus.CurrentGrandOverlordId = null;
+                _gameStatus.CurrentGrandOverlord = null;
+                _gameStatus.CurrentGrandOverlordTime = null;
+            }
+            else {
+                _gameStatus.CurrentGrandOverlordId = grandOverlord.Id;
+                _gameStatus.CurrentGrandOverlord = grandOverlord.DisplayName;
+                _gameStatus.CurrentGrandOverlordTime = grandOverlord.GrandOverlordTime;
+            }
+
+            // TODO create and dispatch event for truce here if game won
 
             return result;
         }
