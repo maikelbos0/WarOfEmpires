5a3d6c26c2e74465f3ce4b50416d1e464851ec46
Maikel Bos <maikel.bos0@gmail.com>
2021-06-28 14:23:21 +0200

    Add game phase admin tool

--- WarOfEmpires/Controllers/AdministrationController.cs
+++ WarOfEmpires/Controllers/AdministrationController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WarOfEmpires.Commands.Events;
+using WarOfEmpires.Commands.Game;
 using WarOfEmpires.Filters;
+using WarOfEmpires.Models.Game;
 using WarOfEmpires.Queries.Events;
+using WarOfEmpires.Queries.Game;
 using WarOfEmpires.Services;
 
 namespace WarOfEmpires.Controllers {
@@ -38,6 +41,18 @@ namespace WarOfEmpires.Controllers {
         public PartialViewResult _PauseScheduledTasks() {
             return BuildPartialViewResultFor(new PauseScheduledTasksCommand())
                 .OnSuccess(_ScheduledTasks)
+                .ThrowOnFailure();
+        }
+
+        [HttpGet("_GamePhase")]
+        public PartialViewResult _GamePhase() {
+            return PartialView(_messageService.Dispatch(new GetGamePhaseQuery()));
+        }
+
+        [HttpPost("_GamePhase")]
+        public PartialViewResult _GamePhase(GamePhaseModel model) {
+            return BuildPartialViewResultFor(new SetGamePhaseCommand(model.Phase))
+                .OnSuccess(_GamePhase)
                 .ThrowOnFailure();
         }
     }
