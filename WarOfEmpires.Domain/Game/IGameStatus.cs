ee5b1ed85fc68bb838c2a4eb340fdf15c78ad4a0
Maikel Bos <maikel.bos0@gmail.com>
2021-06-29 19:57:34 +0200

    Delete unused interface

--- WarOfEmpires.Domain/Game/IGameStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿using WarOfEmpires.Domain.Players;
-
-namespace WarOfEmpires.Domain.Game {
-    public interface IGameStatus {
-        Player CurrentGrandOverlord { get; set; }
-        GamePhase Phase { get; set; }
-    }
-}
