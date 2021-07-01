497b289de60964efee642933e5ab65269aa1e8a3
Maikel Bos <maikel.bos0@gmail.com>
2021-07-01 15:59:50 +0200

    Add new attack type to database

---
+++ WarOfEmpires.Database/Migrations/20210701135119_AttackType-GrandOverlord.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfEmpires.Database.Migrations
+{
+    public partial class AttackTypeGrandOverlord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "Attacks",
+                table: "AttackTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Grand overlord attack" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Attacks",
+                table: "AttackTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
