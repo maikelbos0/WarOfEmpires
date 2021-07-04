747de346fbca167b28a2e42aa93969cef9fed5a9
Maikel Bos <maikel.bos0@gmail.com>
2021-07-04 11:07:34 +0200

    Add Attacks has war damage property to database

---
+++ WarOfEmpires.Database/Migrations/20210704085023_Attacks-HasWarDamage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfEmpires.Database.Migrations
+{
+    public partial class AttacksHasWarDamage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasWarDamage",
+                schema: "Attacks",
+                table: "Attacks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasWarDamage",
+                schema: "Attacks",
+                table: "Attacks");
+        }
+    }
+}
