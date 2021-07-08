1049a4818a12aa4e9a527cd7a51d8b2fc6dbcee1
Maikel Bos <maikel.bos0@gmail.com>
2021-07-08 20:21:18 +0200

    Add HasNewChatMessages to database

---
+++ WarOfEmpires.Database/Migrations/20210708181740_Players-HasNewChatMessages.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfEmpires.Database.Migrations
+{
+    public partial class PlayersHasNewChatMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasNewChatMessages",
+                schema: "Players",
+                table: "Players",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasNewChatMessages",
+                schema: "Players",
+                table: "Players");
+        }
+    }
+}
