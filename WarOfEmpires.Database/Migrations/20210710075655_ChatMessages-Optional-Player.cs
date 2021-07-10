1bdd8ce13a8899c6144b00f0fbb356b19cf564f0
Maikel Bos <maikel.bos0@gmail.com>
2021-07-10 10:08:24 +0200

    Allow chat messages without player to be saved

---
+++ WarOfEmpires.Database/Migrations/20210710075655_ChatMessages-Optional-Player.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfEmpires.Database.Migrations
+{
+    public partial class ChatMessagesOptionalPlayer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PlayerId",
+                schema: "Alliances",
+                table: "ChatMessages",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PlayerId",
+                schema: "Alliances",
+                table: "ChatMessages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
