03114a220c45b2b89777053fbe9276f6aa5b7096
Maikel Bos <maikel.bos0@gmail.com>
2021-07-21 20:51:11 +0200

    Implement users query for admin

--- WarOfEmpires.QueryHandlers.Tests/Security/GetUsersQueryHandlerTests.cs
+++ WarOfEmpires.QueryHandlers.Tests/Security/GetUsersQueryHandlerTests.cs
@@ -1,21 +1,69 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Linq;
+using WarOfEmpires.Domain.Security;
+using WarOfEmpires.Queries.Security;
+using WarOfEmpires.QueryHandlers.Security;
+using WarOfEmpires.Test.Utilities;
+using WarOfEmpires.Utilities.Formatting;
 
 namespace WarOfEmpires.QueryHandlers.Tests.Security {
     [TestClass]
     public sealed class GetUsersQueryHandlerTests {
         [TestMethod]
         public void GetUsersQueryHandler_Returns_All_Users() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .WithPlayer(1, status: UserStatus.New)
+                .WithPlayer(2, status: UserStatus.Inactive)
+                .WithPlayer(3, status: UserStatus.Active);
+
+            var handler = new GetUsersQueryHandler(builder.Context, new EnumFormatter());
+            var query = new GetUsersQuery(null);
+
+            var result = handler.Execute(query);
+
+            result.Should().HaveCount(3);
         }
 
         [TestMethod]
         public void GetUsersQueryHandler_Returns_Correct_Information() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .BuildAlliance(1)
+                .BuildMember(2, lastOnline: new DateTime(2021, 2, 3, 14, 30, 45));
+
+            builder.User.IsAdmin.Returns(true);
+
+            var handler = new GetUsersQueryHandler(builder.Context, new EnumFormatter());
+            var query = new GetUsersQuery(null);
+
+            var result = handler.Execute(query);
+
+            result.Should().HaveCount(1);
+            result.Single().Id.Should().Be(2);
+            result.Single().Email.Should().Be("test2@test.com");
+            result.Single().DisplayName.Should().Be("Test display name 2");
+            result.Single().AllianceCode.Should().Be("FS");
+            result.Single().AllianceName.Should().Be("Føroyskir Samgonga");
+            result.Single().Status.Should().Be("Active");
+            result.Single().IsAdmin.Should().BeTrue();
+            result.Single().LastOnline.Should().Be(new DateTime(2021, 2, 3, 14, 30, 45));
         }
 
         [TestMethod]
         public void GetUsersQueryHandler_Searches() {
-            throw new System.NotImplementedException();
+            var builder = new FakeBuilder()
+                .WithPlayer(1)
+                .WithPlayer(2, displayName: "Wrong");
+
+            var handler = new GetUsersQueryHandler(builder.Context, new EnumFormatter());
+            var query = new GetUsersQuery("Test");
+
+            var result = handler.Execute(query);
+
+            result.Should().HaveCount(1);
+            result.Single().DisplayName.Should().Be("Test display name 1");
         }
     }
 }
--- WarOfEmpires.QueryHandlers/Security/GetUsersQueryHandler.cs
+++ WarOfEmpires.QueryHandlers/Security/GetUsersQueryHandler.cs
@@ -1,22 +1,53 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using VDT.Core.DependencyInjection;
 using WarOfEmpires.Database;
 using WarOfEmpires.Models.Security;
 using WarOfEmpires.Queries.Security;
 using WarOfEmpires.QueryHandlers.Decorators;
+using WarOfEmpires.Utilities.Formatting;
 
 namespace WarOfEmpires.QueryHandlers.Security {
     [TransientServiceImplementation(typeof(IQueryHandler<GetUsersQuery, IEnumerable<UserViewModel>>))]
     public sealed class GetUsersQueryHandler : IQueryHandler<GetUsersQuery, IEnumerable<UserViewModel>> {
         private readonly IWarContext _context;
+        private readonly IEnumFormatter _formatter;
 
-        public GetUsersQueryHandler(IWarContext context) {
+        public GetUsersQueryHandler(IWarContext context, IEnumFormatter formatter) {
             _context = context;
+            _formatter = formatter;
         }
 
         [Audit]
         public IEnumerable<UserViewModel> Execute(GetUsersQuery query) {
-            throw new System.NotImplementedException();
+            var players = _context.Players.AsQueryable();
+
+            if (!string.IsNullOrEmpty(query.DisplayName)) {
+                players = players.Where(p => p.DisplayName.Contains(query.DisplayName));
+            }
+
+            // Materialize before setting the status
+            return players
+                .Select(p => new {
+                    p.Id,
+                    p.User.Email,
+                    p.DisplayName,
+                    p.Alliance,
+                    p.User.Status,
+                    p.User.IsAdmin,
+                    p.User.LastOnline
+                })
+                .ToList()
+                .Select(u => new UserViewModel() {
+                    Id = u.Id,
+                    Email = u.Email,
+                    DisplayName = u.DisplayName,
+                    AllianceCode = u.Alliance?.Code,
+                    AllianceName = u.Alliance?.Name,
+                    Status = _formatter.ToString(u.Status),
+                    IsAdmin = u.IsAdmin,
+                    LastOnline = u.LastOnline
+                });
         }
     }
 }
