ae91839e26901c952d35b9037835d02dcaefb0b9
Maikel Bos <maikel.bos0@gmail.com>
2021-07-25 14:32:34 +0200

    Update user command handler with test setup

---
+++ WarOfEmpires.CommandHandlers.Tests/Security/UpdateUserDetailsCommandHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarOfEmpires.Domain.Security;
+
+namespace WarOfEmpires.CommandHandlers.Tests.Security {
+    [TestClass]
+    public sealed class UpdateUserDetailsCommandHandlerTests {
+        [TestMethod]
+        public void UpdateUserDetailsCommandHandler_Succeeds() {
+            throw new System.NotImplementedException();
+        }
+
+        [DataTestMethod]
+        [DataRow("New", UserStatus.New, DisplayName = "New")]
+        [DataRow("Active", UserStatus.Active, DisplayName = "Active")]
+        [DataRow("Inactive", UserStatus.Inactive, DisplayName = "Inactive")]
+        public void UpdateUserDetailsCommandHandler_Resolves_Status_To_Correct_Status(string status, UserStatus expectedStatus) {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void UpdateUserDetailsCommandHandler_Fails_For_Existing_Email() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void UpdateUserDetailsCommandHandler_Throws_Exception_For_Invalid_User() {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void UpdateUserDetailsCommandHandler_Throws_Exception_For_Nonexistent_Type() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ WarOfEmpires.CommandHandlers/Security/UpdateUserDetailsCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using VDT.Core.DependencyInjection;
+using WarOfEmpires.CommandHandlers.Decorators;
+using WarOfEmpires.Commands.Security;
+using WarOfEmpires.Repositories.Security;
+
+namespace WarOfEmpires.CommandHandlers.Security {
+    [TransientServiceImplementation(typeof(ICommandHandler<UpdateUserDetailsCommand>))]
+    public sealed class UpdateUserDetailsCommandHandler : ICommandHandler<UpdateUserDetailsCommand> {
+        private readonly IUserRepository repository;
+
+        public UpdateUserDetailsCommandHandler(IUserRepository repository) {
+            this.repository = repository;
+        }
+
+        [Audit]
+        public CommandResult<UpdateUserDetailsCommand> Execute(UpdateUserDetailsCommand command) {
+            throw new NotImplementedException();
+        }
+    }
+}
