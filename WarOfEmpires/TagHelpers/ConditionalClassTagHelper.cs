8dbd69c25c48d6be2512feb1548c7a35b75cf2ff
Maikel Bos <maikel.bos0@gmail.com>
2021-07-31 18:11:45 +0200

    Add tag helper for conditional style

---
+++ WarOfEmpires/TagHelpers/ConditionalClassTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WarOfEmpires.TagHelpers {
+    [HtmlTargetElement("*")]
+    public class ConditionalClassTagHelper : TagHelper {
+        public bool Condition { get; set; }
+        public string ClassName { get; set; }
+        public string NegativeClassName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output) {
+            var className = Condition ? ClassName : NegativeClassName;
+
+            if (!string.IsNullOrWhiteSpace(className)) {
+                if (output.Attributes.TryGetAttribute("class", out var attribute)) {
+                    output.Attributes.SetAttribute("class", $"{attribute.Value} {className}");
+                }
+                else {
+                    output.Attributes.SetAttribute("class", className);
+                }
+            }
+        }
+    }
+}
