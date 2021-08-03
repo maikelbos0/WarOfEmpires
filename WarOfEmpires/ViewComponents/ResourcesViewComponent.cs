02f542430c1ed6a018cbcb7fbfe570f823ed7ab7
Maikel Bos <maikel.bos0@gmail.com>
2021-08-03 18:44:06 +0200

    Use view component for resources

---
+++ WarOfEmpires/ViewComponents/ResourcesViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarOfEmpires.Models.Empires;
+
+namespace WarOfEmpires.ViewComponents {
+    public class ResourcesViewComponent : ViewComponent {
+        public IViewComponentResult Invoke(ResourcesViewModel model) {
+            return View(model);
+        }
+    }
+}
