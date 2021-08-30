96a33ffe1bebb2994b966010ec68b3e3e4eaee2a
Maikel Bos <maikel.bos0@gmail.com>
2021-08-30 19:37:55 +0200

    Add reset command to interface

--- WarOfEmpires/Controllers/AdministrationController.cs
+++ WarOfEmpires/Controllers/AdministrationController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using WarOfEmpires.Commands.Events;
 using WarOfEmpires.Commands.Game;
+using WarOfEmpires.Commands.Players;
 using WarOfEmpires.Commands.Security;
 using WarOfEmpires.Filters;
 using WarOfEmpires.Models.Game;
@@ -82,6 +83,19 @@ namespace WarOfEmpires.Controllers {
             return BuildViewResultFor(new UpdateUserDetailsCommand(model.Id, model.Email, model.DisplayName, model.AllianceCode, model.AllianceName, model.Status, model.IsAdmin))
                 .OnSuccess(nameof(Users))
                 .OnFailure(model);
+        }
+
+        [HttpGet(nameof(_ResetPlayers))]
+        public PartialViewResult _ResetPlayers() {
+            return PartialView();
+        }
+
+
+        [HttpPost(nameof(_ResetPlayers))]
+        public ActionResult _ResetPlayersPost() {
+            return BuildPartialViewResultFor(new ResetPlayersCommand())
+                .OnSuccess(nameof(_ResetPlayers))
+                .ThrowOnFailure();
         }
     }
 }
