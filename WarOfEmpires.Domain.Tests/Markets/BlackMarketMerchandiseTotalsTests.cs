016851889ccce9fd26ce721be2a07eb49c4d12f5
Maikel Bos <maikel.bos0@gmail.com>
2021-09-02 19:27:26 +0200

    Add merchandise totals for black market

--- WarOfEmpires.Domain.Tests/Markets/BlackMarketMerchandiseTotalsTests.cs
+++ WarOfEmpires.Domain.Tests/Markets/BlackMarketMerchandiseTotalsTests.cs
@@ -6,23 +6,20 @@ using WarOfEmpires.Domain.Markets;
 
 namespace WarOfEmpires.Domain.Tests.Markets {
     [TestClass]
-    public sealed class MerchandiseTotalsTests {
+    public sealed class BlackMarketMerchandiseTotalsTests {
         [TestMethod]
-        public void MerchandiseTotals_Constructor_Succeeds() {
-            var totals = new MerchandiseTotals(MerchandiseType.Wood, 50000, 15);
+        public void BlackMarketMerchandiseTotals_Constructor_Succeeds() {
+            var totals = new BlackMarketMerchandiseTotals(MerchandiseType.Wood, 50000);
 
             totals.Type.Should().Be(MerchandiseType.Wood);
             totals.Quantity.Should().Be(50000);
-            totals.Price.Should().Be(15);
         }
 
         [DataTestMethod]
-        [DataRow(-1, 1, DisplayName = "Negative quantity")]
-        [DataRow(0, 1, DisplayName = "Zero quantity")]
-        [DataRow(1, -1, DisplayName = "Negative price")]
-        [DataRow(1, 0, DisplayName = "Zero price")]
-        public void MerchandiseTotals_Constructor_Throws_Exception_For_Negative_Values(int quantity, int price) {
-            Action action = () => new MerchandiseTotals(MerchandiseType.Wood, quantity, price);
+        [DataRow(-1, DisplayName = "Negative quantity")]
+        [DataRow(0, DisplayName = "Zero quantity")]
+        public void BlackMarketMerchandiseTotals_Constructor_Throws_Exception_For_Negative_Values(int quantity) {
+            Action action = () => new BlackMarketMerchandiseTotals(MerchandiseType.Wood, quantity);
 
             action.Should().Throw<ArgumentOutOfRangeException>();
         }
@@ -32,8 +29,8 @@ namespace WarOfEmpires.Domain.Tests.Markets {
         [DataRow(MerchandiseType.Wood, 1, 0, 0, 1, 0, 0, DisplayName = "Wood")]
         [DataRow(MerchandiseType.Stone, 1, 0, 0, 0, 1, 0, DisplayName = "Stone")]
         [DataRow(MerchandiseType.Ore, 1, 0, 0, 0, 0, 1, DisplayName = "Ore")]
-        public void MerchandiseTotals_ToResources_Succeeds(MerchandiseType type, int quantity, int expectedGold, int expectedFood, int expectedWood, int expectedStone, int expectedOre) {
-            var totals = new MerchandiseTotals(type, quantity, 5);
+        public void BlackMarketMerchandiseTotals_ToResources_Succeeds(MerchandiseType type, int quantity, int expectedGold, int expectedFood, int expectedWood, int expectedStone, int expectedOre) {
+            var totals = new BlackMarketMerchandiseTotals(type, quantity);
 
             totals.ToResources().Should().Be(new Resources(expectedGold, expectedFood, expectedWood, expectedStone, expectedOre));
         }
---
+++ WarOfEmpires.Domain/Markets/BlackMarketMerchandiseTotals.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using WarOfEmpires.Domain.Common;
+
+namespace WarOfEmpires.Domain.Markets {
+    public sealed class BlackMarketMerchandiseTotals : ValueObject {
+        public static Resources ToResources(MerchandiseType type, int quantity) {
+            switch (type) {
+                case MerchandiseType.Food:
+                    return new Resources(food: quantity);
+                case MerchandiseType.Wood:
+                    return new Resources(wood: quantity);
+                case MerchandiseType.Stone:
+                    return new Resources(stone: quantity);
+                case MerchandiseType.Ore:
+                    return new Resources(ore: quantity);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public MerchandiseType Type { get; private set; }
+        public int Quantity { get; private set; }
+        public int Price { get; private set; }
+
+        public BlackMarketMerchandiseTotals(MerchandiseType type, int quantity) {
+            if (quantity <= 0) throw new ArgumentOutOfRangeException("quantity", "Negative values and zero are not allowed");
+
+            Type = type;
+            Quantity = quantity;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents() {
+            yield return Type;
+            yield return Quantity;
+        }
+
+        public Resources ToResources() {
+            return ToResources(Type, Quantity);
+        }
+    }
+}
