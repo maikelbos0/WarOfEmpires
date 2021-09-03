624750666f5e8c00d261768e9c4e9019d586cd14
Maikel Bos <maikel.bos0@gmail.com>
2021-09-03 20:11:45 +0200

    Add sell to black market command handler

---
+++ WarOfEmpires.CommandHandlers.Tests/Markets/SellBlackMarketResourcesCommandHandlerTests.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using WarOfEmpires.CommandHandlers.Markets;
+using WarOfEmpires.Commands.Markets;
+using WarOfEmpires.Domain.Markets;
+using WarOfEmpires.Repositories.Players;
+using WarOfEmpires.Test.Utilities;
+using WarOfEmpires.Utilities.Formatting;
+
+namespace WarOfEmpires.CommandHandlers.Tests.Markets {
+    [TestClass]
+    public sealed class SellBlackMarketResourcesCommandHandlerTests {
+        [TestMethod]
+        public void SellBlackMarketResourcesCommandHandler_Succeeds() {
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new SellBlackMarketResourcesCommandHandler(new PlayerRepository(builder.Context), new EnumFormatter());
+            var command = new SellBlackMarketResourcesCommand("test1@test.com", new List<BlackMarketMerchandiseInfo>() {
+                new BlackMarketMerchandiseInfo("Food", 2000),
+                new BlackMarketMerchandiseInfo("Wood", 16000)
+            });
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+
+            builder.Player.Received().SellResourcesToBlackMarket(new BlackMarketMerchandiseTotals(MerchandiseType.Food, 2000));
+            builder.Player.Received().SellResourcesToBlackMarket(new BlackMarketMerchandiseTotals(MerchandiseType.Wood, 16000));
+            builder.Context.CallsToSaveChanges.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void SellBlackMarketResourcesCommandHandler_Allows_Empty_Values() {
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new SellBlackMarketResourcesCommandHandler(new PlayerRepository(builder.Context), new EnumFormatter());
+            var command = new SellBlackMarketResourcesCommand("test1@test.com", new List<BlackMarketMerchandiseInfo>() {
+                new BlackMarketMerchandiseInfo("Wood", null)
+            });
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+            builder.Player.DidNotReceiveWithAnyArgs().SellResourcesToBlackMarket(default);
+        }
+
+        [TestMethod]
+        public void SellBlackMarketResourcesCommandHandler_Throws_Exception_For_Invalid_Type() {
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            var handler = new SellBlackMarketResourcesCommandHandler(new PlayerRepository(builder.Context), new EnumFormatter());
+            var command = new SellBlackMarketResourcesCommand("test1@test.com", new List<BlackMarketMerchandiseInfo>() {
+                new BlackMarketMerchandiseInfo("Test", 1)
+            });
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<ArgumentException>();
+            builder.Player.DidNotReceiveWithAnyArgs().SellResourcesToBlackMarket(default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SellBlackMarketResourcesCommandHandler_Fails_For_Too_Little_Resources() {
+            var builder = new FakeBuilder()
+                .BuildPlayer(1, canAffordAnything: false);
+
+            var handler = new SellBlackMarketResourcesCommandHandler(new PlayerRepository(builder.Context), new EnumFormatter());
+            var command = new SellBlackMarketResourcesCommand("test1@test.com", new List<BlackMarketMerchandiseInfo>() {
+                new BlackMarketMerchandiseInfo("Wood", 6000)
+            });
+
+            var result = handler.Execute(command);
+
+            result.AddError(c => c.Merchandise[0].Quantity, $"You don't have enough wood available to sell that much");
+            builder.Player.DidNotReceiveWithAnyArgs().SellResourcesToBlackMarket(default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
+        }
+    }
+}
---
+++ WarOfEmpires.CommandHandlers/Markets/SellBlackMarketResourcesCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using VDT.Core.DependencyInjection;
+using WarOfEmpires.CommandHandlers.Decorators;
+using WarOfEmpires.Commands.Markets;
+using WarOfEmpires.Domain.Markets;
+using WarOfEmpires.Repositories.Players;
+using WarOfEmpires.Utilities.Formatting;
+
+namespace WarOfEmpires.CommandHandlers.Markets {
+    [TransientServiceImplementation(typeof(ICommandHandler<SellBlackMarketResourcesCommand>))]
+    public sealed class SellBlackMarketResourcesCommandHandler : ICommandHandler<SellBlackMarketResourcesCommand> {
+        private readonly IPlayerRepository _repository;
+        private readonly IEnumFormatter _formatter;
+
+        public SellBlackMarketResourcesCommandHandler(IPlayerRepository repository, IEnumFormatter formatter) {
+            _repository = repository;
+            _formatter = formatter;
+        }
+
+        [Audit]
+        public CommandResult<SellBlackMarketResourcesCommand> Execute(SellBlackMarketResourcesCommand command) {
+            var result = new CommandResult<SellBlackMarketResourcesCommand>();
+            var merchandiseTotals = new List<BlackMarketMerchandiseTotals>();
+            var player = _repository.Get(command.Email);
+
+            for (var index = 0; index < command.Merchandise.Count; index++) {
+                var i = index; // Don't use iterator in lambdas
+                var type = (MerchandiseType)Enum.Parse(typeof(MerchandiseType), command.Merchandise[i].Type);
+
+                if (command.Merchandise[i].Quantity.HasValue && !player.CanAfford(MerchandiseTotals.ToResources(type, command.Merchandise[i].Quantity.Value))) {
+                    result.AddError(c => c.Merchandise[i].Quantity, $"You don't have enough {_formatter.ToString(type, false)} available to sell that much");
+                }
+
+                if (result.Success && command.Merchandise[i].Quantity.HasValue) {
+                    merchandiseTotals.Add(new BlackMarketMerchandiseTotals(type, command.Merchandise[i].Quantity.Value));
+                }
+            }
+
+            if (result.Success) {
+                foreach (var totals in merchandiseTotals) {
+                    player.SellResourcesToBlackMarket(totals);
+                }
+
+                _repository.SaveChanges();
+            }
+
+            return result;
+        }
+    }
+}
