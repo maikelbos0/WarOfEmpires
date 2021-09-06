d0d7debc8fdb0c5142c01daa46185eb8af590eb2
Maikel Bos <maikel.bos0@gmail.com>
2021-09-06 12:16:55 +0200

    Add new university types to database

---
+++ WarOfEmpires.Database/Migrations/20210906101003_University-Scientists.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfEmpires.Database.Migrations
+{
+    public partial class UniversityScientists : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "Empires",
+                table: "BuildingTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 20, "University" });
+
+            migrationBuilder.InsertData(
+                schema: "Empires",
+                table: "WorkerTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 7, "Scientists" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Empires",
+                table: "BuildingTypes",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                schema: "Empires",
+                table: "WorkerTypes",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}
