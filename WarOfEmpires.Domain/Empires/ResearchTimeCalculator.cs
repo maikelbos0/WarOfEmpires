12743a9d4692c75b35c998f40dd63f104bea7109
Maikel Bos <maikel.bos0@gmail.com>
2021-09-08 20:27:26 +0200

    Add first attempt at needed research time calculation

---
+++ WarOfEmpires.Domain/Empires/ResearchTimeCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WarOfEmpires.Domain.Empires {
+    public static class ResearchTimeCalculator {
+        private const long ResearchTimePerCompletedResearch = 5000;
+        private const long BaseResearchTime = 5000;
+
+        public static long GetResearchTime(int completedResearchCount, int completedResearchLevelForType) {
+            return ResearchTimePerCompletedResearch * completedResearchCount
+                + BaseResearchTime * (long)(Math.Pow(3, completedResearchLevelForType) + 0.5);
+        }
+    }
+}
