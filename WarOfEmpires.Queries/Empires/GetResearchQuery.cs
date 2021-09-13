accbc44a7c80eda5e3ad88d34435e0aa137fbc96
Maikel Bos <maikel.bos0@gmail.com>
2021-09-13 16:48:51 +0200

    Add research query setup

---
+++ WarOfEmpires.Queries/Empires/GetResearchQuery.cs
@@ -0,0 +1,11 @@
+﻿using WarOfEmpires.Models.Empires;
+
+namespace WarOfEmpires.Queries.Empires {
+    public sealed class GetResearchQuery : IQuery<ResearchModel> {
+        public string Email { get; }
+
+        public GetResearchQuery(string email) {
+            Email = email;
+        }
+    }
+}
