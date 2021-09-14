1dc9df4863dbdaaeebbb90d25964e0ec4c10bb43
Maikel Bos <maikel.bos0@gmail.com>
2021-09-14 09:32:37 +0200

    Add queued research tests to research query handler

--- WarOfEmpires.Models/Empires/QueuedResearchViewModel.cs
+++ WarOfEmpires.Models/Empires/QueuedResearchViewModel.cs
@@ -1,7 +1,9 @@
 ﻿namespace WarOfEmpires.Models.Empires {
     public sealed class QueuedResearchViewModel : EntityViewModel {
-        public int Priority { get; set; }
         public string Type { get; set; }
         public string Name { get; set; }
+        public int Priority { get; set; }
+        public int CompletedResearchTime { get; set; }
+        public int NeededResearchTime { get; set; }
     }
 }
