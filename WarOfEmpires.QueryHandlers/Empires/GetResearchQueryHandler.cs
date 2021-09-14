b24b4e963c938f5dba30b690f1d009302982de2b
Maikel Bos <maikel.bos0@gmail.com>
2021-09-14 13:54:25 +0200

    Split research query part 2: select single research

--- WarOfEmpires.QueryHandlers/Empires/GetResearchQueryHandler.cs
+++ WarOfEmpires.QueryHandlers/Empires/GetResearchQueryHandler.cs
@@ -11,8 +11,8 @@ using WarOfEmpires.Utilities.Formatting;
 using WarOfEmpires.Utilities.Services;
 
 namespace WarOfEmpires.QueryHandlers.Empires {
-    [TransientServiceImplementation(typeof(IQueryHandler<GetResearchQuery, ResearchModel>))]
-    public sealed class GetResearchQueryHandler : IQueryHandler<GetResearchQuery, ResearchModel> {
+    [TransientServiceImplementation(typeof(IQueryHandler<GetResearchQuery, ResearchViewModel>))]
+    public sealed class GetResearchQueryHandler : IQueryHandler<GetResearchQuery, ResearchViewModel> {
         private readonly IWarContext _context;
         private readonly IEnumFormatter _formatter;
 
@@ -22,37 +22,21 @@ namespace WarOfEmpires.QueryHandlers.Empires {
         }
 
         [Audit]
-        public ResearchModel Execute(GetResearchQuery query) {
-            var player = _context.Players
+        public ResearchViewModel Execute(GetResearchQuery query) {
+            var researchType = (ResearchType)Enum.Parse(typeof(ResearchType), query.ResearchType);
+            var research = _context.Players
                 .Include(p => p.Research)
-                .Include(p => p.QueuedResearch)
-                .Single(p => EmailComparisonService.Equals(p.User.Email, query.Email));
-            var completedResearch = player.Research.Sum(r => r.Level);
-            var queuedResearch = player.QueuedResearch
-                .OrderBy(r => r.Priority)
-                .Select((r, i) => new { 
-                    QueuedResearch = r,
-                    Index = i
-                })
-                .ToList();
+                .Where(p => EmailComparisonService.Equals(p.User.Email, query.Email))
+                .SelectMany(p => p.Research)
+                .SingleOrDefault(r => r.Type == researchType);
 
-            return new ResearchModel() {
-                Research = Enum.GetValues<ResearchType>().Select(t => new ResearchViewModel() {
-                    Type = t.ToString(),
-                    Name = _formatter.ToString(t),
-                    Description = ResearchDefinitionFactory.Get(t).Description,
-                    Level = player.Research.SingleOrDefault(r => r.Type == t)?.Level ?? 0,
-                    LevelBonus = ResearchDefinitionFactory.Get(t).LevelBonus,
-                    CurrentBonus = (player.Research.SingleOrDefault(r => r.Type == t)?.Level ?? 0) * ResearchDefinitionFactory.Get(t).LevelBonus
-                }).ToList(),
-                QueuedResearch = queuedResearch.Select(r => new QueuedResearchViewModel() {
-                    Id = r.QueuedResearch.Id,
-                    Type = r.QueuedResearch.Type.ToString(),
-                    Name = _formatter.ToString(r.QueuedResearch.Type),
-                    Priority = r.QueuedResearch.Priority,
-                    CompletedResearchTime = r.QueuedResearch.CompletedResearchTime,
-                    NeededResearchTime = ResearchTimeCalculator.GetResearchTime(completedResearch + r.Index, (player.Research.SingleOrDefault(s => s.Type == r.QueuedResearch.Type)?.Level ?? 0) + queuedResearch.Count(q => q.Index < r.Index && q.QueuedResearch.Type == r.QueuedResearch.Type))
-                }).ToList()
+            return new ResearchViewModel() {
+                ResearchType = researchType.ToString(),
+                Name = _formatter.ToString(researchType),
+                Description = ResearchDefinitionFactory.Get(researchType).Description,
+                Level = research?.Level ?? 0,
+                LevelBonus = ResearchDefinitionFactory.Get(researchType).LevelBonus,
+                CurrentBonus = (research?.Level ?? 0) * ResearchDefinitionFactory.Get(researchType).LevelBonus
             };
         }
     }
