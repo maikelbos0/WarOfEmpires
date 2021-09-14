250b94e867f6fa20790dfd58d79b273f640132fa
Maikel Bos <maikel.bos0@gmail.com>
2021-09-14 09:32:37 +0200

    Add queued research tests to research query handler

--- WarOfEmpires.QueryHandlers.Tests/Empires/GetResearchQueryHandlerTests.cs
+++ WarOfEmpires.QueryHandlers.Tests/Empires/GetResearchQueryHandlerTests.cs
@@ -44,11 +44,41 @@ namespace WarOfEmpires.QueryHandlers.Tests.Empires {
         public void GetResearchQueryHandler_Returns_Correct_QueuedResearch() {
             var builder = new FakeBuilder()
                 .BuildPlayer(1)
-                .WithQueuedResearch(4, ResearchType.CombatMedicine, 3, 0)
-                .WithQueuedResearch(2, ResearchType.CombatMedicine, 1, completedResearchTime: 1500)
+                .WithResearch(1, ResearchType.CombatMedicine, 2)
+                .WithQueuedResearch(2, ResearchType.CombatMedicine, 3, 0)
+                .WithQueuedResearch(4, ResearchType.CombatMedicine, 1, completedResearchTime: 1500)
                 .WithQueuedResearch(3, ResearchType.Commerce, 2, completedResearchTime: 1000);
 
-            throw new NotImplementedException();
+            var handler = new GetResearchQueryHandler(builder.Context, new EnumFormatter());
+            var query = new GetResearchQuery("test1@test.com");
+
+            var result = handler.Execute(query);
+
+            result.QueuedResearch.Should().HaveCount(3);
+
+            var combatMedicine1 = result.QueuedResearch.Should().ContainSingle(r => r.Id == 4).Subject;
+
+            combatMedicine1.Type.Should().Be("CombatMedicine");
+            combatMedicine1.Name.Should().Be("Combat medicine");
+            combatMedicine1.Priority.Should().Be(1);
+            combatMedicine1.CompletedResearchTime.Should().Be(1500);
+            combatMedicine1.NeededResearchTime.Should().Be(55000);
+
+            var commerce = result.QueuedResearch.Should().ContainSingle(r => r.Id == 3).Subject;
+
+            commerce.Type.Should().Be("Commerce");
+            commerce.Name.Should().Be("Commerce");
+            commerce.Priority.Should().Be(2);
+            commerce.CompletedResearchTime.Should().Be(0);
+            commerce.NeededResearchTime.Should().Be(20000);
+
+            var combatMedicine2 = result.QueuedResearch.Should().ContainSingle(r => r.Id == 2).Subject;
+
+            combatMedicine2.Type.Should().Be("CombatMedicine");
+            combatMedicine2.Name.Should().Be("Combat medicine");
+            combatMedicine2.Priority.Should().Be(3);
+            combatMedicine2.CompletedResearchTime.Should().Be(0);
+            combatMedicine2.NeededResearchTime.Should().Be(155000);
         }
     }
 }
