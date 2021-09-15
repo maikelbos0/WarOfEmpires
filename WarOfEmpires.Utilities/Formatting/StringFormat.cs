086a954a673c171a8772a3efd98e09fde4d14f9b
Maikel Bos <maikel.bos0@gmail.com>
2021-09-15 17:05:32 +0200

    Add research view

--- WarOfEmpires.Utilities/Formatting/StringFormat.cs
+++ WarOfEmpires.Utilities/Formatting/StringFormat.cs
@@ -2,5 +2,6 @@
     public static class StringFormat {
         public const string Date = "yyyy-MM-dd HH:mm";
         public const string Integer = "N0";
+        public const string Percentage = "P0";
     }
 }
