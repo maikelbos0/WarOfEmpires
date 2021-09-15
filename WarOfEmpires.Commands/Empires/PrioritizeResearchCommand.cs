989cdecddbffc43bb57a8ac2dba02057b3439608
Maikel Bos <maikel.bos0@gmail.com>
2021-09-15 19:09:46 +0200

    Add prioritize research command

---
+++ WarOfEmpires.Commands/Empires/PrioritizeResearchCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace WarOfEmpires.Commands.Empires {
+    public sealed class PrioritizeResearchCommand : ICommand {
+        public string Email { get; }
+        public string ResearchType { get; }
+
+        public PrioritizeResearchCommand(string email, string researchType) {
+            Email = email;
+            ResearchType = researchType;
+        }
+    }
+}
