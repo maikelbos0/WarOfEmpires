4fd4d937938e4b9f29ea5df5f053438459205f5e
Maikel Bos <maikel.bos0@gmail.com>
2021-09-17 20:42:52 +0200

    Add more detail about queued research

--- WarOfEmpires.Models/Empires/QueuedResearchViewModel.cs
+++ WarOfEmpires.Models/Empires/QueuedResearchViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace WarOfEmpires.Models.Empires {
+﻿using System;
+
+namespace WarOfEmpires.Models.Empires {
     public sealed class QueuedResearchViewModel : EntityViewModel {
         public string Type { get; set; }
         public string Name { get; set; }
         public int Priority { get; set; }
+        public long ResearchTime { get; set; }
         public long CompletedResearchTime { get; set; }
         public long NeededResearchTime { get; set; }
+        public TimeSpan NeededTime { get; set; }
     }
 }
