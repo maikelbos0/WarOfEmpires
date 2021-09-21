93159ddb91798dae84f8fcc71f35f5d746a1a335
Maikel Bos <maikel.bos0@gmail.com>
2021-09-21 17:42:08 +0200

    Add query for transfer of resources

---
+++ WarOfEmpires.Models/Alliances/TransferResourcesRecipientViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WarOfEmpires.Models.Alliances {
+    public sealed class TransferResourcesRecipientViewModel : EntityViewModel {
+        public int Rank { get; set; }
+        public string DisplayName { get; set; }
+        public string Role { get; set; }
+        public DateTime? LastOnline { get; set; }
+    }
+}
---
+++ WarOfEmpires.Queries/Alliances/GetTransferResourcesQuery.cs
@@ -0,0 +1,11 @@
+﻿using WarOfEmpires.Models.Alliances;
+
+namespace WarOfEmpires.Queries.Alliances {
+    public sealed class GetTransferResourcesQuery : IQuery<TransferResourcesModel> {
+        public string Email { get; set; }
+
+        public GetTransferResourcesQuery(string email) {
+            Email = email;
+        }
+    }
+}
