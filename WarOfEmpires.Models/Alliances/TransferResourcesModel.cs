d4462e935a1bd308f43487512c92c89d6b4fd607
Maikel Bos <maikel.bos0@gmail.com>
2021-09-21 20:33:08 +0200

    Fix model property types and validation

--- WarOfEmpires.Models/Alliances/TransferResourcesModel.cs
+++ WarOfEmpires.Models/Alliances/TransferResourcesModel.cs
@@ -6,28 +6,28 @@ namespace WarOfEmpires.Models.Alliances {
     public sealed class TransferResourcesModel {
         [DisplayName("Recipient")]
         [Required(ErrorMessage = "Recipient is required")]
-        public string RecipientId { get; set; }
+        public int? RecipientId { get; set; }
 
         public List<TransferResourcesRecipientViewModel> Recipients { get; set; }
 
         [DisplayName("Gold")]
-        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Invalid number")]
+        [RegularExpression("^\\d{0,9}$", ErrorMessage = "Invalid number")]
         public int? Gold { get; set; }
 
         [DisplayName("Food")]
-        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Invalid number")]
+        [RegularExpression("^\\d{0,9}$", ErrorMessage = "Invalid number")]
         public int? Food { get; set; }
 
         [DisplayName("Wood")]
-        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Invalid number")]
+        [RegularExpression("^\\d{0,9}$", ErrorMessage = "Invalid number")]
         public int? Wood { get; set; }
 
         [DisplayName("Stone")]
-        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Invalid number")]
+        [RegularExpression("^\\d{0,9}$", ErrorMessage = "Invalid number")]
         public int? Stone { get; set; }
 
         [DisplayName("Ore")]
-        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Invalid number")]
+        [RegularExpression("^\\d{0,9}$", ErrorMessage = "Invalid number")]
         public int? Ore { get; set; }
     }
 }
--- WarOfEmpires.Models/Markets/BlackMarketMerchandiseModel.cs
+++ WarOfEmpires.Models/Markets/BlackMarketMerchandiseModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace WarOfEmpires.Models.Markets {
     public sealed class BlackMarketMerchandiseModel {
         [DisplayName("Quantity")]
-        [RegularExpression("^\\d{0,8}$", ErrorMessage = "Invalid number")]
+        [RegularExpression("^\\d{0,9}$", ErrorMessage = "Invalid number")]
         public int? Quantity { get; set; }
         public string Type { get; set; }
         public string Name { get; set; }
