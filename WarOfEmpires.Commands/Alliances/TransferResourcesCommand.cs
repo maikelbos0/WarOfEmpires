8adafed1d46e4b0e0421b5eb7a9cc193994d1c70
Maikel Bos <maikel.bos0@gmail.com>
2021-09-22 17:26:08 +0200

    Add action and command to handle transfer resources

---
+++ WarOfEmpires.Commands/Alliances/TransferResourcesCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace WarOfEmpires.Commands.Alliances {
+    public sealed class TransferResourcesCommand : ICommand {
+        public string Email { get; }
+        public int RecipientId { get; }
+        public int? Gold { get; }
+        public int? Food { get; }
+        public int? Wood { get; }
+        public int? Stone { get; }
+        public int? Ore { get; }
+
+        public TransferResourcesCommand(string email, int recipientId, int? gold, int? food, int? wood, int? stone, int? ore) {
+            Email = email;
+            RecipientId = recipientId;
+            Gold = gold;
+            Food = food;
+            Wood = Wood;
+            Stone = stone;
+            Ore = ore;
+        }
+    }
+}
