e571068dcf9ce4fa4d49017dcaac082cc75dc7df
Maikel Bos <maikel.bos0@gmail.com>
2021-09-23 18:13:14 +0200

    Add ratio calculation to rank service

--- WarOfEmpires.Domain/Players/RankService.cs
+++ WarOfEmpires.Domain/Players/RankService.cs
@@ -12,7 +12,34 @@ namespace WarOfEmpires.Domain.Players {
 
         private readonly List<TitleDefinition> _titles = TitleDefinitionFactory.GetAll().OrderByDescending(t => t.Type).ToList();
 
-        public double GetPoints(Player player) {
+        public double GetRatio(Player dividendPlayer, Player divisorPlayer) {
+            var dividend = GetPoints(dividendPlayer);
+            var divisor = GetPoints(divisorPlayer);
+
+            if (dividend == 0 && divisor == 0) {
+                return 1;
+            }
+            
+            if (divisor == 0) {
+                return double.PositiveInfinity;
+            }
+
+            return GetPoints(dividendPlayer) / divisor;
+        }
+
+        public virtual void Update(IEnumerable<Player> players) {
+            var rank = 1;
+
+            foreach (var player in players
+                .Select(p => new { Player = p, RankPoints = GetPoints(p) })
+                .OrderByDescending(p => p.RankPoints)
+                .Select(p => p.Player)) {
+
+                player.UpdateRank(rank, GetTitle(player, rank++));
+            }
+        }
+
+        public virtual double GetPoints(Player player) {
             var troops = player.Troops.Select(t => player.GetTroopInfo(t.Type)).Sum(t => t.GetTotalAttack() + t.GetTotalDefense());
             var workers = player.Workers.Sum(w => w.Count);
             var defences = player.GetBuildingBonus(BuildingType.Defences);
@@ -26,18 +53,6 @@ namespace WarOfEmpires.Domain.Players {
             return _titles.First(title => title.RequiredDefenceLevel <= player.GetBuildingBonus(BuildingType.Defences)
                 && title.RequiredSoldiers <= player.Troops.Sum(t => t.Soldiers)
                 && title.RequiredRank >= newRank).Type;
-        }
-
-        public virtual void Update(IEnumerable<Player> players) {
-            var rank = 1;
-
-            foreach (var player in players
-                .Select(p => new { Player = p, RankPoints = GetPoints(p) })
-                .OrderByDescending(p => p.RankPoints)
-                .Select(p => p.Player)) {
-
-                player.UpdateRank(rank, GetTitle(player, rank++));
-            }
         }
     }
 }
