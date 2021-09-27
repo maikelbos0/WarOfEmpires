ebe9dc6ed64c5c25396d5173511227f9e97150a1
Maikel Bos <maikel.bos0@gmail.com>
2021-09-27 17:58:04 +0200

    Add profile to database

---
+++ WarOfEmpires.Database/Migrations/20210927155100_Player-Profiles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfEmpires.Database.Migrations
+{
+    public partial class PlayerProfiles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                schema: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AvatarLocation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profiles_Players_Id",
+                        column: x => x.Id,
+                        principalSchema: "Players",
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profiles",
+                schema: "Players");
+        }
+    }
+}
