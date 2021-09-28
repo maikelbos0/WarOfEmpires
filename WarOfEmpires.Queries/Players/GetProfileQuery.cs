161755c051393af4ed5464be1e3de76019326821
Maikel Bos <maikel.bos0@gmail.com>
2021-09-28 20:42:12 +0200

    Edit profile page

---
+++ WarOfEmpires.Queries/Players/GetProfileQuery.cs
@@ -0,0 +1,11 @@
+﻿using WarOfEmpires.Models.Players;
+
+namespace WarOfEmpires.Queries.Players {
+    public sealed class GetProfileQuery : IQuery<ProfileModel> {
+        public string Email { get; }
+
+        public GetProfileQuery(string email) {
+            Email = email;
+        }
+    }
+}
