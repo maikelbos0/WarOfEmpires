dd567c174a72c6ed94bb28b51df4683aaeed420d
Maikel Bos <maikel.bos0@gmail.com>
2021-09-29 17:30:59 +0200

    Get player profile query handler implementation

---
+++ WarOfEmpires.QueryHandlers.Tests/Players/GetProfileQueryHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarOfEmpires.Queries.Players;
+using WarOfEmpires.QueryHandlers.Players;
+using WarOfEmpires.Test.Utilities;
+using WarOfEmpires.Utilities.Configuration;
+
+namespace WarOfEmpires.QueryHandlers.Tests.Players {
+    [TestClass]
+    public sealed class GetProfileQueryHandlerTests {
+        [TestMethod]
+        public void GetPlayerDetailsQueryHandler_Returns_Correct_Information() {
+            var builder = new FakeBuilder()
+                .BuildPlayer(1)
+                .WithProfile(1, "A name", "Description", "1.jpeg");
+            var appSettings = new AppSettings() {
+                UserImageBaseUrl = "https://localhost/user"
+            };
+
+            var handler = new GetProfileQueryHandler(builder.Context, appSettings);
+            var query = new GetProfileQuery("test1@test.com");
+
+            var result = handler.Execute(query);
+
+            result.FullName.Should().Be("A name");
+            result.Description.Should().Be("Description");
+            result.AvatarLocation.Should().Be("https://localhost/user/1.jpeg");
+        }
+    }
+}
---
+++ WarOfEmpires.QueryHandlers/Players/GetProfileQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using VDT.Core.DependencyInjection;
+using WarOfEmpires.Database;
+using WarOfEmpires.Models.Players;
+using WarOfEmpires.Queries.Players;
+using WarOfEmpires.QueryHandlers.Decorators;
+using WarOfEmpires.Utilities.Configuration;
+using WarOfEmpires.Utilities.Services;
+
+namespace WarOfEmpires.QueryHandlers.Players {
+    [TransientServiceImplementation(typeof(IQueryHandler<GetProfileQuery, ProfileModel>))]
+    public sealed class GetProfileQueryHandler : IQueryHandler<GetProfileQuery, ProfileModel> {
+        private readonly IWarContext _context;
+        private readonly AppSettings _appSettings;
+
+        public GetProfileQueryHandler(IWarContext context, AppSettings appSettings) {
+            _context = context;
+            _appSettings = appSettings;
+        }
+
+        [Audit]
+        public ProfileModel Execute(GetProfileQuery query) {
+            return _context.Players
+                .Where(p => EmailComparisonService.Equals(p.User.Email, query.Email))
+                .Select(p => new ProfileModel() {
+                    FullName = p.Profile.FullName,
+                    Description = p.Profile.Description,
+                    AvatarLocation = string.IsNullOrWhiteSpace(p.Profile.AvatarLocation) ? null : $"{_appSettings.UserImageBaseUrl.TrimEnd('/')}/{p.Profile.AvatarLocation}"
+                })
+                .Single();
+        }
+    }
+}
