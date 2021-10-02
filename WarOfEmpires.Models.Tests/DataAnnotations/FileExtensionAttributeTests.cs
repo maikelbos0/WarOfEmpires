7bd3f0fe446ed0ba1d506cc698cb6bca5c1ebdcc
Maikel Bos <maikel.bos0@gmail.com>
2021-10-02 10:04:23 +0200

    Add file extension validation

---
+++ WarOfEmpires.Models.Tests/DataAnnotations/FileExtensionAttributeTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using WarOfEmpires.Models.DataAnnotations;
+
+namespace WarOfEmpires.Models.Tests.DataAnnotations {
+    [TestClass]
+    public sealed class FileExtensionAttributeTests {
+        [DataTestMethod]
+        [DataRow(new[] { ".jpg" }, "test.jpg", true)]
+        [DataRow(new[] { ".jpeg", ".jpg", ".gif" }, "test.jpg", true)]
+        [DataRow(new[] { ".jpeg", ".jpg", ".gif" }, "test.png", false)]
+        public void FileExtensionAttribute_IsValid_Succeeds(string[] extensions, string fileName, bool expectedIsValid) {
+            var attribute = new FileExtensionAttribute(extensions);
+            var value = Substitute.For<IFormFile>();
+
+            value.FileName.Returns(fileName);
+
+            attribute.IsValid(value).Should().Be(expectedIsValid);
+        }
+
+        [TestMethod]
+        public void FileExtensionAttributeTests_IsValid_Succeeds_For_Not_IFormFile() {
+            var attribute = new FileExtensionAttribute(".jpg");
+
+            attribute.IsValid("A pretty long string").Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void FileExtensionAttributeTests_FormatErrorMessage_Succeeds() {
+            var attribute = new FileExtensionAttribute(".jpg", ".jpeg") {
+                ErrorMessage = "Test {0} with extensions {1}"
+            };
+
+            attribute.FormatErrorMessage("Property").Should().Be("Test Property with extensions .jpg, .jpeg");
+        }
+    }
+}
---
+++ WarOfEmpires.Models/DataAnnotations/FileExtensionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace WarOfEmpires.Models.DataAnnotations {
+    public sealed class FileExtensionAttribute : ValidationAttribute {
+        public string[] Extensions { get; }
+
+        public FileExtensionAttribute(params string[] extensions) {
+            Extensions = extensions;
+        }
+
+        public override bool IsValid(object value) {
+            return !(value is IFormFile file) || Extensions.Contains(Path.GetExtension(file.FileName));
+        }
+
+        public override string FormatErrorMessage(string name) {
+            return string.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, string.Join(", ", Extensions));
+        }
+    }
+}
