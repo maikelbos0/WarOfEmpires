c3853db630f02b3d7b41485d183f64738a57034f
Maikel Bos <maikel.bos0@gmail.com>
2021-10-02 11:57:24 +0200

    Add client-side file extension validation

--- WarOfEmpires.Models/DataAnnotations/FileExtensionAttribute.cs
+++ WarOfEmpires.Models/DataAnnotations/FileExtensionAttribute.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 
 namespace WarOfEmpires.Models.DataAnnotations {
-    public sealed class FileExtensionAttribute : ValidationAttribute {
+    public sealed class FileExtensionAttribute : ValidationAttribute, IClientModelValidator {
         public string[] Extensions { get; }
 
         public FileExtensionAttribute(params string[] extensions) {
@@ -18,6 +19,12 @@ namespace WarOfEmpires.Models.DataAnnotations {
 
         public override string FormatErrorMessage(string name) {
             return string.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, string.Join(", ", Extensions));
+        }
+
+        public void AddValidation(ClientModelValidationContext context) {
+            context.Attributes["data-val"] = "true";
+            context.Attributes["data-val-fileextension"] = FormatErrorMessage(context.ModelMetadata.GetDisplayName());
+            context.Attributes["data-val-fileextension-extensions"] = string.Join(",", Extensions);
         }
     }
 }
--- WarOfEmpires/wwwroot/js/_bundle.js
+++ WarOfEmpires/wwwroot/js/_bundle.js
@@ -434,3 +434,20 @@ $.validator.unobtrusive.adapters.add("maxfilesize", ["size"], function (options)
     };
     options.messages["maxfilesize"] = options.message;
 });
+
+$.validator.addMethod("fileextension", function (value, element, params) {
+    if (element.files && element.files[0]) {
+        return params.extensions.some(function (extension) {
+            return element.files[0].name.endsWith(extension);
+        });
+    }
+
+    return true;
+});
+
+$.validator.unobtrusive.adapters.add("fileextension", ["extensions"], function (options) {
+    options.rules["fileextension"] = {
+        extensions: options.params.extensions.split(",")
+    };
+    options.messages["fileextension"] = options.message;
+});
