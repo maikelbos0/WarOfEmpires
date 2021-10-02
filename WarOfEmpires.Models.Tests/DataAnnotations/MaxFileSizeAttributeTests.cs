27393b4f7a0fde61d6a2baf6d69010d78a71697a
Maikel Bos <maikel.bos0@gmail.com>
2021-10-02 09:16:12 +0200

    Add file size validation

---
+++ WarOfEmpires.Models.Tests/DataAnnotations/MaxFileSizeAttributeTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using WarOfEmpires.Models.DataAnnotations;
+
+namespace WarOfEmpires.Models.Tests.DataAnnotations {
+    [TestClass]
+    public sealed class MaxFileSizeAttributeTests {
+        [DataTestMethod]
+        [DataRow(10, 9, true)]
+        [DataRow(10, 10, true)]
+        [DataRow(10, 11, false)]
+        public void MaxFileSizeAttribute_IsValid_Succeeds(int maxSize, int fileSize, bool expectedIsValid) {
+            var attribute = new MaxFileSizeAttribute(maxSize);
+            var value = Substitute.For<IFormFile>();
+
+            value.Length.Returns(fileSize);
+
+            attribute.IsValid(value).Should().Be(expectedIsValid);
+        }
+
+        [TestMethod]
+        public void MaxFileSizeAttribute_IsValid_Succeeds_For_Not_IFormFile() {
+            var attribute = new MaxFileSizeAttribute(10);
+
+            attribute.IsValid("A pretty long string").Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void MaxFileSizeAttribute_FormatErrorMessage_Succeeds() {
+            var attribute = new MaxFileSizeAttribute(10) {
+                ErrorMessage = "Test {0} with size {1}"
+            };
+
+            attribute.FormatErrorMessage("Property").Should().Be("Test Property with size 10");
+        }
+    }
+}
---
+++ WarOfEmpires.Models/DataAnnotations/MaxFileSizeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace WarOfEmpires.Models.DataAnnotations {
+    public sealed class MaxFileSizeAttribute : ValidationAttribute {
+        public int Size { get; }
+
+        public MaxFileSizeAttribute(int size) {
+            Size = size;
+        }
+
+        public override bool IsValid(object value) {
+            return !(value is IFormFile file && file.Length > Size);
+        }
+
+        public override string FormatErrorMessage(string name) {
+            return string.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, Size);
+        }
+    }
+}
