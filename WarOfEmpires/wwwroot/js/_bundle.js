8afe0ab6c51e18ff7fa4aedb0e8e85a2e3c6d711
Maikel Bos <maikel.bos0@gmail.com>
2021-10-02 09:41:19 +0200

    Add client-side file size validation

--- WarOfEmpires/wwwroot/js/_bundle.js
+++ WarOfEmpires/wwwroot/js/_bundle.js
@@ -420,3 +420,17 @@ $(function () {
         });
     });
 });
+$.validator.addMethod("maxfilesize", function (value, element, params) {
+    if (element.files && element.files[0]) {
+        return element.files[0].size <= params.size;
+    }
+
+    return true;
+});
+
+$.validator.unobtrusive.adapters.add("maxfilesize", ["size"], function (options) {
+    options.rules["maxfilesize"] = {
+        size: parseInt(options.params.size)
+    };
+    options.messages["maxfilesize"] = options.message;
+});
---
+++ WarOfEmpires/wwwroot/js/max-file-size.js
@@ -0,0 +1,14 @@
+﻿$.validator.addMethod("maxfilesize", function (value, element, params) {
+    if (element.files && element.files[0]) {
+        return element.files[0].size <= params.size;
+    }
+
+    return true;
+});
+
+$.validator.unobtrusive.adapters.add("maxfilesize", ["size"], function (options) {
+    options.rules["maxfilesize"] = {
+        size: parseInt(options.params.size)
+    };
+    options.messages["maxfilesize"] = options.message;
+});
