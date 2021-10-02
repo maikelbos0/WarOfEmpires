e55ca5a4b1eeee4dc2679838c0002d2078794e85
Maikel Bos <maikel.bos0@gmail.com>
2021-10-02 11:57:24 +0200

    Add client-side file extension validation

---
+++ WarOfEmpires/wwwroot/js/file-extension.js
@@ -0,0 +1,16 @@
+﻿$.validator.addMethod("fileextension", function (value, element, params) {
+    if (element.files && element.files[0]) {
+        return params.extensions.some(function (extension) {
+            return element.files[0].name.endsWith(extension);
+        });
+    }
+
+    return true;
+});
+
+$.validator.unobtrusive.adapters.add("fileextension", ["extensions"], function (options) {
+    options.rules["fileextension"] = {
+        extensions: options.params.extensions.split(",")
+    };
+    options.messages["fileextension"] = options.message;
+});
