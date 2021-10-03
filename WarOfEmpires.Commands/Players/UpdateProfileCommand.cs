e761dfa2578ea142840e5bbde4d88d7286acbcd0
Maikel Bos <maikel.bos0@gmail.com>
2021-10-03 10:02:37 +0200

    Set up command for saving profile

---
+++ WarOfEmpires.Commands/Players/UpdateProfileCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace WarOfEmpires.Commands.Players {
+    public sealed class UpdateProfileCommand : ICommand {
+        public string Email { get; }
+        public string FullName { get; }
+        public string Description { get; }
+        public Func<Stream> Avatar { get; }
+
+        public UpdateProfileCommand(string email, string fullName, string description, Func<Stream> avatar) {
+            Email = email;
+            FullName = fullName;
+            Description = description;
+            Avatar = avatar;
+        }
+    }
+}
