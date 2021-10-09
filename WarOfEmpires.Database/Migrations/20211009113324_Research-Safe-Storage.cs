b81c8e3a81a7a45846b92b7393bef0c4e64758ac
Maikel Bos <maikel.bos0@gmail.com>
2021-10-09 13:37:40 +0200

    Add safe storage to database

---
+++ WarOfEmpires.Database/Migrations/20211009113324_Research-Safe-Storage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfEmpires.Database.Migrations
+{
+    public partial class ResearchSafeStorage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "Empires",
+                table: "ResearchTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 5, "Safe storage" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "Empires",
+                table: "ResearchTypes",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
