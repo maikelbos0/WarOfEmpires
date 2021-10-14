70fdb58e49bb27ff18c8402539690cb4d9fc9ce4
Maikel Bos <maikel.bos0@gmail.com>
2021-10-14 18:32:23 +0200

    Add race bonuses

--- WarOfEmpires.Domain/Players/RaceDefinitionFactory.cs
+++ WarOfEmpires.Domain/Players/RaceDefinitionFactory.cs
@@ -13,27 +13,42 @@ namespace WarOfEmpires.Domain.Players {
             }
         }
 
-        // TODO set sensible bonuses
         private static RaceDefinition GenerateHumans() {
             return new RaceDefinition(
                 Race.Humans, 
-                "Humans are excellent producers that specialize agriculture. While overall not the strongest on the battlefield, their cavalry is unrivaled."
+                "Humans are excellent producers that specialize agriculture. While overall not the strongest on the battlefield, their cavalry is unrivaled.",
+                farmerModifier: 1.3M,
+                woodWorkerModifier: 1.1M,
+                archerModifier: 0.9M,
+                cavalryModifier: 1.1M,
+                footmenModifier: 0.8M
             );
         }
 
-        // TODO set sensible bonuses
         private static RaceDefinition GenerateDwarves() {
             return new RaceDefinition(
                 Race.Dwarves, 
-                "Dwarves excel in the mines where they get their ores and stones. They are fierce in hand-to-hand combat but lack experience with the bow or on horseback."
+                "Dwarves excel in the mines where they get their ores and stones. They are fierce in hand-to-hand combat but lack experience with the bow or on horseback.",
+                farmerModifier: 0.8M,
+                woodWorkerModifier: 0.8M,
+                stoneMasonModifier: 1.2M,
+                oreMinerModifier: 1.2M,
+                archerModifier: 0.9M,
+                cavalryModifier: 0.9M,
+                footmenModifier: 1.3M
             );
         }
 
-        // TODO set sensible bonuses
         private static RaceDefinition GenerateElves() {
             return new RaceDefinition(
                 Race.Elves,
-                "Elves can cultivate woodland like no other. With this wood they make the finest bows for their extremely lethal archers."
+                "Elves can cultivate woodland like no other. With this wood they make the finest bows for their extremely lethal archers.",
+                farmerModifier: 1.1M,
+                woodWorkerModifier: 1.3M,
+                stoneMasonModifier: 0.8M,
+                oreMinerModifier: 0.8M,
+                archerModifier: 1.3M,
+                footmenModifier: 0.8M
             );
         }
 
