9ecf007b4428b54c4bb77f7a167bdf23f6c849c1
Maikel Bos <maikel.bos0@gmail.com>
2021-10-18 09:40:45 +0200

    Add full profile properties to register player view

--- WarOfEmpires.Commands/Players/RegisterPlayerCommand.cs
+++ WarOfEmpires.Commands/Players/RegisterPlayerCommand.cs
@@ -1,11 +1,23 @@
-﻿namespace WarOfEmpires.Commands.Players {
+﻿using System;
+using System.IO;
+
+namespace WarOfEmpires.Commands.Players {
     public sealed class RegisterPlayerCommand : ICommand {
         public string Email { get; }
         public string DisplayName { get; }
+        public string FullName { get; }
+        public string Description { get; }
+        public Func<Stream> Avatar { get; }
 
-        public RegisterPlayerCommand(string email, string displayName) {
+        // TODO add race
+        // TODO rename to create
+
+        public RegisterPlayerCommand(string email, string displayName, string fullName, string description, Func<Stream> avatar) {
             Email = email;
             DisplayName = displayName;
+            FullName = fullName;
+            Description = description;
+            Avatar = avatar;
         }
     }
 }
