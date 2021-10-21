bef21973014e53f79598f02876469780ec4593d2
Maikel Bos <maikel.bos0@gmail.com>
2021-10-21 19:13:26 +0200

    Add race to create player command handler

--- WarOfEmpires.Commands/Players/CreatePlayerCommand.cs
+++ WarOfEmpires.Commands/Players/CreatePlayerCommand.cs
@@ -5,15 +5,15 @@ namespace WarOfEmpires.Commands.Players {
     public sealed class CreatePlayerCommand : ICommand {
         public string Email { get; }
         public string DisplayName { get; }
+        public string Race { get; }
         public string FullName { get; }
         public string Description { get; }
         public Func<Stream> Avatar { get; }
 
-        // TODO add race
-
-        public CreatePlayerCommand(string email, string displayName, string fullName, string description, Func<Stream> avatar) {
+        public CreatePlayerCommand(string email, string displayName, string race, string fullName, string description, Func<Stream> avatar) {
             Email = email;
             DisplayName = displayName;
+            Race = race;
             FullName = fullName;
             Description = description;
             Avatar = avatar;
