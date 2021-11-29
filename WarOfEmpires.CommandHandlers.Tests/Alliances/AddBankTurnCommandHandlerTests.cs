1e9496b2602fda827a3b6340832553e739aad969
Maikel Bos <maikel.bos0@gmail.com>
2021-11-29 14:02:24 +0100

    Add bank turn command handler

---
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/AddBankTurnCommandHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using WarOfEmpires.CommandHandlers.Alliances;
+using WarOfEmpires.Commands.Alliances;
+using WarOfEmpires.Repositories.Alliances;
+using WarOfEmpires.Test.Utilities;
+
+namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
+    [TestClass]
+    public sealed class AddBankTurnCommandHandlerTests {
+        [TestMethod]
+        public void AddBankTurnCommandHandler_Calls_AddBankTurn_For_All_Alliances() {
+            var builder = new FakeBuilder()
+                .WithAlliance(1, out var alliance)
+                .WithAlliance(2, out var anotherAlliance);
+
+            var handler = new AddBankTurnCommandHandler(new AllianceRepository(builder.Context));
+            var command = new AddBankTurnCommand();
+
+            handler.Execute(command);
+
+            alliance.Received().AddBankTurn();
+            anotherAlliance.Received().AddBankTurn();
+            builder.Context.CallsToSaveChanges.Should().Be(1);
+        }
+    }
+}
---
+++ WarOfEmpires.CommandHandlers/Alliances/AddBankTurnCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using VDT.Core.DependencyInjection;
+using WarOfEmpires.CommandHandlers.Decorators;
+using WarOfEmpires.Commands.Alliances;
+using WarOfEmpires.Repositories.Alliances;
+
+namespace WarOfEmpires.CommandHandlers.Alliances {
+    [TransientServiceImplementation(typeof(ICommandHandler<AddBankTurnCommand>))]
+    public sealed class AddBankTurnCommandHandler : ICommandHandler<AddBankTurnCommand> {
+        private readonly IAllianceRepository _repository;
+
+        public AddBankTurnCommandHandler(IAllianceRepository repository) {
+            _repository = repository;
+        }
+
+        [Audit]
+        public CommandResult<AddBankTurnCommand> Execute(AddBankTurnCommand command) {
+            var result = new CommandResult<AddBankTurnCommand>();
+
+            foreach (var alliance in _repository.GetAll()) {
+                alliance.AddBankTurn();
+            }
+
+            _repository.SaveChanges();
+
+            return result;
+        }
+    }
+}
