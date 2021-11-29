d02cd9032d54a87c8036cc6e9eb56d41f04f9fc0
Maikel Bos <maikel.bos0@gmail.com>
2021-11-29 09:44:23 +0100

    Add interface for alliance banking

---
+++ WarOfEmpires.Models/Alliances/BankedResourcesModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using WarOfEmpires.Models.Empires;
+
+namespace WarOfEmpires.Models.Alliances {
+    public class BankedResourcesModel {
+        public int BankTurns { get; set; }
+        public ResourcesViewModel BankedResources { get; set; }
+        [DisplayName("Gold")]
+        public int? Gold { get; set; }
+        [DisplayName("Food")]
+        public int? Food { get; set; }
+        [DisplayName("Wood")]
+        public int? Wood { get; set; }
+        [DisplayName("Stone")]
+        public int? Stone { get; set; }
+        [DisplayName("Ore")]
+        public int? Ore { get; set; }
+    }
+}
--- WarOfEmpires.Models/Alliances/BankedResourcesViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using WarOfEmpires.Models.Empires;
-
-namespace WarOfEmpires.Models.Alliances {
-    public class BankedResourcesViewModel {
-        public int BankTurns { get; set; }
-        public ResourcesViewModel BankedResources { get; set; }
-    }
-}
