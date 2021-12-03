ab28f104ebb476fda3d03c1da6b47a21f2b788d4
Maikel Bos <maikel.bos0@gmail.com>
2021-12-03 08:58:37 +0100

    Add can bank to role overview

--- WarOfEmpires.Models/Alliances/RoleViewModel.cs
+++ WarOfEmpires.Models/Alliances/RoleViewModel.cs
@@ -3,29 +3,33 @@ using WarOfEmpires.Utilities.Formatting;
 
 namespace WarOfEmpires.Models.Alliances {    
     public sealed class RoleViewModel : EntityViewModel {
-        [GridColumn(0, 18, "Name")]
+        [GridColumn(0, 20, "Name")]
         public string Name { get; set; }
         public bool CanInvite { get; set; }
-        [GridColumn(1, 12, "Can invite", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
+        [GridColumn(1, 10, "Invite", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
         public string CanInviteString { get { return CanInvite ? "Yes" : "No"; } }
         public bool CanManageRoles { get; set; }
-        [GridColumn(2, 12, "Can manage roles", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
+        [GridColumn(2, 10, "Manage roles", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
         public string CanManageRolesString { get { return CanManageRoles ? "Yes" : "No"; } }
         public bool CanDeleteChatMessages { get; set; }
-        [GridColumn(3, 12, "Can delete chat", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
+        [GridColumn(3, 10, "Delete chat", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
         public string CanDeleteChatMessagesString { get { return CanDeleteChatMessages ? "Yes" : "No"; } }       
         public bool CanKickMembers { get; set; }
-        [GridColumn(4, 12, "Can kick members", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
+        [GridColumn(4, 10, "Kick members", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
         public string CanKickMembersString { get { return CanKickMembers ? "Yes" : "No"; } }
         public bool CanManageNonAggressionPacts { get; set; }
-        [GridColumn(5, 12, "Can manage pacts", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
+        [GridColumn(5, 10, "Manage pacts", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
         public string CanManageNonAggressionPactsString { get { return CanManageNonAggressionPacts ? "Yes" : "No"; } }
         public bool CanManageWars { get; set; }
 
-        [GridColumn(6, 12, "Can manage wars", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
-        public string CanWarsString { get { return CanManageWars ? "Yes" : "No"; } }
+        [GridColumn(6, 10, "Manage wars", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
+        public string CanManageWarsString { get { return CanManageWars ? "Yes" : "No"; } }
+        public bool CanBank { get; set; }
+
+        [GridColumn(7, 10, "Bank", ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromSmall)]
+        public string CanBankString { get { return CanBank ? "Yes" : "No"; } }
         public int Players { get; set; }
-        [GridColumn(7, 10, "Players", SortData = nameof(Players), ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromMedium)]
+        [GridColumn(8, 10, "Players", SortData = nameof(Players), ResponsiveDisplayBehaviour = ResponsiveDisplayBehaviour.HiddenFromMedium)]
         public string PlayersString { get { return Players.ToString(StringFormat.Integer); } }
     }
 }
