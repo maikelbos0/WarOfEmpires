20fa2892dced83e17bd6253f527cb295dce6ea62
Maikel Bos <maikel.bos0@gmail.com>
2021-12-06 19:08:48 +0100

    Increase alliance bank turns accumulation

---
+++ WarOfEmpires.Database/Migrations/20211206180433_Alliances-Increased-BankTurns.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfEmpires.Database.Migrations
+{
+    public partial class AlliancesIncreasedBankTurns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Events",
+                table: "ScheduledTasks",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Interval",
+                value: new TimeSpan(0, 1, 0, 0, 0));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Events",
+                table: "ScheduledTasks",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Interval",
+                value: new TimeSpan(0, 2, 0, 0, 0));
+        }
+    }
+}
