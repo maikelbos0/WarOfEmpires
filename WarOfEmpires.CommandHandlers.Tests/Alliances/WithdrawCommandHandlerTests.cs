e5991f92d308072e7ab05c91adbb070babc10598
Maikel Bos <maikel.bos0@gmail.com>
2021-12-08 19:33:49 +0100

    Add withdraw command handler

---
+++ WarOfEmpires.CommandHandlers.Tests/Alliances/WithdrawCommandHandlerTests.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using WarOfEmpires.CommandHandlers.Alliances;
+using WarOfEmpires.Commands.Alliances;
+using WarOfEmpires.Domain.Common;
+using WarOfEmpires.Repositories.Players;
+using WarOfEmpires.Test.Utilities;
+
+namespace WarOfEmpires.CommandHandlers.Tests.Alliances {
+    [TestClass]
+    public sealed class WithdrawCommandHandlerTests {
+        [TestMethod]
+        public void WithdrawCommandHandler_Succeeds() {
+            var builder = new FakeBuilder()
+                .BuildAlliance(1, bankedResources: new Resources(2, 3, 4, 5, 6))
+                .BuildMember(1);
+
+            var handler = new WithdrawCommandHandler(new PlayerRepository(builder.Context));
+            var command = new WithdrawCommand("test1@test.com", 2, 3, 4, 5, 6);
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+            builder.Alliance.Received().Withdraw(builder.Player, new Resources(2, 3, 4, 5, 6));
+            builder.Context.CallsToSaveChanges.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void WithdrawCommandHandler_Allows_Empty_Values() {
+            var builder = new FakeBuilder()
+                .BuildAlliance(1)
+                .BuildMember(1);
+
+            var handler = new WithdrawCommandHandler(new PlayerRepository(builder.Context));
+            var command = new WithdrawCommand("test1@test.com", null, null, null, null, null);
+
+            var result = handler.Execute(command);
+
+            result.Success.Should().BeTrue();
+            builder.Alliance.Received().Withdraw(builder.Player, new Resources());
+            builder.Context.CallsToSaveChanges.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void WithdrawCommandHandler_Fails_For_Too_Little_Resources() {
+            var builder = new FakeBuilder()
+                .BuildAlliance(1)
+                .WithMember(1);
+
+            var handler = new WithdrawCommandHandler(new PlayerRepository(builder.Context));
+            var command = new WithdrawCommand("test1@test.com", 2, 3, 4, 5, 6);
+
+            var result = handler.Execute(command);
+
+            result.Should().HaveError("Your alliance doesn't have enough resources available to withdraw that much");
+            builder.Alliance.DidNotReceiveWithAnyArgs().Withdraw(default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void WithdrawCommandHandler_Fails_For_Too_Few_Bank_Turns() {
+            var builder = new FakeBuilder()
+                .BuildAlliance(1, bankedResources: new Resources(2, 3, 4, 5, 6), bankTurns: 0)
+                .WithMember(1);
+
+            var handler = new WithdrawCommandHandler(new PlayerRepository(builder.Context));
+            var command = new WithdrawCommand("test1@test.com", 2, 3, 4, 5, 6);
+
+            var result = handler.Execute(command);
+
+            result.Should().HaveError("Your alliance doesn't have any bank turns available");
+            builder.Alliance.DidNotReceiveWithAnyArgs().Withdraw(default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void WithdrawCommandHandler_Throws_Exception_For_Player_Not_In_Alliance() {
+            var builder = new FakeBuilder()
+                .WithPlayer(1)
+                .BuildAlliance(1);
+
+            var handler = new WithdrawCommandHandler(new PlayerRepository(builder.Context));
+            var command = new WithdrawCommand("test1@test.com", 2, 3, 4, 5, 6);
+
+            Action action = () => handler.Execute(command);
+
+            action.Should().Throw<NullReferenceException>();
+            builder.Alliance.DidNotReceiveWithAnyArgs().Withdraw(default, default);
+            builder.Context.CallsToSaveChanges.Should().Be(0);
+        }
+    }
+}
---
+++ WarOfEmpires.CommandHandlers/Alliances/WithdrawCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using VDT.Core.DependencyInjection;
+using WarOfEmpires.CommandHandlers.Decorators;
+using WarOfEmpires.Commands.Alliances;
+using WarOfEmpires.Domain.Common;
+using WarOfEmpires.Repositories.Players;
+
+namespace WarOfEmpires.CommandHandlers.Alliances {
+
+    [TransientServiceImplementation(typeof(ICommandHandler<WithdrawCommand>))]
+    public sealed class WithdrawCommandHandler : ICommandHandler<WithdrawCommand> {
+        private readonly IPlayerRepository _repository;
+
+        public WithdrawCommandHandler(IPlayerRepository repository) {
+            _repository = repository;
+        }
+
+        [Audit]
+        public CommandResult<WithdrawCommand> Execute(WithdrawCommand command) {
+            var result = new CommandResult<WithdrawCommand>();
+            var player = _repository.Get(command.Email);
+            var alliance = player.Alliance;
+            var resources = new Resources(command.Gold ?? 0, command.Food ?? 0, command.Wood ?? 0, command.Stone ?? 0, command.Ore ?? 0);
+
+            if (!alliance.BankedResources.CanAfford(resources)) {
+                result.AddError("Your alliance doesn't have enough resources available to withdraw that much");
+            }
+
+            if (alliance.BankTurns <= 0) {
+                result.AddError("Your alliance doesn't have any bank turns available");
+            }
+
+            if (result.Success) {
+                alliance.Withdraw(player, resources);
+                _repository.SaveChanges();
+            }
+
+            return result;
+        }
+    }
+}
