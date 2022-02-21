bb64b3f2e744164e085e8abae253c0db2e5d948c
Maikel Bos <maikel.bos0@gmail.com>
2022-02-21 10:17:35 +0100

    Add new decorator for action execution auditing

---
+++ WarOfEmpires.Database/Auditing/AuditDecorator.cs
@@ -0,0 +1,29 @@
+using WarOfEmpires.Domain.Auditing;
+using WarOfEmpires.Utilities.Serialization;
+using System.Diagnostics;
+using VDT.Core.DependencyInjection.Decorators;
+using WarOfEmpires.Utilities.Auditing;
+
+namespace WarOfEmpires.Database.Auditing {
+    public sealed class AuditDecorator : IAuditDecorator {
+        private readonly IWarContext _context;
+        private readonly ISerializer _serializer;
+        private Stopwatch _stopwatch;
+
+        public AuditDecorator(IWarContext context, ISerializer serializer) {
+            _context = context;
+            _serializer = serializer;
+        }
+
+        void IDecorator.BeforeExecute(MethodExecutionContext context) {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        void IDecorator.AfterExecute(MethodExecutionContext context) {
+            _stopwatch.Stop();
+
+            _context.ActionExecutions.Add(new ActionExecution(context.TargetType.FullName, context.Method.Name, _serializer.SerializeToJson(context.Arguments), _stopwatch.Elapsed.TotalMilliseconds));
+            _context.SaveChanges();
+        }
+    }
+}
