3f60a6fe945f4b68a119d5a17fb331477648a2c0
Maikel Bos <maikel.bos0@gmail.com>
2022-02-28 14:17:34 +0100

    Remove old auditing commandhandler and queryhandler

--- WarOfEmpires.QueryHandlers/Decorators/AuditAttribute.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using VDT.Core.DependencyInjection.Decorators;
-
-namespace WarOfEmpires.QueryHandlers.Decorators {
-    [AttributeUsage(AttributeTargets.Method)]
-    public sealed class AuditAttribute : Attribute, IDecorateAttribute<IAuditDecorator> { }
-}
--- WarOfEmpires.QueryHandlers/Decorators/IAuditDecorator.cs
+++
@@ -1,5 +0,0 @@
-﻿using VDT.Core.DependencyInjection.Decorators;
-
-namespace WarOfEmpires.QueryHandlers.Decorators {
-    public interface IAuditDecorator : IDecorator { }
-}
