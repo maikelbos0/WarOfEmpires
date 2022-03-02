bb80a39bbc3e13e815660eb0f2f5a65e3557fe95
Maikel Bos <maikel.bos0@gmail.com>
2022-03-02 13:45:47 +0100

    Make sure serialization ignores properties when appropriate

--- WarOfEmpires.Utilities.Tests/Serialization/SerializerTests.cs
+++ WarOfEmpires.Utilities.Tests/Serialization/SerializerTests.cs
@@ -1,6 +1,7 @@
 using WarOfEmpires.Utilities.Serialization;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json.Serialization;
 
 namespace WarOfEmpires.Utilities.Tests.Serialization {
     [TestClass]
@@ -8,17 +9,20 @@ namespace WarOfEmpires.Utilities.Tests.Serialization {
         public sealed class TestObject {
             public int Id { get; private set; }
             public string Value { get; private set; }
+            [JsonIgnore]
+            public string Password { get; private set; }
 
-            public TestObject(int id, string value) {
+            public TestObject(int id, string value, string password) {
                 Id = id;
                 Value = value;
+                Password = password;
             }
         }
 
         [TestMethod]
         public void Serializer_SerializeJson_Succeeds() {
             var serializer = new Serializer();
-            var testObject = new TestObject(1, "test");
+            var testObject = new TestObject(1, "test", "pass");
 
             serializer.SerializeToJson(testObject).Should().Be("{\"Id\":1,\"Value\":\"test\"}");
         }
@@ -26,7 +30,7 @@ namespace WarOfEmpires.Utilities.Tests.Serialization {
         [TestMethod]
         public void Serializer_SerializeJson_Succeeds_For_Special_Characters() {
             var serializer = new Serializer();
-            var testObject = new TestObject(1, "\"'\\<([{test}])>%&\r\n");
+            var testObject = new TestObject(1, "\"'\\<([{test}])>%&\r\n", "pass");
 
             serializer.SerializeToJson(testObject).Should().Be("{\"Id\":1,\"Value\":\"\\u0022\\u0027\\\\\\u003C([{test}])\\u003E%\\u0026\\r\\n\"}");
         }
