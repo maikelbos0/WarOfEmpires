8d4ff1b886109474682b0d635bc832c791ac375d
Maikel Bos <maikel.bos0@gmail.com>
2022-03-06 10:55:26 +0100

    Add middleware for user online update

---
+++ WarOfEmpires/Middleware/UserOnlineMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using WarOfEmpires.Commands.Security;
+using WarOfEmpires.Services;
+
+namespace WarOfEmpires.Middleware {
+    public class UserOnlineMiddleware {
+        private readonly RequestDelegate _next;
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IMessageService _messageService;
+
+        public UserOnlineMiddleware(RequestDelegate next, IAuthenticationService authenticationService, IMessageService messageService) {
+            _next = next;
+            _authenticationService = authenticationService;
+            _messageService = messageService;
+        }
+
+        public async Task InvokeAsync(HttpContext context) {
+            await _next(context);
+
+            if (_authenticationService.IsAuthenticated) {
+                var command = new UpdateUserLastOnlineCommand(_authenticationService.Identity);
+
+                _messageService.Dispatch(command);
+            }
+        }
+    }
+}
