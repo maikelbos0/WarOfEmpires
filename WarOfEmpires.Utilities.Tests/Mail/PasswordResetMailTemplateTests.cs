060546e3d7387907da2c3663a51c2e7e31d71ed6
Maikel Bos <maikel.bos0@gmail.com>
2022-03-11 20:15:29 +0100

    Fix utility test analyzer messages

--- WarOfEmpires.Utilities.Tests/Mail/PasswordResetMailTemplateTests.cs
+++ WarOfEmpires.Utilities.Tests/Mail/PasswordResetMailTemplateTests.cs
@@ -6,7 +6,7 @@ using WarOfEmpires.Utilities.Mail;
 namespace WarOfEmpires.Utilities.Tests.Mail {
     [TestClass]
     public sealed class PasswordResetMailTemplateTests {
-        private readonly AppSettings _appSettings = new AppSettings() {
+        private readonly AppSettings _appSettings = new() {
             ApplicationBaseUrl = "http://localhost"
         };
 
