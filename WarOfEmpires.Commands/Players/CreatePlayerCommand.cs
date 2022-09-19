f43949c8b1bbddd7b2af5f9d4a9c245589e46454
Maikel Bos <maikel.bos0@gmail.com>
2022-09-19 14:23:27 +0200

    Stop serialization of avatar streams

--- WarOfEmpires.Commands/Players/CreatePlayerCommand.cs
+++ WarOfEmpires.Commands/Players/CreatePlayerCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.Json.Serialization;
 
 namespace WarOfEmpires.Commands.Players {
     public sealed class CreatePlayerCommand : ICommand {
@@ -8,6 +9,7 @@ namespace WarOfEmpires.Commands.Players {
         public string Race { get; }
         public string FullName { get; }
         public string Description { get; }
+        [JsonIgnore]
         public Func<Stream> Avatar { get; }
 
         public CreatePlayerCommand(string email, string displayName, string race, string fullName, string description, Func<Stream> avatar) {
--- WarOfEmpires.Commands/Players/UpdateProfileCommand.cs
+++ WarOfEmpires.Commands/Players/UpdateProfileCommand.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.IO;
+using System.Text.Json.Serialization;
 
 namespace WarOfEmpires.Commands.Players {
     public sealed class UpdateProfileCommand : ICommand {
         public string Email { get; }
         public string FullName { get; }
         public string Description { get; }
+        [JsonIgnore]
         public Func<Stream> Avatar { get; }
 
         public UpdateProfileCommand(string email, string fullName, string description, Func<Stream> avatar) {
