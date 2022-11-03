3ed68f53da7d83729184f9f891dae3544b5b05ce
Maikel Bos <maikel.bos0@gmail.com>
2022-11-03 19:37:52 +0100

    Add model state merge extension

---
+++ WarOfEmpires.Api.Tests/Extensions/ModelStateDictionaryExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using FluentAssertions.Equivalency;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarOfEmpires.Api.Extensions;
+using WarOfEmpires.CommandHandlers;
+using WarOfEmpires.Commands;
+
+namespace WarOfEmpires.Api.Tests.Extensions {
+    [TestClass]
+    public sealed class ModelStateDictionaryExtensionsTests {
+        private sealed class TestCommand : ICommand {
+            public Property Test { get; set; }
+        }
+
+        [TestMethod]
+        public void Merge_Merges_Property_Error() {
+            var modelState = new ModelStateDictionary();
+            var commandResult = new CommandResult<TestCommand>();
+
+            commandResult.AddError(t => t.Test, "Test is wrong");
+            modelState.Merge(commandResult);
+
+            modelState.Should().HaveCount(1);
+            modelState.Should().ContainKey("Test").WhoseValue.Errors.Should().ContainSingle().Which.ErrorMessage.Should().Be("Test is wrong");
+        }
+
+        [TestMethod]
+        public void Merge_Merges_Model_Error() {
+            var modelState = new ModelStateDictionary();
+            var commandResult = new CommandResult<TestCommand>();
+
+            commandResult.AddError("Everything is wrong");
+            modelState.Merge(commandResult);
+
+            modelState.Should().HaveCount(1);
+            modelState.Should().ContainKey("").WhoseValue.Errors.Should().ContainSingle().Which.ErrorMessage.Should().Be("Everything is wrong");
+        }
+    }
+}
---
+++ WarOfEmpires.Api/Extensions/ModelStateDictionaryExtensions.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WarOfEmpires.CommandHandlers;
+using WarOfEmpires.Commands;
+using WarOfEmpires.Utilities.Extensions;
+
+namespace WarOfEmpires.Api.Extensions {
+    public static class ModelStateDictionaryExtensions {
+        public static void Merge<TCommand>(this ModelStateDictionary modelState, CommandResult<TCommand> commandResult) where TCommand : ICommand {
+            foreach (var commandError in commandResult.Errors) {
+                string? propertyName = null;
+
+                if (commandError.Expression != null) {
+                    propertyName = commandError.Expression.GetExpressionText();
+                }
+                
+                if (propertyName != null) {
+                    modelState.AddModelError(propertyName, commandError.Message);
+                }
+                else {
+                    modelState.AddModelError(string.Empty, commandError.Message);
+                }
+            }
+        }
+    }
+}
