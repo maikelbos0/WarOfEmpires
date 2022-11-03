1dd5190eac4a32456a6d98a264bcd8df09d2252e
Maikel Bos <maikel.bos0@gmail.com>
2022-11-03 20:28:56 +0100

    Add register post request

---
+++ WarOfEmpires.Api/Controllers/SecurityController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarOfEmpires.Api.Extensions;
+using WarOfEmpires.Api.Services;
+using WarOfEmpires.Commands.Security;
+using WarOfEmpires.Models.Security;
+
+namespace WarOfEmpires.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SecurityController : ControllerBase {
+    private readonly IMessageService messageService;
+
+    public SecurityController(IMessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    [HttpPost(nameof(Register))]
+    public IActionResult Register(RegisterUserModel model) {
+        // TODO centralize
+        // TODO warnings?
+        if (ModelState.IsValid) {
+            var result = messageService.Dispatch(new RegisterUserCommand(model.Email, model.Password));
+            ModelState.Merge(result);
+        }
+
+        if (ModelState.IsValid) {
+            return Ok();
+        }
+        else {
+            return BadRequest(ModelState);
+        }
+    }
+}
--- WarOfEmpires.Client/Program.cs
+++ WarOfEmpires.Client/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System;
 using WarOfEmpires.Client;
+using WarOfEmpires.Client.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var apiSettings = builder.Configuration.GetSection(nameof(ApiSettings)).Get<ApiSettings>();
+
+builder.Services.AddSingleton(apiSettings);
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiSettings.BaseUrl) });
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
