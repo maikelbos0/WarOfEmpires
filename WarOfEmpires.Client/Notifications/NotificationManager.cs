b309664561fcd2df84f63ded790fc580b8a1ab8d
Maikel Bos <maikel.bos0@gmail.com>
2022-11-05 09:34:56 +0100

    Show success notification

--- WarOfEmpires.Client/Notifications/NotificationManager.cs
+++ WarOfEmpires.Client/Notifications/NotificationManager.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace WarOfEmpires.Client.Notifications;
 
 public class NotificationManager {
-    private const int DefaultTimeoutInSeconds = 3;
+    private const int TimeoutInSeconds = 3;
 
     private readonly List<Notification> notifications = new();
 
@@ -13,17 +13,21 @@ public class NotificationManager {
 
     public IReadOnlyCollection<Notification> Notifications => notifications.AsReadOnly();
 
-    public async Task Notify(NotificationType type, string message, bool automaticallyDismiss) {
+    public void Notify(NotificationType type, string message, bool automaticallyDismiss) {
         var notification = new Notification(type, message);
 
         notifications.Add(notification);
         StateHasChanged?.Invoke();
 
         if (automaticallyDismiss) {
-            await Task.Delay(DefaultTimeoutInSeconds * 1000);
-
-            Dismiss(notification);
+            _ = DismissAfterTimeout(notification);
         }
+    }
+
+    private async Task DismissAfterTimeout(Notification notification) {
+        await Task.Delay(TimeoutInSeconds * 1000);
+
+        Dismiss(notification);
     }
 
     public void Dismiss(Notification notification) {
