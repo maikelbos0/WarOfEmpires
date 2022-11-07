1caa57d445f9c6e98e91754f6e17d496970f5da4
Maikel Bos <maikel.bos0@gmail.com>
2022-11-07 10:00:50 +0100

    Move command handling to base

---
+++ WarOfEmpires.Api/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarOfEmpires.Api.Extensions;
+using WarOfEmpires.Api.Services;
+using WarOfEmpires.Commands;
+
+namespace WarOfEmpires.Api.Controllers {
+    public abstract class BaseController : ControllerBase {
+        private readonly IMessageService messageService;
+
+        public BaseController(IMessageService messageService) {
+            this.messageService = messageService;
+        }
+
+        protected IActionResult ExecuteCommand<TCommand>(TCommand command) where TCommand : ICommand {
+            var result = messageService.Dispatch(command);
+
+            ModelState.Merge(result);
+
+            if (ModelState.IsValid) {
+                return Ok(result!.Warnings);
+            }
+            else {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+        }
+    }
+}
