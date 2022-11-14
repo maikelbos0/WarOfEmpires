6a6b0dad9bb5acc3f825e7d65f05c0257bec1ae3
Maikel Bos <maikel.bos0@gmail.com>
2022-11-14 14:04:08 +0100

    Add new, better named query for user claims

---
+++ WarOfEmpires.QueryHandlers.Tests/Security/GetUserClaimsQueryHandlerTests.cs
@@ -0,0 +1,120 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using WarOfEmpires.Domain.Alliances;
+using WarOfEmpires.Queries.Security;
+using WarOfEmpires.QueryHandlers.Security;
+using WarOfEmpires.Test.Utilities;
+
+namespace WarOfEmpires.QueryHandlers.Tests.Security {
+    [TestClass]
+    public sealed class GetUserClaimsQueryHandlerTests {
+        [TestMethod]
+        public void GetUserClaimsQueryHandler_Returns_Correct_Information() {
+            var builder = new FakeBuilder()
+                .BuildPlayer(1);
+
+            builder.User.IsAdmin.Returns(false);
+
+            var handler = new GetUserClaimsQueryHandler(builder.Context);
+            var query = new GetUserClaimsQuery("test1@test.com");
+
+            var result = handler.Execute(query);
+
+            result.IsAdmin.Should().BeFalse();
+            result.IsPlayer.Should().BeTrue();
+            result.IsInAlliance.Should().BeFalse();
+            result.CanTransferLeadership.Should().BeFalse();
+            result.CanInvite.Should().BeFalse();
+            result.CanManageRoles.Should().BeFalse();
+            result.CanLeaveAlliance.Should().BeFalse();
+            result.CanDisbandAlliance.Should().BeFalse();
+            result.CanManageNonAggressionPacts.Should().BeFalse();
+            result.CanManageWars.Should().BeFalse();
+            result.CanBank.Should().BeFalse();
+            result.DisplayName.Should().Be("Test display name 1");
+        }
+
+        [TestMethod]
+        public void GetUserClaimsQueryHandler_Returns_Correct_Information_IsPlayer() {
+            var builder = new FakeBuilder()
+                .BuildUser(1);
+
+            var handler = new GetUserClaimsQueryHandler(builder.Context);
+            var query = new GetUserClaimsQuery("test1@test.com");
+
+            var result = handler.Execute(query);
+
+            result.IsPlayer.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void GetUserClaimsQueryHandler_Returns_Correct_Information_IsAdmin_IsInAlliance() {
+            var builder = new FakeBuilder()
+                .BuildAlliance(1)
+                .BuildMember(1);
+
+            builder.Player.AllianceRole.Returns((Role)null);
+            builder.User.IsAdmin.Returns(true);
+
+            var handler = new GetUserClaimsQueryHandler(builder.Context);
+            var query = new GetUserClaimsQuery("test1@test.com");
+
+            var result = handler.Execute(query);
+
+            result.IsAdmin.Should().BeTrue();
+            result.IsInAlliance.Should().BeTrue();
+            result.CanTransferLeadership.Should().BeFalse();
+            result.CanInvite.Should().BeFalse();
+            result.CanManageRoles.Should().BeFalse();
+            result.CanLeaveAlliance.Should().BeTrue();
+            result.CanDisbandAlliance.Should().BeFalse();
+            result.CanManageNonAggressionPacts.Should().BeFalse();
+            result.CanManageWars.Should().BeFalse();
+            result.CanBank.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void GetUserClaimsQueryHandler_Returns_Correct_Information_Rights_In_Role() {
+            var builder = new FakeBuilder()
+                .BuildAlliance(1)
+                .WithMember(1, out var player)
+                .WithRole(2, "Test", player);
+
+            var handler = new GetUserClaimsQueryHandler(builder.Context);
+            var query = new GetUserClaimsQuery("test1@test.com");
+
+            var result = handler.Execute(query);
+
+            result.CanInvite.Should().BeTrue();
+            result.CanManageRoles.Should().BeTrue();
+            result.CanLeaveAlliance.Should().BeTrue();
+            result.CanTransferLeadership.Should().BeFalse();
+            result.CanDisbandAlliance.Should().BeFalse();
+            result.CanManageNonAggressionPacts.Should().BeTrue();
+            result.CanManageWars.Should().BeTrue();
+            result.CanBank.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void GetUserClaimsQueryHandler_Returns_Correct_Information_Rights_For_Alliance_Leader() {
+            var builder = new FakeBuilder()
+                .BuildAlliance(1)
+                .WithLeader(1);
+
+            var handler = new GetUserClaimsQueryHandler(builder.Context);
+            var query = new GetUserClaimsQuery("test1@test.com");
+
+            var result = handler.Execute(query);
+
+            result.CanInvite.Should().BeTrue();
+            result.CanManageRoles.Should().BeTrue();
+            result.CanLeaveAlliance.Should().BeFalse();
+            result.CanTransferLeadership.Should().BeTrue();
+            result.CanDisbandAlliance.Should().BeTrue();
+            result.CanManageNonAggressionPacts.Should().BeTrue();
+            result.CanManageWars.Should().BeTrue();
+            result.CanBank.Should().BeTrue();
+        }
+    }
+}
