3c878d273fd6112b83b027a080f5dbc4bd9c9f4e
Maikel Bos <maikel.bos0@gmail.com>
2022-11-20 10:37:40 +0100

    Trigger sign out when token expires

--- WarOfEmpires.Client.Tests/Services/AccessControlProviderTests.cs
+++ WarOfEmpires.Client.Tests/Services/AccessControlProviderTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
 using WarOfEmpires.Client.Services;
@@ -10,19 +11,20 @@ namespace WarOfEmpires.Client.Tests.Services;
 
 [TestClass]
 public class AccessControlProviderTests {
-    private const string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwibmFtZSI6IlRlc3QiLCJyb2xlIjoiQWRtaW5pc3RyYXRvciIsIm5iZiI6MTY2ODc2Njc2NywiZXhwIjoxNjY4NzcwMzY3LCJpYXQiOjE2Njg3NjY3Njd9.mxsVQsEVGCXuXJa7hafBzUkLExFGhqCH_nPNhNebBOc";
+    private const string validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwibmFtZSI6IlRlc3QiLCJyb2xlIjoiQWRtaW5pc3RyYXRvciIsIm5iZiI6MTY2ODg3MzcxNSwiZXhwIjo0ODIyNDczNzE1LCJpYXQiOjE2Njg4NzM3MTV9.6fJd0T_O_5sTtRCrJ4ZTurdq8mpin0wUZ4fSdEAUkdM";
+    private const string expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwibmFtZSI6IlRlc3QiLCJyb2xlIjoiQWRtaW5pc3RyYXRvciIsIm5iZiI6MTY2ODc2Njc2NywiZXhwIjoxNjY4NzcwMzY3LCJpYXQiOjE2Njg3NjY3Njd9.mxsVQsEVGCXuXJa7hafBzUkLExFGhqCH_nPNhNebBOc";
 
     [TestMethod]
     public async Task SignIn() {
         var storageService = Substitute.For<ILocalStorageService>();
-        var provider = new AccessControlProvider(storageService, new JwtSecurityTokenHandler());
+        var provider = new AccessControlProvider(storageService, Substitute.For<ITimerService>(), new JwtSecurityTokenHandler());
         AccessControlState state = null;
 
         provider.AccessControlStateChanged += s => state = s;
 
-        await provider.SignIn(token);
+        await provider.SignIn(validToken);
 
-        await storageService.Received().SetItemAsync(Constants.AccessToken, token);
+        await storageService.Received().SetItemAsync(Constants.AccessToken, validToken);
         state.Should().NotBeNull();
         state.DisplayName.Should().Be("Test");
         state.IsAuthenticated.Should().BeTrue();
@@ -32,7 +34,7 @@ public class AccessControlProviderTests {
     [TestMethod]
     public async Task SignOut() {
         var storageService = Substitute.For<ILocalStorageService>();
-        var provider = new AccessControlProvider(storageService, new JwtSecurityTokenHandler());
+        var provider = new AccessControlProvider(storageService, Substitute.For<ITimerService>(), new JwtSecurityTokenHandler());
         AccessControlState state = null;
 
         provider.AccessControlStateChanged += s => state = s;
@@ -47,11 +49,12 @@ public class AccessControlProviderTests {
     }
 
     [TestMethod]
-    public async Task GetAccessControlState_With_Token() {
+    public async Task GetAccessControlState_With_Valid_Token() {
         var storageService = Substitute.For<ILocalStorageService>();
-        var provider = new AccessControlProvider(storageService, new JwtSecurityTokenHandler());
+        var timerService = Substitute.For<ITimerService>();
+        var provider = new AccessControlProvider(storageService, timerService, new JwtSecurityTokenHandler());
 
-        storageService.GetItemAsync<string>(Constants.AccessToken).Returns(token);
+        storageService.GetItemAsync<string>(Constants.AccessToken).Returns(validToken);
 
         var state = await provider.GetAccessControlState();
 
@@ -59,20 +62,41 @@ public class AccessControlProviderTests {
         state.DisplayName.Should().Be("Test");
         state.IsAuthenticated.Should().BeTrue();
         state.IsAdmin.Should().BeTrue();
+
+        timerService.Received().ExecuteAfter(provider.SignOut, Arg.Is<TimeSpan>(timeSpan => timeSpan > (new DateTime(2122, 10, 26) - DateTime.UtcNow) && timeSpan < (new DateTime(2122, 10, 27) - DateTime.UtcNow)));
+    }
+
+    [TestMethod]
+    public async Task GetAccessControlState_With_Expired_Token() {
+        var storageService = Substitute.For<ILocalStorageService>();
+        var timerService = Substitute.For<ITimerService>();
+        var provider = new AccessControlProvider(storageService, timerService, new JwtSecurityTokenHandler());
+
+        storageService.GetItemAsync<string>(Constants.AccessToken).Returns(expiredToken);
+
+        var state = await provider.GetAccessControlState();
+        state.Should().NotBeNull();
+        state.DisplayName.Should().BeNull();
+        state.IsAuthenticated.Should().BeFalse();
+        state.IsAdmin.Should().BeFalse();
+
+        timerService.DidNotReceiveWithAnyArgs().ExecuteAfter(default, default);
     }
 
     [TestMethod]
     public async Task GetAccessControlState_Without_Token() {
         var storageService = Substitute.For<ILocalStorageService>();
-        var provider = new AccessControlProvider(storageService, new JwtSecurityTokenHandler());
+        var timerService = Substitute.For<ITimerService>();
+        var provider = new AccessControlProvider(storageService, timerService, new JwtSecurityTokenHandler());
 
         storageService.GetItemAsync<string>(Constants.AccessToken).Returns((string)null);
 
         var state = await provider.GetAccessControlState();
-        
         state.Should().NotBeNull();
         state.DisplayName.Should().BeNull();
         state.IsAuthenticated.Should().BeFalse();
         state.IsAdmin.Should().BeFalse();
+
+        timerService.DidNotReceiveWithAnyArgs().ExecuteAfter(default, default);
     }
 }
--- WarOfEmpires.Client/Services/AccessControlProvider.cs
+++ WarOfEmpires.Client/Services/AccessControlProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Blazored.LocalStorage;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,44 +9,70 @@ namespace WarOfEmpires.Client.Services;
 
 public sealed class AccessControlProvider {
     private readonly ILocalStorageService storageService;
+    private readonly ITimerService timerService;
     private readonly JwtSecurityTokenHandler jwtSecurityTokenHandler;
+    private bool isTimerStarted = false;
 
     public delegate void AccessControlStateChangedHandler(AccessControlState state);
 
     public event AccessControlStateChangedHandler? AccessControlStateChanged;
 
-    public AccessControlProvider(ILocalStorageService storageService, JwtSecurityTokenHandler jwtSecurityTokenHandler) {
+    public AccessControlProvider(ILocalStorageService storageService, ITimerService timerService, JwtSecurityTokenHandler jwtSecurityTokenHandler) {
         this.storageService = storageService;
+        this.timerService = timerService;
         this.jwtSecurityTokenHandler = jwtSecurityTokenHandler;
     }
 
     public async Task SignIn(string token) {
         await storageService.SetItemAsync(Constants.AccessToken, token);
 
-        AccessControlStateChanged?.Invoke(GetAccessControlState(token));
+        AccessControlStateChanged?.Invoke(await GetAccessControlState(token));
     }
 
     public async Task SignOut() {
         await storageService.RemoveItemAsync(Constants.AccessToken);
 
-        AccessControlStateChanged?.Invoke(GetAccessControlState(null));
+        AccessControlStateChanged?.Invoke(await GetAccessControlState(null));
+        isTimerStarted = false;
     }
 
     public async Task<AccessControlState> GetAccessControlState() {
         var token = await storageService.GetItemAsync<string?>(Constants.AccessToken);
 
-        return GetAccessControlState(token);
+        return await GetAccessControlState(token);
     }
 
-    private AccessControlState GetAccessControlState(string? token) {
+    private async Task<AccessControlState> GetAccessControlState(string? token) {
         if (token == null) {
             return new AccessControlState();
         }
 
-        var accessToken = jwtSecurityTokenHandler.ReadJwtToken(token);
+        var accessToken = await GetValidToken(token);
+
+        if (accessToken == null) {
+            return new AccessControlState();
+        }
+
         var displayName = accessToken.Claims.SingleOrDefault(c => c.Type == JwtRegisteredClaimNames.Name)?.Value;
         var isAdmin = accessToken.Claims.Any(c => c.Type == Roles.ClaimName && c.Value == Roles.Administrator);
 
         return new AccessControlState(displayName, true, isAdmin);
+    }
+
+    private async Task<JwtSecurityToken?> GetValidToken(string token) {
+        var accessToken = jwtSecurityTokenHandler.ReadJwtToken(token);
+
+        if (accessToken.ValidTo < DateTime.UtcNow) {
+            await storageService.RemoveItemAsync(Constants.AccessToken);
+
+            return null;
+        }
+
+        if (!isTimerStarted) {
+            timerService.ExecuteAfter(SignOut, accessToken.ValidTo - DateTime.UtcNow);
+            isTimerStarted = true;
+        }
+
+        return accessToken;
     }
 }
