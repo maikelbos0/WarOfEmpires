adc9d2406ecf187b2129035d81779ecaee91abf4
Maikel Bos <maikel.bos0@gmail.com>
2022-11-20 12:29:47 +0100

    Add page authorization

---
+++ WarOfEmpires.Client/Services/TokenAuthenticationStateProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WarOfEmpires.Client.Services;
+
+public sealed class TokenAuthenticationStateProvider : AuthenticationStateProvider {
+    private readonly IAccessControlProvider accessControlProvider;
+
+    public TokenAuthenticationStateProvider(IAccessControlProvider accessControlProvider) {
+        this.accessControlProvider = accessControlProvider;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync() {
+        var accessToken = await accessControlProvider.GetValidToken();
+
+        if (accessToken == null) {
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+        else {
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(accessToken.Claims, Constants.AuthenticationScheme)));
+        }
+    }
+}
