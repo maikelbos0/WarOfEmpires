65f94f2da605a69f91f06739b9ec9b533f1db3c2
Maikel Bos <maikel.bos0@gmail.com>
2022-11-27 19:23:58 +0100

    Add refresh token to database

---
+++ WarOfEmpires.Domain/Security/ExpiredRefreshToken.cs
@@ -0,0 +1,6 @@
+﻿namespace WarOfEmpires.Domain.Security {
+    public class ExpiredRefreshToken : Entity {
+        public virtual RefreshTokenFamily RefreshTokenFamily { get; protected set; }
+        public virtual byte[] Token { get; set; }
+    }
+}
---
+++ WarOfEmpires.Domain/Security/RefreshTokenFamily.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace WarOfEmpires.Domain.Security {
+    public class RefreshTokenFamily : Entity {
+        public virtual ICollection<ExpiredRefreshToken> ExpiredRefreshTokens { get; set; } = new List<ExpiredRefreshToken>();
+        public virtual byte[] CurrentToken { get; set; }
+    }
+}
