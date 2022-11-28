2d27071ba84b66e10932ebce4809b31b64460a55
Maikel Bos <maikel.bos0@gmail.com>
2022-11-28 09:59:07 +0100

    Add refresh token rotation method

--- WarOfEmpires.Domain/Security/RefreshTokenFamily.cs
+++ WarOfEmpires.Domain/Security/RefreshTokenFamily.cs
@@ -2,7 +2,19 @@
 
 namespace WarOfEmpires.Domain.Security {
     public class RefreshTokenFamily : Entity {
-        public virtual ICollection<ExpiredRefreshToken> ExpiredRefreshTokens { get; set; } = new List<ExpiredRefreshToken>();
-        public virtual byte[] CurrentToken { get; set; }
+        public virtual ICollection<ExpiredRefreshToken> ExpiredRefreshTokens { get; protected set; } = new List<ExpiredRefreshToken>();
+        public virtual byte[] CurrentToken { get; protected set; }
+
+        protected RefreshTokenFamily() {
+        }
+
+        public RefreshTokenFamily(byte[] token) : this() {
+            CurrentToken = token;
+        }
+
+        public void RotateRefreshToken(byte[] newToken) {
+            ExpiredRefreshTokens.Add(new ExpiredRefreshToken(CurrentToken));
+            CurrentToken = newToken;
+        }
     }
 }
