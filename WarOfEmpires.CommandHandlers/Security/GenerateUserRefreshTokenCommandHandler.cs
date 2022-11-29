7110d6af6c3ba6e84a9f4291e8827817c6462133
Maikel Bos <maikel.bos0@gmail.com>
2022-11-29 20:11:52 +0100

    Add handler for generating refresh token

---
+++ WarOfEmpires.CommandHandlers/Security/GenerateUserRefreshTokenCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using WarOfEmpires.Commands.Security;
+using WarOfEmpires.Repositories.Security;
+
+namespace WarOfEmpires.CommandHandlers.Security {
+    public sealed class GenerateUserRefreshTokenCommandHandler : ICommandHandler<GenerateUserRefreshTokenCommand> {
+        private readonly IUserRepository _repository;
+
+        public GenerateUserRefreshTokenCommandHandler(IUserRepository repository) {
+            _repository = repository;
+        }
+
+        public CommandResult<GenerateUserRefreshTokenCommand> Execute(GenerateUserRefreshTokenCommand command) {
+            var result = new CommandResult<GenerateUserRefreshTokenCommand>();
+            var user = _repository.GetActiveByEmail(command.Email);
+
+            user.GenerateRefreshToken(command.RequestId);
+            _repository.SaveChanges();
+
+            return result;
+        }
+    }
+}
