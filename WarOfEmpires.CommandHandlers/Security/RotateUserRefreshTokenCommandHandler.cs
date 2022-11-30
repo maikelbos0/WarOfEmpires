6d3703399914257a5f2cadf1b69a3b0ba181a61d
Maikel Bos <maikel.bos0@gmail.com>
2022-11-30 20:11:10 +0100

    Add command to rotate token

---
+++ WarOfEmpires.CommandHandlers/Security/RotateUserRefreshTokenCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WarOfEmpires.Commands.Security;
+using WarOfEmpires.Repositories.Security;
+
+namespace WarOfEmpires.CommandHandlers.Security {
+    public sealed class RotateUserRefreshTokenCommandHandler : ICommandHandler<RotateUserRefreshTokenCommand> {
+        private readonly IUserRepository _repository;
+
+        public RotateUserRefreshTokenCommandHandler(IUserRepository repository) {
+            _repository = repository;
+        }
+
+        public CommandResult<RotateUserRefreshTokenCommand> Execute(RotateUserRefreshTokenCommand command) {
+            var result = new CommandResult<RotateUserRefreshTokenCommand>();
+            var user = _repository.GetActiveByEmail(command.Email);
+
+            if (user.RotateRefreshToken(command.RequestId, Convert.FromBase64String(command.RefreshToken))) {
+                _repository.SaveChanges();
+            }
+            else {
+                result.AddError("Invalid refresh token");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ WarOfEmpires.Commands/Security/RotateUserRefreshTokenCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WarOfEmpires.Commands.Security {
+    public sealed class RotateUserRefreshTokenCommand : ICommand {
+        public string Email { get; }
+        public Guid RequestId { get; }
+        public string RefreshToken { get; }
+
+        public RotateUserRefreshTokenCommand(string email, Guid requestId, string refreshToken) {
+            Email = email;
+            RequestId = requestId;
+            RefreshToken = refreshToken;
+        }
+    }
+}
