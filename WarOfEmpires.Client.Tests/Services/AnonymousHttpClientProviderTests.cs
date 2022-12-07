3570c1b0af546e06867537860c283043fb115bf5
Maikel Bos <maikel.bos0@gmail.com>
2022-12-07 20:21:33 +0100

    Split http client factory part 1: anonymous http client provider

---
+++ WarOfEmpires.Client.Tests/Services/AnonymousHttpClientProviderTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WarOfEmpires.Client.Configuration;
+using WarOfEmpires.Client.Services;
+
+namespace WarOfEmpires.Client.Tests.Services;
+
+[TestClass]
+public class AnonymousHttpClientProviderTests {
+    [TestMethod]
+    public void AnonymousHttpClientProvider_Provide_Returns_Configured_Client() {
+        var provider = new AnonymousHttpClientProvider(new ApiSettings() { BaseUrl = "https://localhost:1234/" });
+
+        var client = provider.Provide();
+
+        client.BaseAddress.ToString().Should().Be("https://localhost:1234/");
+    }
+
+    [TestMethod]
+    public void AnonymousHttpClientProvider_Caches_Client() {
+        var provider = new AnonymousHttpClientProvider(new ApiSettings() { BaseUrl = "https://localhost:1234/" });
+
+        var client = provider.Provide();
+
+        client.Should().BeSameAs(provider.Provide());
+    }
+}
---
+++ WarOfEmpires.Client/Services/IAnonymousHttpClientProvider.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Net.Http;
+
+namespace WarOfEmpires.Client.Services;
+
+public interface IAnonymousHttpClientProvider : IDisposable {
+    HttpClient Provide();
+}
