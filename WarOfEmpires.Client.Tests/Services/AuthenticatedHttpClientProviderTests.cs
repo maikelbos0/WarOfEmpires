79797d1e1e4557b037e11500927a8a66906deab0
Maikel Bos <maikel.bos0@gmail.com>
2022-12-07 20:34:05 +0100

    Split http client factory part 2: authenticated http client provider

---
+++ WarOfEmpires.Client.Tests/Services/AuthenticatedHttpClientProviderTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Threading.Tasks;
+using WarOfEmpires.Client.Configuration;
+using WarOfEmpires.Client.Services;
+
+namespace WarOfEmpires.Client.Tests.Services;
+
+[TestClass]
+public class AuthenticatedHttpClientProviderTests {
+    [TestMethod]
+    public async Task AuthenticatedHttpClientProvider_Provide_Returns_Configured_Client() {
+        var accessControlService = Substitute.For<IAccessControlService>();
+        var provider = new AuthenticatedHttpClientProvider(accessControlService, new ApiSettings() { BaseUrl = "https://localhost:1234/" });
+
+        accessControlService.GetAccessToken().Returns("token");
+
+        var client = await provider.Provide();
+
+        client.BaseAddress.ToString().Should().Be("https://localhost:1234/");
+        client.DefaultRequestHeaders.Authorization.Should().NotBeNull();
+        client.DefaultRequestHeaders.Authorization.Scheme.Should().Be(Constants.AuthenticationScheme);
+        client.DefaultRequestHeaders.Authorization.Parameter.Should().Be("token");
+
+    }
+
+    [TestMethod]
+    public async Task AuthenticatedHttpClientProvider_Caches_Client() {
+        var provider = new AuthenticatedHttpClientProvider(Substitute.For<IAccessControlService>(), new ApiSettings() { BaseUrl = "https://localhost:1234/" });
+
+        var client = await provider.Provide();
+
+        client.Should().BeSameAs(await provider.Provide());
+    }
+}
---
+++ WarOfEmpires.Client/Services/IAuthenticatedHttpClientProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WarOfEmpires.Client.Services;
+
+public interface IAuthenticatedHttpClientProvider : IDisposable {
+    Task<HttpClient> Provide();
+}
