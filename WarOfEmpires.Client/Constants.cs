7d3a975a50695713cde49d47a19e53480c18f765
Maikel Bos <maikel.bos0@gmail.com>
2022-12-08 20:52:55 +0100

    Clean up unnecessary bloat

--- WarOfEmpires.Client/Constants.cs
+++ WarOfEmpires.Client/Constants.cs
@@ -2,8 +2,6 @@
 
 public static class Constants
 {
-    public const string UnauthenticatedClient = nameof(UnauthenticatedClient);
-    public const string AuthenticatedClient = nameof(AuthenticatedClient);
     public const string AuthenticationScheme = "Bearer";
     public const string Tokens = nameof(Tokens);
 }
--- WarOfEmpires.Client/Services/IHttpClientProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-
-namespace WarOfEmpires.Client.Services;
-
-public interface IHttpClientProvider {
-    Task<HttpClient> Provide(bool requireAuthentication);
-}
