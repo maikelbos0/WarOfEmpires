5af9812990834efbefb9985e65e2537f08fece4c
Maikel Bos <maikel.bos0@gmail.com>
2022-12-10 10:13:03 +0100

    Save after rotating out token family

--- WarOfEmpires.CommandHandlers.Tests/Security/RotateUserRefreshTokenCommandHandlerTests.cs
+++ WarOfEmpires.CommandHandlers.Tests/Security/RotateUserRefreshTokenCommandHandlerTests.cs
@@ -14,8 +14,7 @@ namespace WarOfEmpires.CommandHandlers.Tests.Security {
         [TestMethod]
         public void RotateUserRefreshTokenCommandHandler_Succeeds() {
             var builder = new FakeBuilder()
-                .BuildUser(1)
-                .WithRefreshTokenFamily(1, Guid.NewGuid(), new byte[] { 1, 1, 1, 1, 1, 1, 1, 1 });
+                .BuildUser(1);
 
             builder.User.RotateRefreshToken(Arg.Any<Guid>(), Arg.Any<byte[]>()).Returns(true);
 
@@ -32,8 +31,7 @@ namespace WarOfEmpires.CommandHandlers.Tests.Security {
         [TestMethod]
         public void RotateUserRefreshTokenCommandHandler_Fails_For_Invalid_Token() {
             var builder = new FakeBuilder()
-                .BuildUser(1)
-                .WithRefreshTokenFamily(1, Guid.NewGuid(), new byte[] { 1, 1, 1, 1, 1, 1, 1, 1 });
+                .BuildUser(1);
 
             builder.User.RotateRefreshToken(Arg.Any<Guid>(), Arg.Any<byte[]>()).Returns(false);
 
@@ -44,14 +42,13 @@ namespace WarOfEmpires.CommandHandlers.Tests.Security {
 
             result.Should().HaveError("Invalid refresh token");
             builder.User.Received().RotateRefreshToken(command.RequestId, Arg.Is<byte[]>(t => t.SequenceEqual(new byte[] { 2, 2, 2, 2, 2, 2, 2, 2 })));
-            builder.Context.CallsToSaveChanges.Should().Be(0);
+            builder.Context.CallsToSaveChanges.Should().Be(1);
         }
 
         [TestMethod]
         public void RotateUserRefreshTokenCommandHandler_Throws_Exception_For_Invalid_User() {
             var builder = new FakeBuilder()
-                .BuildUser(1)
-                .WithRefreshTokenFamily(1, Guid.NewGuid(), new byte[] { 1, 1, 1, 1, 1, 1, 1, 1 });
+                .BuildUser(1);
 
             var handler = new RotateUserRefreshTokenCommandHandler(new UserRepository(builder.Context));
             var command = new RotateUserRefreshTokenCommand("wrong@test.com", Guid.NewGuid(), Convert.ToBase64String(new byte[] { 2, 2, 2, 2, 2, 2, 2, 2 }));
