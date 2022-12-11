67ed809c24c99f9268700a8f2c75414e083a1e98
Maikel Bos <maikel.bos0@gmail.com>
2022-12-11 11:20:41 +0100

    Reintroduce messagehandler to ensure tokens are not expired

---
+++ WarOfEmpires.Client/Services/HttpMessageAuthenticationHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WarOfEmpires.Client.Services;
+
+public class HttpMessageAuthenticationHandler : DelegatingHandler {
+    private readonly IAccessControlService accessControlService;
+
+    public HttpMessageAuthenticationHandler(IAccessControlService accessControlService, HttpMessageHandler innerHandler) {
+        this.accessControlService = accessControlService;
+        InnerHandler = innerHandler;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+        var token = await accessControlService.GetAccessToken();
+
+        if (token != null) {
+            request.Headers.Authorization = new AuthenticationHeaderValue(Constants.AuthenticationScheme, token);
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
--- WarOfEmpires.Client/Services/IAuthenticatedHttpClientProvider.cs
+++ WarOfEmpires.Client/Services/IAuthenticatedHttpClientProvider.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace WarOfEmpires.Client.Services;
 
 public interface IAuthenticatedHttpClientProvider : IDisposable {
-    Task<HttpClient> Provide();
+    HttpClient Provide();
 }
