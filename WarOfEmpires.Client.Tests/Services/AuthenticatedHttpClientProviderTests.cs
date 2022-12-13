a6e62a4b93978794c178721e345927891fc1fe9a
Maikel Bos <maikel.bos0@gmail.com>
2022-12-13 20:23:31 +0100

    Remove arduous test for authentication

--- WarOfEmpires.Client.Tests/Services/AuthenticatedHttpClientProviderTests.cs
+++ WarOfEmpires.Client.Tests/Services/AuthenticatedHttpClientProviderTests.cs
@@ -1,7 +1,7 @@
 ﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
-using System.Threading.Tasks;
+using System.Net.Http;
 using WarOfEmpires.Client.Configuration;
 using WarOfEmpires.Client.Services;
 
@@ -10,27 +10,20 @@ namespace WarOfEmpires.Client.Tests.Services;
 [TestClass]
 public class AuthenticatedHttpClientProviderTests {
     [TestMethod]
-    public async Task AuthenticatedHttpClientProvider_Provide_Returns_Configured_Client() {
-        var accessControlService = Substitute.For<IAccessControlService>();
-        var provider = new AuthenticatedHttpClientProvider(accessControlService, new ApiSettings() { BaseUrl = "https://localhost:1234/" });
+    public void AuthenticatedHttpClientProvider_Provide_Returns_Configured_Client() {
+        var provider = new AuthenticatedHttpClientProvider(Substitute.For<HttpMessageAuthenticationHandler>(Substitute.For<IAccessControlService>(), Substitute.For<HttpMessageHandler>()), new ApiSettings() { BaseUrl = "https://localhost:1234/" });
 
-        accessControlService.GetAccessToken().Returns("token");
-
-        var client = await provider.Provide();
+        var client = provider.Provide();
 
         client.BaseAddress.ToString().Should().Be("https://localhost:1234/");
-        client.DefaultRequestHeaders.Authorization.Should().NotBeNull();
-        client.DefaultRequestHeaders.Authorization.Scheme.Should().Be(Constants.AuthenticationScheme);
-        client.DefaultRequestHeaders.Authorization.Parameter.Should().Be("token");
-
     }
 
     [TestMethod]
-    public async Task AuthenticatedHttpClientProvider_Caches_Client() {
-        var provider = new AuthenticatedHttpClientProvider(Substitute.For<IAccessControlService>(), new ApiSettings() { BaseUrl = "https://localhost:1234/" });
+    public void AuthenticatedHttpClientProvider_Caches_Client() {
+        var provider = new AuthenticatedHttpClientProvider(Substitute.For<HttpMessageAuthenticationHandler>(Substitute.For<IAccessControlService>(), Substitute.For<HttpMessageHandler>()), new ApiSettings() { BaseUrl = "https://localhost:1234/" });
 
-        var client = await provider.Provide();
+        var client = provider.Provide();
 
-        client.Should().BeSameAs(await provider.Provide());
+        client.Should().BeSameAs(provider.Provide());
     }
 }
