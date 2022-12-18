4388c95b4ef0bf898bba5149e8fbf51982b5c49d
Maikel Bos <maikel.bos0@gmail.com>
2022-12-18 13:12:06 +0100

    Add empire controller for resource header

---
+++ WarOfEmpires.Api.Routing/Empire.cs
@@ -0,0 +1,5 @@
+﻿namespace WarOfEmpires.Api.Routing;
+
+public enum Empire {
+    ResourceHeader
+}
---
+++ WarOfEmpires.Api/Controllers/EmpireController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarOfEmpires.Api.Services;
+using WarOfEmpires.Queries.Empires;
+
+namespace WarOfEmpires.Api.Controllers;
+
+[ApiController]
+[Route(nameof(Routing.Empire))]
+public class EmpireController : BaseController {
+    public EmpireController(IMessageService messageService, IIdentityService identityService) : base(messageService, identityService) {
+    }
+
+    [HttpGet(nameof(Routing.Empire.ResourceHeader))]
+    public IActionResult ResourceHeader()
+        => Ok(messageService.Dispatch(new GetResourceHeaderQuery(identityService.Identity)));
+}
