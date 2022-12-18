dea874101eff6e222029206863ba7f93249d882f
Maikel Bos <maikel.bos0@gmail.com>
2022-12-18 19:58:20 +0100

    Move class out of component

---
+++ WarOfEmpires.Client/Components/ValidationProblemDetails.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WarOfEmpires.Client.Components;
+
+public sealed class ValidationProblemDetails {
+    public IDictionary<string, string[]> Errors { get; }
+
+    public ValidationProblemDetails(IDictionary<string, string[]> errors) {
+        Errors = errors;
+    }
+}
